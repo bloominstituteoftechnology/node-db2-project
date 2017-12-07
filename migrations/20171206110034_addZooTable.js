6f1effa020adeaceb4e25fa311a48821852d5a09
Kia Choi <sgchoi529@email.arizona.edu>
2017-12-06 17:59:47 -0700

    Kia's Commit - Update on server.js

--- migrations/20171206110034_addZooTable.js
+++ migrations/20171206110034_addZooTable.js
@@ -1,10 +1,10 @@
 exports.up = function (knex, Promise) {
     // what happens when we run the migrations
-    return knex.schema.createTable('zoos', function (tbl) {
+    return knex.schema.createTable('zoos', function(tbl) {
         tbl.increments('id');
         tbl.string('name', 255).notNullable().unique('name', 'uq_zoo_name');
         tbl.timestamp('created_at').defaultTo(knex.fn.now());
-    })
+    });
 };
 
 exports.down = function (knex, Promise) {
--- server.js
+++ server.js
@@ -9,30 +9,89 @@ const server = express();
 server.use(bodyParser.json());
 
 // endpoints here
-server.get('/zoos', function (req, res) {
+server.get('/zoos', function(req, res) {
     const zoos = knex('zoos')
-        .then(function (records) {
+        .then(function(records) {
             res.status(200).json(records);
         })
-        .catch(function (error) {
+        .catch(function(error) {
             res.status(500).json({ error });
         });
 });
 
-server.get('/zoos/:id', function (req, res) {
+server.get('/zoos/:id', function(req, res) {
     const { id } = req.params;
 
     const zoos = knex('zoos')
         .where('id', id)
-        .then(function (records) {
+        .then(function(records) {
             res.status(200).json(records);
         })
-        .catch(function (error) {
+        .catch(function(error) {
             res.status(500).json({ error });
         });
 });
 
+server.post('/zoos', function(req, res) {
+    const zoo = req.body;
+    knex
+        .insert(zoo)
+        .into('zoos')
+        .then(function(ids) {
+            res.status(201).json({ ids: ids });
+        })
+        .catch(function(err) {
+            if (err.code === 'SQLITE_CONSTRAINT') {
+                res.status(422).json({ error: 'The Zoo already exist' });
+            } else {
+                res.status(500).json(err);
+            }
+        });
+});
+
+server.put('/zoos/:id', function(req, res) {
+    knex('zoos')
+        .where('id', req.params.id)
+        .update(req.body)
+        .then(function(count) {
+            res.status(200).json({ updated: count });
+        })
+        .catch(function(err) {
+            if (err.code === 'SQLITE_CONSTRAINT') {
+                res.status(422)
+                    .json({ error: 'The Zoo already exist' });
+            } else {
+                res.status(500).json(err);
+            }
+        });
+});
+
+server.delete('/zoos/:id', function(req, res) {
+    knex('zoos')
+        .where('id', req.params.id)
+        .del()
+        .then(function(count) {
+            res.status(200).json({ deleted: count });
+        })
+        .catch(function(err) {
+            if (err.code === 'SQLITE_CONSTRAINT') {
+                res.status(422).json({ error: 'The Zoo already exist' });
+            } else {
+                res.status(500).json(err);
+            }
+        });
+});
+
+server.get('/', function(req, res) {
+    knex('zoos')
+        .join('bears')
+        .select('zoos.*', 'bears.species')
+        .then(function(records) {
+            res.status(200).json(records);
+        });
+});
+
 const port = 3000;
-server.listen(port, function () {
+server.listen(port, function() {
     console.log(`Server Listening on ${port}`);
 });
