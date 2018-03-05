9eed7b79d6c4c7b88aaf91864a5765675ddf08cc
Johnathan Justinn <jjustinn85@gmail.com>
2018-03-05 15:51:37 -0500

    First table created for name in Knex

---
+++ database/migrations/20180305135941_CreateZoosTable.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  // for making changes to the database
+  return knex.schema.createTable('zoos', function(tbl) {
+    tbl.increments();
+
+    tbl
+      .string('name', 255)
+      .notNullable()
+      .unique('name');
+
+    tbl.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  // for undoing changes
+  return knex.schema.dropTableIfExists('zoos');
+};
