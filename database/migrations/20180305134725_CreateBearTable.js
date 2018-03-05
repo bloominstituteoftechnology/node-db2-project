6cf608c2408a880596867a72fbd0e35e59f6f35e
Troy Williams <troyDonwilliams@gmail.com>
2018-03-05 15:35:22 -0600

    mini done

---
+++ database/migrations/20180305134725_CreateBearTable.js
@@ -0,0 +1,21 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("bears", tbl => {
+    tbl.increments("id").primary();
+
+    tbl
+      .integer("zooid")
+      .unsigned()
+      .references("id") // foreign key
+      .inTable("zoos");
+
+    tbl.string("species", 80).unique("species");
+
+    tbl.string("latinName", 80);
+
+    tbl.timestamp("createdAt").default(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("bears");
+};
--- server.js
+++ server.js
@@ -1,13 +1,63 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const knex = require("./database/db");
 const server = express();
 
 server.use(bodyParser.json());
 
 // endpoints here
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "Running....." });
+});
+
+server.post("/zoos", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    knex
+      .insert({ name })
+      .into("zoos")
+      .then(id => {
+        res.status(200).json({ success: `${name} created with id: ${id}` });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "problem adding the zoo" });
+      });
+  } else {
+    res.status(422).json({ error: "Please pass in a name, man!" });
+  }
+});
+
+server.get("/zoos", (req, res) => {
+  knex
+    .select()
+    .from("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "No zoos found or error getting zoos" });
+    });
+});
+
+server.get("/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    knex("zoos")
+      .where({ id })
+      .select("name")
+      .then(zoo => {
+        res.status(200).json(zoo[0].name);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Cannot find a zoo with that id." });
+      });
+  } else {
+    res.status(422).json({ error: "Please include an id number" });
+  }
+});
 
 const port = 3000;
 server.listen(port, function() {
-    console.log(`Server Listening on ${port}`);
+  console.log(`Server Listening on ${port}`);
 });
