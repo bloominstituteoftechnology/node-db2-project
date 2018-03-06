3021d74c12ff5726a099d8dd065f6c1767e38bd3
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-03-06 00:13:12 -0600

    knexfile updated

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      database: 'zoos_db',
+      user: 'LordOrbnauticus',
+      password: 'Hwo0dj28'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: '.database/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './database/seeds',
+    }
+  }
+};
