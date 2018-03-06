e4c6b39fd3fa9aae470c9040d1c8803e0fa3a903
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-03-06 00:35:14 -0600

    DB import and zoos complete

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
       max: 10
     },
     migrations: {
-      directory: '.database/migrations',
+      directory: './database/migrations',
       tableName: 'knex_migrations'
     },
     seeds: {
