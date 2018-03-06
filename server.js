62509637dfac1b68627006516301172dd3857192
Samuel Cha <sunchipssam@gmail.com>
2018-03-05 16:17:58 -0800

    Deleted knex dep in server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const knex = require('./database/db');
-
 const server = express();
 
 server.use(bodyParser.json());
