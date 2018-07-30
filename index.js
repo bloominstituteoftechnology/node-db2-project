3cd0adcb1bb3232d99a7e760bfdcb4f22fef10ce
Luis Hernandez <luishrd@gmail.com>
2018-07-29 21:10:16 -0600

    updatess dependencies and instructions

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+// endpoints here
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
