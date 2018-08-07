364d50239a86d6084f7a1db3e28231d2019a9685
nickolaussmith <nickolaussmith721@gmail.com>
2018-08-07 16:53:14 -0600

    WOrking on CRUD

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 
+const db = require('./data/db');
+
 const server = express();
 
 server.use(express.json());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('up and running...');
+});
+
+server.get('/zoos', (req, res) => {
+  db('zoos').then(zoos => {
+    res.status(200).json(zoos);
+
+  }) .catch(err => res.status(500).json(err));
+})
 
 const port = 3300;
 server.listen(port, function() {
