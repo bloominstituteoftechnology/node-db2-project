3834538ea2fbf4543d67fff26c8d9f393e318f8c
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-08-07 16:18:41 -0600

    GET and POST endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+// const helmet = require('helmet');
+// const cors = require('cors');
+const db = require('./data/db');
 
 
 const server = express();
@@ -8,6 +9,28 @@ const server = express();
 server.use(express.json());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('up and running...');
+});
+
+server.get('/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(err => res.status(500).json(err));
+});
+
+server.post('/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db.insert(zoo).into('zoos')
+  .then(ids =>{
+    const id = ids[0];
+     res.status(201).json({ id, ...zoo });
+  })
+  .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,8 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/cs11.sqlite3'
-    }
+    },
+    useNullAsDefault: true,
   },
 
   staging: {
