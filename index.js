2a7e9f59d504da8bacee136597a745fc3d4e972a
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-07 12:43:09 -0700

    Sean Chavez RDBMS-API-Mini Create GET zoos endpoint Create POST zoos endpoint Create GET zoo by id endpoint

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
 
+const db = require('./data/db');
+
 const server = express();
 
 server.use(express.json());
 
-// endpoints here
+server.get('/', (req, res) => {
+  res.send('up and running...');
+})
+
+server.get('/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/zoos', (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo).into('zoos')
+  .then(ids => {
+    const id = ids[0];
+    res.status(201).json({id, ...zoo })
+  })
+  .catch(err => res.status(500).json(err));
+});
+
+server.get('/zoos/:id', (req, res) => {
+  db('zoos').select().where(req.params)
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(200).json(err);
+    });
+})
 
 const port = 3300;
 server.listen(port, function() {
