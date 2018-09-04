9830e67a2e0793d5072b28495f1b26f99131fbbb
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-04 15:16:12 -0500

    added GET endpoint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +12,18 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('API Running?');
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+      // .select('name')
+      .then(zoos => {
+          res.status(200).json(zoos);
+      })
+      .catch(err => res.status(500).json(err));
+})
 
 const port = 3300;
 server.listen(port, function() {
