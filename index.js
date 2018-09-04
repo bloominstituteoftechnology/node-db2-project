d3632f299d2c4fa3574fa4f4f6563b96dc17bc21
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-09-04 17:47:04 -0500

    made db and CRUD ops for bears

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ server.get("/", (req, res) => {
   res.send("API running....");
 });
 
+//ZOOS ENDPOINTS
+
 server.get("/api/zoos", (req, res) => {
   db("zoos")
     .then(zoos => {
@@ -41,7 +43,7 @@ server.get("/api/zoos/:id", (req, res) => {
       console.log("error", err);
       res
         .status(500)
-        .json({ error: "The zoos information could not be retrieved" });
+        .json({ error: "The bear information could not be retrieved" });
     });
 });
 
@@ -57,7 +59,7 @@ server.post("/api/zoos", (req, res) => {
       console.log("error", err);
       res
         .status(500)
-        .json({ error: "The zoo information could not be retrieved" });
+        .json({ error: "There was an error saving the zoo to the database." });
     });
 });
 
@@ -90,9 +92,87 @@ server.delete("/api/zoos/:id", (req, res) => {
     })
     .catch(err => {
       console.log("error", err);
+      res.status(500).json({ error: "The zoo could not be deleted" });
+    });
+});
+
+
+//BEARS ENDPOINTS
+
+server.get("/api/bears", (req, res) => {
+  db("bears")
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => {
+      console.log("error", err);
       res
         .status(500)
-        .json({ error: "The zoo could not be deleted" });
+        .json({ error: "The bears information could not be retrieved" });
+    });
+});
+
+server.get("/api/bears/:id", (req, res) => {
+  const { id } = req.params;
+  db("bears")
+    .where({ id })
+    .then(bear => {
+      res.status(200).json(bear);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The bear information could not be retrieved" });
+    });
+});
+
+server.post("/api/bears", (req, res) => {
+  const bear = req.body;
+
+  db.insert(bear)
+    .into("bears")
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "There was an error saving the bear to the database." });
+    });
+});
+
+server.put("/api/bears/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  db("bears")
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The bear information could not be updated" });
+    });
+});
+
+server.delete("/api/bears/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("bears")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ error: "The bear could not be deleted" });
     });
 });
 
