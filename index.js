c253071947ef33ebabb2127341fd3dd304ea7a21
Trevor <trevorfehrman@gmail.com>
2018-09-04 13:19:39 -0700

    Server configured, Post request working

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+
+const knex = require("knex");
+const knexConfig = require("./knexfile");
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -8,7 +13,23 @@ server.use(helmet());
 
 // endpoints here
 
+server.get("/", (req, res) => {
+	res.send("API is workin' yo");
+});
+
+server.post("/api/zoos", (req, res) => {
+	const zoo = req.body;
+	db.insert(zoo)
+		.into("zoos")
+		.then(ids => {
+			res.status(201).json(ids);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
+
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
