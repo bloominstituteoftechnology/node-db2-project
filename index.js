c41b92021fa431afa712fab7c4fb45e13f30ad7f
francistse23 <wtse.ucsd@gmail.com>
2018-09-04 11:54:23 -0700

    DELETE working.

--- index.js
+++ index.js
@@ -64,6 +64,19 @@ server.put('/api/zoos/:id', (req, res) => {
     .catch( err => res.status(500).json(err));
 });
 
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .del()
+    .then( count => {
+      res.status(200).json(count);
+    })
+    .catch( err => {
+      res.status(500).json(err);
+    });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
