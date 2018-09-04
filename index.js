8f9d49d469f8e335c239155d5245882009c55739
Ashwin <aswinjeganath@gmail.com>
2018-09-05 02:25:01 +0530

    Set up knex

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 
 const server = express();
 
+const db = require('knex')(require('./knexfile').development);
+
 server.use(express.json());
 server.use(helmet());
 
