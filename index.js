254b5eb87852d1decbbe7b0445e4fd94b223bd5b
szincone <sawyerzincone@gmail.com>
2018-09-04 13:53:20 -0600

    get request test working

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const helmet = require("helmet");
+// knex
+const knex = require("knex");
+const dbConfig = require("./knexfile");
+const db = knex(dbConfig.development);
 
 const server = express();
 
-server.use(express.json());
 server.use(helmet());
+server.use(express.json());
 
 // endpoints here
+server.get("/api/zoos", (req, res) => {
+  res.send("Get request working");
+});
 
+// end endpoints
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
