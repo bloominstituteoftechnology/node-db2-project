1ec9d5ea6e390f90702f67d2f6563e22d93fb166
mkerbleski <mkerbles@gmail.com>
2018-09-04 14:28:23 -0600

    completed stretch

--- index.js
+++ index.js
@@ -53,6 +53,45 @@ server.put('/api/zoos/:id', (req, res) => {
   }).catch(err => res.status(500).json(err))
 })
 
+
+server.get('/api/bears/', (req, res) => {
+  db('bears').then(bears => {
+    res.status(200).json(bears)
+  }).catch(err => res.status(500).json(err))
+})
+
+server.get('/api/bears/:id', (req, res) => {
+  db('bears').where('id', '=', req.params.id).then(bears => {
+    res.status(200).json(bears)
+  }).catch(err => res.status(500).json(err))
+})
+
+server.post('/api/bears', (req, res) => {
+  if (req.body.name) {
+    db.insert(req.body).into('bears').then(zooId => {
+      res.status(201).json(zooId)
+    }).catch(err => res.status(500).json(err))
+  } else {
+    res.status(400).json({error: "Please include a name in your request and try again."})
+  }
+})
+
+server.delete('/api/bears/:id', (req, res) => {
+  db('bears').where('id', '=', req.params.id)
+    .del()
+    .then(count => {
+    res.status(200).json(count)
+  }).catch(err => res.status(500).json(err))
+})
+
+server.put('/api/bears/:id', (req, res) => {
+  db('bears').where('id', '=', req.params.id)
+    .update(req.body)
+    .then(count => {
+    res.status(200).json(count)
+  }).catch(err => res.status(500).json(err))
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
