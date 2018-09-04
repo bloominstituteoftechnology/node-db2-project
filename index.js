8caabeecd14d8512d29d30e9add595702d0315a2
Ashwin <aswinjeganath@gmail.com>
2018-09-05 02:58:37 +0530

    Add route handlre to delete zoo records by id

--- index.js
+++ index.js
@@ -59,6 +59,21 @@ server
           .json({ message: 'Record updated successfully', count: data });
       })
       .catch(next);
+  })
+  .delete((req, res, next) => {
+    db('zoos')
+      .delete()
+      .where({ id: req.params.id })
+      .then(data => {
+        if (!data)
+          return res
+            .status(404)
+            .json({ message: 'The zoo with the specified ID cannot be found' });
+
+        res
+          .status(200)
+          .json({ message: 'Record deleted successfully', count: data });
+      });
   });
 
 server.use(function(err, _, res, _) {
