6b73fcbec943326da3fd740900fda015c1ffecd0
Grant Reighard <grant.reighard@gmail.com>
2018-09-04 17:09:46 -0400

    Finished stretch tasks.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// zoos endpoints here
 server.post('/api/zoos', (req, res) => {
   const { name } = req.body;
   console.log(name);
@@ -57,6 +57,62 @@ server.put('/api/zoos/:id', (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
   db('zoos')
+  .where({id})
+  .update({name})
+  .then(count => {
+    res.status(200).json(count);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
+})
+
+// bears endpoints here
+server.post('/api/bears', (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+  db.insert({name}).into('bears')
+  .then(ids => {
+    res.status(201).json(ids)
+  })
+  .catch(err => res.status(500).json(err))
+})
+
+server.get('/api/bears', (req, res) => {
+  db('bears')
+  .then(bears => {
+    res.status(200).json(bears);
+  })
+  .catch(err => res.status(500).json(err));
+})
+
+server.get('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  db('bears')
+  .where({id})
+  .then(bear => {
+    res.status(200).json(bear);
+  })
+  .catch(err => res.status(500).json(err));
+})
+
+server.delete('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  db('bears')
+  .where({id})
+  .del()
+  .then(count => {
+    res.status(200).json(count);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
+})
+
+server.put('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  db('bears')
   .where({id})
   .update({name})
   .then(count => {
