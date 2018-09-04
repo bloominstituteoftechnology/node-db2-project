ff658469a6a67d9ef43d0c89bb07c74ae427118a
francistse23 <wtse.ucsd@gmail.com>
2018-09-04 11:29:17 -0700

    POST working.

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
+
 // endpoints here
+
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+  if (!zoo.name) {
+    res.status(400).json({
+      message: "Name is required."
+    });
+  } else {
+    db.insert(zoo)
+      .into('zoos')
+      .then( ids => {
+        res.status(201).json(ids);
+      })
+      .catch( err => {
+        res.status(500).json(err);
+      })
+  };
+});
+
+server.get("/api/zoos", (req, res) => {
+
+});
 
 const port = 3300;
 server.listen(port, function() {
