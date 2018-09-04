f80d219ffecee5e126aca2b7ead57ad1e5681b50
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-04 16:17:36 -0400

    Setup, working on routes -get all -get by id -post -delete

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +12,49 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+//GET all zoos
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json(err));
+});
+
+//GET zoo by id
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('zoos')
+    .where({ id })
+    .then(zoo => zoo.length ?
+              res.status(200).json(zoo[0]) :
+              res.status(404).json({ message: "No zoo with that id" })
+          )
+    .catch(err => res.status(500).json(err));
+});
+
+//POST a new zoo
+server.post('/api/zoos', (req, res) => {
+  const { name } = req.body;
+
+  db
+    .insert({ name })
+    .into('zoos')
+    .then(zoos => res.status(201).json(zoos))
+    .catch(err => res.status(500).json(errr));
+
+});
+
+//DELETE zoo by id
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('zoos')
+    .where({ id })
+    .del()
+    .then(delZoo => res.status(200).json(delZoo))
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
