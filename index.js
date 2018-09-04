fe6d648c457d0ff7331834b1eafbf03b48c2a9aa
Sean <sean47900@gmail.com>
2018-09-04 16:41:07 -0500

    PUT working

--- index.js
+++ index.js
@@ -53,7 +53,20 @@ server.post("/api/zoos", (req, res) => {
       })
   };
 });
- 
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const updated = req.body;
+  db('zoos')
+    .where({ id })
+    .update(updated)
+    .then( update => {
+      res.status(200).json(update);
+    })
+    .catch( err => res.status(500).json(err));
+});
+
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
