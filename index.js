4dacd0702fdb0b6c020f6e88296eb81dbe489825
Sean <sean47900@gmail.com>
2018-09-04 17:00:56 -0500

    DELETE working

--- index.js
+++ index.js
@@ -66,6 +66,18 @@ server.put('/api/zoos/:id', (req, res) => {
     .catch( err => res.status(500).json(err));
 });
 
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .del()
+    .then( count => {
+      res.status(200).json(count);
+    })
+    .catch( err => {
+      res.status(500).json(err);
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
