52fb116459616740af6ac7ce0aba91caf025f344
dasgma <dasgrigoma@gmail.com>
2018-09-04 11:33:31 -0700

    Get by ID working.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +12,38 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos').then(zoos => {
+      res.status(201).json(zoos);
+  })
+  .catch(error => {
+      res.status(500).json(error);
+  })
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+  const {id} = req.params;
+  // Using knex
+  db('zoos')
+    .select()
+    .where('id', id)
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo).into('zoos')
+      .then(ids => {
+          res.status(201).json(ids);
+      })
+      .catch(error => res.status(500).json(error))
+});
 
 const port = 3300;
 server.listen(port, function() {
