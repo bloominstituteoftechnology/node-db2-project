60ec22ad03a4ffd252a4e1590be0f4879a1f9ff1
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-09-04 11:40:11 -0700

    implement GET/POST endpoints

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ app.use(helmet());
 
 // endpoints here
 
+app.get('/api/zoos', async (req, res) => {
+	try {
+		const names = await db('zoos');
+		res.status(200).json(names)
+	} catch(err){
+		res.status(500).json(err);
+	}
+})
+
+app.post('/api/zoos', async (req, res) => {
+	const zoo = req.body;
+	try {
+		const id = await db('zoos').insert(zoo)
+		res.status(201).json(id);
+	} catch(err){
+		res.status(500).json(err);
+	}
+})
+
 const port = 9000;
 app.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
