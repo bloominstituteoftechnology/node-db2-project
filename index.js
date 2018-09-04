e86ccca9ec2ec145767a39202038a9407555726f
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-04 15:51:28 -0400

    post and get

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const config = require('./knexfile');
+const db = knex(config.development);
 
 const server = express();
 
@@ -7,6 +11,32 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('API is running...')
+});
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  if (!zoo.name) {
+    res.status(400).json({ error: "Please provide a name for the zoo." })
+  } else
+    db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json({ error: "There was an error saving the zoo." }))
+})
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(err => res.status(500).json(err));
+})
+
+
 
 const port = 3300;
 server.listen(port, function() {
