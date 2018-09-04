cf830ea7c3909344007ea9081589a58937796c24
Sean <sean47900@gmail.com>
2018-09-04 15:41:30 -0500

    server test working

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
 const server = express();
+
+const dbConfig = require("./knexfile"); 
+const db = knex(dbConfig.development); 
 
 server.use(express.json());
 server.use(helmet());
 
 
 // endpoints here
+server.get("/", (req, res)=>{
+  res.send("API Running..."); 
+})
+
+
 
 const port = 3300;
 server.listen(port, function() {
