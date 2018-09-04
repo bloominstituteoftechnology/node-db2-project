f2cf774ae751b9afd3143c8663f349be80b2bead
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-04 17:12:26 -0400

    added bears table

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// zoos
 server.get('/', (req, res) => {
   res.send('API is running...')
 });
@@ -78,6 +78,69 @@ server.put('/api/zoos/:id', (req, res) => {
     .catch(err => res.status(500).json(err));
 })
 
+// bears ---------------------------------------------------------------
+server.post('/api/bears', (req, res) => {
+  const bear = req.body;
+  if (!bear.name) {
+    res.status(400).json({ error: "Please provide a name for the bear." })
+  } else
+    db.insert(bear)
+    .into('bears')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json({ error: "There was an error saving the bear." }))
+})
+
+server.get('/api/bears', (req, res) => {
+  db('bears')
+  .then(bears => {
+    res.status(200).json(bears)
+  })
+  .catch(err => res.status(500).json(err));
+})
+
+server.get('/api/bears/:id', (req, res) => {
+  const {id} = req.params;
+  db('bears').where({ id: id })
+  .then(bear => {
+    if (bear.length === 0) {
+      res.status(404).json({ message: "The bear with the specified ID does not exist." });
+    } else 
+      res.status(200).json(bear);
+  })
+  .catch(err => res.status(500).json(err));
+})
+
+server.delete('/api/bears/:id', (req, res) => {
+  const {id} = req.params;
+  db('bears').where({ id: id }).del()
+  .then(count => {
+    if (count) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "The bear with the specified ID does not exist." });
+    }
+  })
+  .catch(err => res.status(500).json(err));
+})
+
+server.put('/api/bears/:id', (req, res) => {
+  const {id} = req.params;
+  const bear = req.body;
+  if (!bear.name) {
+    res.status(400).json({ error: "Please provide a name for the bear." })
+  } else
+    db('bears').where({ id: id }).update(bear)
+    .then(count => {
+      if (count) {
+        res.status(200).json({ message: "The bear was successfully updated." });
+      } else {
+        res.status(404).json({ message: "The bear with the specified ID does not exist." });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+})
 
 const port = 3300;
 server.listen(port, function() {
