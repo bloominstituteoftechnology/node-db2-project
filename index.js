9a74537dd735ac3d83b7d2c3c882cb99d89fd184
Erik Kimsey <erik@erikkimsey.com>
2018-09-04 17:28:14 -0400

    GET, POST, PUT complete.

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/zoos', (req, res)=>{
+  db('zoos')
+  .select('name')
+  .then((data) => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    console.log('get error: ', err);
+    res.status(500).json({message: 'Unable to request zoos'})
+  })
+});
+
+server.post('/zoos', (req, res)=>{
+  let zoo = req.body;
+  db.insert(zoo)
+    .into('zoos')
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch( err => {
+      console.log('Error: ', err);
+      
+      res.status(500).json({message: 'Zoo was not created. Please, make sure a zoo name was provided.'})
+    })
+});
+
+server.put('/zoos', (req, res)=>{
+  let id = req.body.id;
+  console.log(id);
+  
+  db('zoos')
+    .where('id', id)
+    .update(req.body)
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => {
+
+      res.status(500).json({message:` ${err}`})
+    })
+})
 
 const port = 3300;
 server.listen(port, function() {
