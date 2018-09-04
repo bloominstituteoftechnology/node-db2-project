913311b967295fafdb4afedd14d2b897174458bf
Carey <careycbaldwin@gmail.com>
2018-09-04 16:54:26 -0400

    knex database configured, API is running

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
+const dbConfig = require('./knexfile')
+
+const db = knex(dbConfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/', (req, res) => {
+  res.send('API is Running');
+});
+
 
 const port = 3300;
 server.listen(port, function() {
