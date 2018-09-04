7883a2f42fa851ae5dbd53c438a472a72571eb02
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-09-04 16:15:57 -0500

    MVP finished
    Finished working on the PUT and its working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex'); 
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
-const dbConfig = require("./knexfile"); 
+const dbConfig = require("./knexfile");
 
-const db = knex(dbConfig.development); 
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -12,57 +12,59 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-server.get("/", (req, res)=>{
-  res.send("API Running..."); 
-})
+server.get("/", (req, res) => {
+  res.send("API Running...");
+});
 
-server.get("/zoos", (req, res)=>{
-  db('zoos')
-  .then(zoos => res.status(200).json(zoos))
-  .catch(err => res.status(500).json(err)); 
-})
+server.get("/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json(err));
+});
 
-// server.get("/zoos/:id", (req, res)=>{
-//   const id = req.params.id; 
-//   db("zoos")
-//   .where(id)
-//   .then(zoos =>{
-//     if(zoos === 0){
-//       res.status(404).json({message: "error getting id"})
-//       return
-//     }
-//     res.json(zoos, {message: "working"}); 
-//   })
-//   .catch(err => res.status(500).json(err)); 
-// })
-server.get('/zoos/:id', (req, res) => {
+server.get("/zoos/:id", (req, res) => {
   const { id } = req.params;
-  db('zoos')
+  db("zoos")
     .where({ id })
     .then(zoo => res.status(200).json(zoo))
     .catch(err => res.status(500).json(err));
 });
 
-server.post('/zoos', (req, res)=>{
-  const animal = req.body; 
+server.post("/zoos", (req, res) => {
+  const animal = req.body;
 
-  console.log(animal)
+  console.log(animal);
   db.insert(animal)
-  .into("zoos")
-  .then(ids =>{
-    res.status(201).json(ids);
-  })
-  .catch(err => res.status(500).json(err)); 
-}); 
+    .into("zoos")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
+});
 
-server.delete('/zoos/:id', (req, res)=>{
+server.delete("/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count) res.status(200).json(count);
+      else res.status(404).json({ error: "the animal was not found" });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put("/zoos/:id", (req, res)=>{
   const { id } = req.params; 
-  db('zoos')
+  db("zoos")
   .where({id})
-  .del()
+  .update(req.body) 
   .then(count =>{
-    if(count) res.status(200).json(count); 
-    else res.status(404).json({error: "the animal was not found"})
+    if(count){
+      res.status(201).json(count)
+    }else{
+      res.status(404).json({error: "the animal was not found, sorry no editting"})
+    }
   })
   .catch(err => res.status(500).json(err)); 
 })
