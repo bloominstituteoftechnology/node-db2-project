29d9b0822f154e21f81f27f22f8c68e051b58a9d
johnoro <officialjohnorourke@gmail.com>
2018-09-04 15:20:17 -0400

    Added endpoints for single get, put, and delete.

--- api/zoos.js
+++ api/zoos.js
@@ -13,4 +13,30 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .del()
+    .then(count => res.status(200).json(count))
+    .catch(err => res.status(500).json(err));
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .update(req.body)
+    .then(count => res.status(200).json(count))
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
