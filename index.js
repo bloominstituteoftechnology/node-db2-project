039409be3cf10959713e684c95ec467ec10e5677
adamcglee <adamcglee@gmail.com>
2018-09-04 14:11:15 -0700

    updated code to reach MVP

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +12,59 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('API running...')
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(err => res.status(500).json(err))
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({ id: req.params.id})
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(err => res.status(500).json(err))
+})
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo)
+  .into('zoos')
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => res.status(500).json(err))
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({ id: req.params.id })
+  .del()
+  .then(() => {
+    res.status(204).end()
+  })
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const zoo = req.body;
+  console.log(zoo)
+  db('zoos')
+  .where({ id: req.params.id})
+  .update(zoo)
+  .then(() => {
+    res.status(200).json({ message: 'Update Completed'})
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Update Failed!'})
+  })
+});
 
 const port = 3300;
 server.listen(port, function() {
