15e6985176b2882c8c858abc916cede3107dd8ab
Lauren <llwor94@gmail.com>
2018-09-04 14:23:56 -0700

    all methods working

--- index.js
+++ index.js
@@ -47,6 +47,40 @@ server.post('/api/zoos', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+server.put('/api/zoos/:id', (req, res) => {
+  const zoo = req.body;
+  if (!zoo.name) {
+    return res
+      .status(400)
+      .json({ message: 'Please provide a name for the zoo.' });
+  }
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(zoo)
+    .then(response => {
+      if (!response) {
+        return res.status(404).json({ message: 'The zoo does not exist.' });
+      }
+      res.status(200).json({ id: req.params.id, name: zoo.name });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(data => {
+      if (!data) {
+        return res
+          .status(404)
+          .json({ message: 'The zoo with that id does not exist' });
+      }
+      res.status(200).json({ id: req.params.id });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
