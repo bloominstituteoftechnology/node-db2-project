bfd4eca810dd514eb7aa18e1e897f6c6d4caaaa2
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-09-04 16:51:49 -0500

    completed basic GETS and POST

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const dbConfig = require("./knexfile");
+
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +12,47 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get("/", (req, res) => {
+  res.send("API running....");
+});
+
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The zoos information could not be retrieved" });
+    });
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id })
+    .then(zoo => {
+      res.status(200).json(zoo);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The zoos information could not be retrieved" });
+    });
+});
+
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+
+  db.insert(zoo)
+    .into("zoos")
+    .then(id => res.status(201).json(id))
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
