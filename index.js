00f74f1fffcb467cc6a922a7167eec854fbd37e3
szincone <sawyerzincone@gmail.com>
2018-09-04 14:10:13 -0600

    post request working

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ server.use(helmet());
 server.use(express.json());
 
 // endpoints here
+// GET
 server.get("/api/zoos", (req, res) => {
   // res.send("Get request working");
   db("zoos")
@@ -21,6 +22,25 @@ server.get("/api/zoos", (req, res) => {
       res.status(500).json({ error: "The zoos could not be retrieved." });
     });
 });
+// end GET
+
+// start POST
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+  if (!zoo.name) {
+    return res.status(400).json({
+      errorMessage: "Please provide a name for the zoo.",
+    });
+  } else {
+    db.insert(zoo)
+      .into("zoos")
+      .then(zoos => {
+        res.status(201).json(zoos);
+      })
+      .catch(err => res.status(500).json(err));
+  }
+});
+// end POST
 
 // end endpoints
 const port = 3300;
