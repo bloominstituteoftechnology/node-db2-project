ebc65d6d472cb8d952139ec3136287f02aa1fad3
Grant Reighard <grant.reighard@gmail.com>
2018-09-04 16:34:39 -0400

    Initial commit.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+// const db = require('./data/db');
+
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +12,23 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', async (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+  db.insert({name}).into('zoos')
+  .then(ids => {
+    res.status(201).json(ids)
+  })
+  .catch(err => res.status(500).json(err))
+})
+
+server.get('/api/zoos', async (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos);
+  })
+  .catch(err => res.status(500).json(err))
+})
 
 const port = 3300;
 server.listen(port, function() {
