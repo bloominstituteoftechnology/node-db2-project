8c230f448c39df0ae955676a5dc08ed98c652d0d
Lauren <llwor94@gmail.com>
2018-09-04 13:17:00 -0700

    post method working

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ server.get('/api/zoos', (req, res) => {
   });
 });
 
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
