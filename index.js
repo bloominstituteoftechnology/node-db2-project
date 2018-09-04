cbe5992b8d7be673d9fb4323f2ed79a96ad8ed76
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-04 16:27:20 -0400

    opening PR

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
 
@@ -7,6 +7,8 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get();
 
 const port = 3300;
 server.listen(port, function() {
