5009c178d1fa7147d07cf7e80b842985eae8e756
codejoncode <jonathanjholloway@gmail.com>
2018-09-04 15:05:18 -0500

    able to perform all CRUD operations on bears table get by id get all put post delete.

--- index.js
+++ index.js
@@ -79,6 +79,76 @@ server.delete("/api/zoos/:id", (req, res) => {
     });
 });
 
+//BEARS Endpoints 
+server.get("/api/bears", (req,res) => {
+  db("bears")
+    .then(bears => {
+      res.status(200).json(bears)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+server.get("/api/bears/:id", (req,res) => {
+  const { id } = req.params; 
+  db("bears")
+    .where({ id })
+    .then(bear => {
+      res.status(200).json(bear)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+server.post("/api/bears", (req,res) => {
+  const bear = req.body; 
+  console.log(bear)
+  if(!bear.name){
+    res.status(500).json({ Error: "Must include name property" }); //REPEATING CONVERT TO FUNCTION.
+  }
+  db.insert(bear)
+    .into("bears")
+    .then(bearId => {
+      res.status(201).json(bearId)
+    })
+    .catch(error => {
+      res.status(500).json({ error })
+    })
+})
+
+server.put("/api/bears/:id", (req,res) => {
+  const changes = req.body;
+  const { id } = req.params; 
+  if(!changes.name){
+    res.status(500).json({ Error: "Must include name property" }); //REPEATING CONVERT TO FUNCTION.
+  }
+  db("bears")
+    .update(changes)
+    .where({ id })
+    .then(count => {
+      res.status(200).json(count)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+server.delete("/api/bears/:id", (req,res) => {
+  const { id } = req.params; 
+  db("bears")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
