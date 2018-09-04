cfd5cad76d9d860fa716ccbe4ad1ce5008ac5924
LizBaker <lizbaker.pdx@gmail.com>
2018-09-04 13:08:57 -0700

    Get, getbyid, post, put, delete methods working -working on an error catch for getbyid requests when the ID doesn't exist

--- index.js
+++ index.js
@@ -1,27 +1,88 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-const dbconfig = require('./knexfile');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const dbconfig = require("./knexfile");
 const db = knex(dbconfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-server.get('/zoos', (req, res)=> {
-  db('zoos').select('name').then(zoo => {
+server.get("/zoos", (req, res) => {
+  db("zoos")
+    .select("name")
+    .then(zoo => {
       res.status(201).json(zoo);
-  })
-})
-
-server.post('/zoos', (req, res)=> {
-  const zoo = req.body;
-  db('zoos').insert(zoo).then(ids => {
-      res.status(201).json(ids);
-  })
-})
-
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "error fetching data" });
+    });
 });
+
+server.get("/zoos/:id", (req, res) => {
+  if (db.select("zoos").where({ id: req.params.id })) {
+    db("zoos")
+      .select("name")
+      .where({ id: req.params.id })
+      .then(zoo => {
+        res.status(200).json(zoo);
+      })
+      .catch(err => {
+        console.log("error", err);
+
+        res.status(500).json({ message: "error fetching data" });
+      });
+  } else {
+    res
+      .status(404)
+      .json({ message: "The zoo with the specified ID does not exist" });
+  }
+});
+
+server.post("/zoos", (req, res) => {
+  const zoo = req.body;
+  if (zoo != null && zoo.name != "") {
+    db("zoos")
+      .insert(zoo)
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        console.log("error", err);
+        res.status(500).json({ message: "error posting data" });
+      });
+  } else {
+    res.status(400).json({ error: "please provide a zoo name" });
+  }
+});
+
+server.put("/zoos/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "error updating data" });
+    });
+});
+
+server.delete("/zoos/:id", (req, res) => {
+  db("zoos")
+    .delete()
+    .where({ id: req.params.id })
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "error deleting data" });
+    });
+});
+
+server.listen(3300, () =>
+  console.log("\n=== Web API Listening on port 3300 ===\n")
+);
