41b2be1b369b555a8d1229d2bd8d61099170f3d1
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-04 17:22:46 -0400

    POST and GET routes completed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
+const knex = require("knex");
+
+const dbConfig = require("./knexfile");
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -8,9 +12,41 @@ server.use(helmet());
 
 // endpoints here
 
-server.get();
-
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+  if (!req.name) {
+    res.status(400).json({ message: "Need to provide a name for the zoo" });
+  }
+  db.insert(zoo)
+    .into("zoos")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
 });
+
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .then(row => {
+      if (row.length < 1) {
+        res.statusZ(404).json({ message: "Item does not exist" });
+      }
+      res.status(200).json(row);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.listen(3300, console.log("\n ==== WORKING ==== \n"));
