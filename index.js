0ca01fdb0a7420235f00d6a482edf599355e0fd6
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-09-04 14:48:15 -0500

    API Running

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex'); 
+
+const dbConfig = require("./knexfile"); 
+
+const db = knex(dbConfig.development); 
 
 const server = express();
 
@@ -7,6 +12,9 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get("/", (req, res)=>{
+  res.send("API Running..."); 
+})
 
 const port = 3300;
 server.listen(port, function() {
