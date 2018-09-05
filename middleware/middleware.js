a02fce4478adc79131d0ca1e138ff6ebb2d65d30
szincone <sawyerzincone@gmail.com>
2018-09-04 19:43:30 -0600

    deleted redundant comments

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -5,12 +5,9 @@ const bearRoutes = require("../routes/bearRoutes.js");
 const zooRoutes = require("../routes/zooRoutes.js");
 
 module.exports = server => {
-  // causes express middleware
-  // stack to be added to every layer (request function)
   server.use(helmet());
   server.use(express.json());
-  // must be used when using express Router
-  // links url with requests
+
   server.use("/api/bears", bearRoutes);
   server.use("/api/zoos", zooRoutes);
 };
