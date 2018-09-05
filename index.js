aeb0aa44a8a30df6c9b4288bc2d0320314e34a56
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-09-04 22:15:01 -0400

    added api routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
+const ZooRoutes = require("./zoo/ZooRoutes");
 
 const dbConfig = require("./knexfile");
 const db = knex(dbConfig.development);
@@ -9,69 +10,12 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use("/api/zoos", ZooRoutes);
 
 // endpoints here
 
-server.post("/api/zoos", (req, res) => {
-  const zoo = req.body;
-  db.insert(zoo)
-    .into("zoos")
-    .then(ids => {
-      res.status(201).json(ids);
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-server.get("/api/zoos", (req, res) => {
-  db("zoos")
-    .then(zoos => {
-      res.status(200).json(zoos);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
-});
-
-server.get("/api/zoos/:id", (req, res) => {
-  db("zoos")
-    .where({ id: req.params.id })
-    .then(row => {
-      if (row.length < 1) {
-        res.statusZ(404).json({ message: "Item does not exist" });
-      }
-      res.status(200).json(row);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
-});
-
-server.put("/api/zoos/:id", (req, res) => {
-  const zoo = req.body;
-  db("zoos")
-    .where({ id: req.params.id })
-    .update(zoo)
-    .then(res => {
-      if (!res) {
-        res.status(404).json({ message: "Item does not exist" });
-      }
-      res.status(200).json({ id: req.params.id, name: zoo.name });
-    })
-
-    .catch(err => res.status(500).json(err));
-});
-
-server.delete("/api/zoos/:id", (req, res) => {
-  db("zoos")
-    .where({ id: req.params.id })
-    .delete()
-    .then(item => {
-      if (!item) {
-        res.status(404).json({ message: "That ID does not exist" });
-      }
-      res.status(200).json({ id: req.params.id });
-    })
-    .catch(err => res.status(500).json(err));
+server.get("/", (req, res) => {
+  res.send("Root Route");
 });
 
 server.listen(3300, console.log("\n ==== WORKING ==== \n"));
---
+++ zoo/ZooRoutes.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const knex = require("knex");
+
+const router = express.Router();
+
+const dbConfig = require("../knexfile");
+const db = knex(dbConfig.development);
+
+router.post("/", (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo)
+    .into("zoos")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+router.get("/", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .then(row => {
+      if (row.length < 1) {
+        res.statusZ(404).json({ message: "Item does not exist" });
+      }
+      res.status(200).json(row);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const zoo = req.body;
+  db("zoos")
+    .where({ id: req.params.id })
+    .update(zoo)
+    .then(res => {
+      if (!res) {
+        res.status(404).json({ message: "Item does not exist" });
+      }
+      res.status(200).json({ id: req.params.id, name: zoo.name });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+router.delete("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .delete()
+    .then(item => {
+      if (!item) {
+        res.status(404).json({ message: "That ID does not exist" });
+      }
+      res.status(200).json({ id: req.params.id });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
