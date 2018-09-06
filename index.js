9b2ef9ec5f708ec49eeb7f682104c6838a212413
Kwaku Dapaah <kwa2the4@gmail.com>
2018-09-06 12:42:36 -0400

    made changes

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const helmet = require('helmet');
+const db = require('./knexfile');
+const morgan = require('morgan');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(morgan('combined'));
 
 // endpoints here
+server.post('/api/zoos', async (req, res) => {
+  const zoo = req.body;
+  if(zoo.name) {
+    try {
+      const response = await db.insert(zoo).into('zoos');
+      res.status(201).json(response)
+
+    } catch(err) {
+      res.status(501).json({ message: 'cannot save name to server'});
+    }
+  } else {
+    res.status(404).json({ error: 'must provide a name'});
+  }
+})
+
+server.get('/api/zoos', async (res, req) => {
+  try {
+    const response = await db('zoos');
+    res.status(201).json(response);
+  } catch(err) {
+    res.status(501).json({ message: 'cannot retrieve the list of zoos' });
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
