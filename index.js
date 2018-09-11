787cd46ac3ef3e14b5bdaabc9151a27f4e0500e9
vanceleon <vanceleon44@gmail.com>
2018-09-11 19:56:21 -0400

    creation of db

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
-const dbConfig = require('./knexfile');
+const dbConfig = require("./knexfile");
 
 const db = knex(dbConfig.development);
-
 
 const server = express();
 
@@ -14,14 +13,40 @@ server.use(helmet());
 
 // endpoints here
 
-server.get('/', (req, res) => {
-  res.send('API Running...');
+server.get("/", (req, res) => {
+  res.send("API Running...");
 });
 
+//get courses
+
+server.get("/api/courses", (req, res) => {
+  db("courses")
+    .select("name")
+    .then(courses => {
+      res.status(200).json(err)
+});
+});
+
+//add a course
 
 
 
-const port = 3300;
+
+
+server.post('/api/courses', (req, res) => {
+  const course = req.body;
+
+  db.insert(course)
+    .into('courses')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
+});
+
+const port = 8000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,12 @@ module.exports = {
   development: {
       client: 'sqlite3',
       connection: {
-        filename: './db/lambda.sqlite3',    // update path 
+        filename: './data/lambda.sqlite3',    // update path 
     },
     useNullAsDefault: true, // add this line 
   },
+  // production: {
+
+  // }
 };
 
