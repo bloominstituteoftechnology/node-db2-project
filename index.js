9a17d230e37cb50688b855dca440d14663c167c1
bri79319 <bri79319@gmail.com>
2018-09-11 16:21:23 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const dataConfig = require("./knexfile");
+
+const dataBase = knex(dataConfig.development);
 
 const server = express();
 
@@ -8,7 +13,11 @@ server.use(helmet());
 
 // endpoints here
 
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
-});
+// const port = 3300;
+// server.listen(port, function() {
+//   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+// });
+
+server.listen(3300, () =>
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
+);
