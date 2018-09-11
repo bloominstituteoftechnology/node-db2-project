279317f27834e022555949e72a069f6388512c53
JoCatherine Griggs <jocafrin@gmail.com>
2018-09-11 11:57:29 -0400

    added dependencies

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const server = express();
+
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
