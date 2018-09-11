ec2f3b739fcba889b9d24c111696a7f3451ba97a
Arthur Bates <aabatesj@gmail.com>
2018-09-11 18:05:55 -0400

    Arthur Bates
    still working on endpoints

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -8,7 +12,24 @@ server.use(helmet());
 
 // endpoints here
 
+server.post('/zoos', (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+  db.insert({ name }).into('zoos')
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => res.status(500).json(err));
+})
+server.get('/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err))
+})
+
 const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+server.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}`);
 });
