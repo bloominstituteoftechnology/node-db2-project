3ebe7a60d63457424d89b919654bbd8946245060
farste <sfargali@gmail.com>
2018-09-11 18:46:57 -0400

    Stephen Fargali Get and Post requests working

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const dbConfig = require("./knexfile");
+const db = knex(dbConfig.development);
 
 const server = express();
-
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
 
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .select("*")
+    .then(names => {
+      res.status(200).json(names);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/api/zoos", (req, res) => {
+  const name = req.body;
+  console.log(name);
+  db.insert(name)
+    .into('zoos')
+    .then(names => {
+      res.status(201).json(names);
+    })
+    .catch(err => res.status(500).json(err));
+});
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
