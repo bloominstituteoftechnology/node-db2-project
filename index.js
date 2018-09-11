bee88ceb1cc0746db799280398668af2542448f6
JoCatherine Griggs <jocafrin@gmail.com>
2018-09-11 13:45:35 -0400

    get, post, put and delete have been set up!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const knex = require('knex');
-const dbconfigure = require('./knexfile');
+const knex = require("knex");
+const dbconfigure = require("./knexfile");
 const db = knex(dbconfigure.development);
 const server = express();
 
@@ -11,21 +11,77 @@ server.use(helmet());
 
 // endpoints here
 
-server.get('zoos', (req,res) => {
-  db('zoos').select('name').then(zoo => {
-    res.status(201).json(zoo);
-  })
-})
+server.get("/zoos", (req, res) => {
+  db("zoos")
+    .select("name")
+    .then(zoo => {
+      res.status(201).json(zoo);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "could not retrieve data" });
+    });
+});
 
-server.post('/zoos', (req, res)=> {
+server.get("/zoos/:id", (req, res) => {
+  if (db.select("zoos").where({ id: req.params.id })) {
+    db("zoos")
+      .select("name")
+      .where({ id: req.params.id })
+      .then(zoo => {
+        res.status(200).json(zoo);
+      })
+      .catch(err => {
+        console.log("error", err);
+        res.status(500).json({ message: "error fetching data" });
+      });
+  } else {
+    res.status(404).json({ message: "There is no zoo with the specified ID" });
+  }
+});
+
+server.post("/zoos", (req, res) => {
   const zoo = req.body;
-  db('zoos').insert(zoo).then(ids => {
-      res.status(201).json(ids);
-  })
-})
+  if (zoo != null && zoo.name != "") {
+    db("zoos")
+      .insert(zoo)
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        console.log("error", err);
+        res.status(500).json({ message: "error posting data" });
+      });
+  } else {
+    res.status(400).json({ error: "zoo name is required" });
+  }
+});
 
+server.put("/zoos/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "could not update data" });
+    });
+});
 
-
+server.delete("/zoos/:id", (req, res) => {
+  db("zoos")
+    .delete()
+    .where({ id: req.params.id })
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "error deleting data!" });
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
