c0d3d6f9f691aa771747a2201f5caf14cff42296
AI2EJAY <JHammett1314@gmail.com>
2018-09-11 15:23:59 -0500

    MVP Complete

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
-
 const dbConfig = require("./knexfile");
-const db = knex(dbConfig.development);
 
+const db = knex(dbConfig.development);
 const server = express();
 
 server.use(express.json());
@@ -14,9 +13,10 @@ server.use(helmet());
 server.get("/", (req, res) => {
   res.send("api is running");
 });
+
 //GET
 server.get("/api/zoos", (req, res) => {
-  db('zoos')
+  db("zoos")
     .then(zoos => {
       res.status(200).json(zoos);
     })
@@ -26,9 +26,49 @@ server.get("/api/zoos", (req, res) => {
 // POST
 server.post("/api/zoos", (req, res) => {
   const zoo = req.body;
+
   db.insert(zoo)
     .into("zoos")
-    .then(ids => res.status(201).json(ids))
+    .then(id => res.status(201).json(id))
+    .catch(err => res.status(500).json(err));
+});
+
+// GET by ID
+server.get("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("zoos")
+    .where({ id })
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(500).json(err));
+});
+
+// PUT
+
+server.put("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) res.status(400).json({ msg: "please enter a name" });
+
+  db("zoos")
+    .where({ id })
+    .update({ name })
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(500).json(err));
+});
+
+//DELETE
+
+server.delete("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("zoos")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
     .catch(err => res.status(500).json(err));
 });
 
