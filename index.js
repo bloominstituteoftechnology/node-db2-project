2722cd04043d37e83067795a4984d622fab221b3
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-11 16:19:28 -0700

    MVP

--- index.js
+++ index.js
@@ -1,12 +1,83 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+
+const dbConfig = require('./knexfile.js');
+
+const db = knex(dbConfig.development);
+
+
+server.get('/', (req, res) => {
+  res.send('API Running...');
+});
+
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos)})
+    .catch(fail => {
+      console.log('fail', fail)
+      res.status(500).json({message: 'Error getting the zoos'});
+  });
+  })
+
+  server.post('/api/zoos', (req, res) => {
+    const zoo = req.body;
+
+
+    db('zoos').insert(zoo)
+        .then((ids)=> { 
+          res.status(201).json(ids);
+        })
+                .catch((fail) => {
+                    console.log(fail);
+                    res.status(500).json({ error: "There was an error while saving the zoo to the database." });
+                });
+        })
+
+
+
+server.put(`/api/zoos/:id`, (req, res) => {
+
+  db('zoos').where({ id:req.params.id } ).update(req.body)
+  .then((zoo) => {
+      res.status(201).json(zoo);
+  })
+  .catch((fail) => {
+      console.log(fail);
+      res.status(404).json({ message: "The zoo with the specified ID does not exist."});
+  });
+
+
+})
+
+
+server.delete('/api/zoos/:id', (req, res) => {
+
+      db('zoos').where({ id:req.params.id }).delete()
+          .then((zoo) => {
+              res.status(201).json(zoo);
+          })
+          .catch((fail) => {
+              console.log(fail);
+              res.status(404).json({ message: "The zoo with the specified ID didn't delete."});
+          });
+
+      
+});
+
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
