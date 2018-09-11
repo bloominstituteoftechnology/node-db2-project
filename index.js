ddfa94d2b5dbca9b7754dc48f91a59ec611108f7
AI2EJAY <JHammett1314@gmail.com>
2018-09-11 14:33:59 -0500

    GET & POST Done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex')
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
-const dbConfig = require('./knexfile')
-const db = knex(dbConfig.development)
+const dbConfig = require("./knexfile");
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -11,9 +11,26 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-server.get('/', (req, res) => {
- res.send('api is running');
-})
+server.get("/", (req, res) => {
+  res.send("api is running");
+});
+//GET
+server.get("/api/zoos", (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// POST
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo)
+    .into("zoos")
+    .then(ids => res.status(201).json(ids))
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
