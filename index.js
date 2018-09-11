e706dd323eb7eb1e31250b37ee878884524f9518
JoCatherine Griggs <jocafrin@gmail.com>
2018-09-11 12:06:09 -0400

    added post and get methods

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const helmet = require('helmet');
 
-
+const knex = require('knex');
+const dbconfigure = require('./knexfile');
+const db = knex(dbconfigure.development);
+const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
 
+server.get('zoos', (req,res) => {
+  db('zoos').select('name').then(zoo => {
+    res.status(201).json(zoo);
+  })
+})
 
+server.post('/zoos', (req, res)=> {
+  const zoo = req.body;
+  db('zoos').insert(zoo).then(ids => {
+      res.status(201).json(ids);
+  })
+})
 
 
 
