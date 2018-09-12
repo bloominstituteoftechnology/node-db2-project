89e7cfde35ae442eff637b49371ba704a1232bc4
bri79319 <bri79319@gmail.com>
2018-09-11 23:58:47 -0400

    did some stretch assignment

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
 
+//=============zoos data table==================//
 const dataConfig = require("./knexfile");
-
 const dataBase = knex(dataConfig.development);
+//=============zoos data table==================//
+
+//==================bears data table================//
+const dataConfig2 = require("./knexfile2");
+const dataBase2 = knex(dataConfig2.development);
+//==================bears data table================//
 
 const server = express();
 
@@ -17,7 +23,7 @@ server.get("/", (req, res) => {
   res.send("API Is Running....");
 });
 
-//-------------------------MVP-----------------------//
+//-------------------------MVP-----------------------------------//
 server.get("/api/zoos", (req, res) => {
   dataBase("zoos")
     .then(names => {
@@ -88,11 +94,84 @@ server.delete("/api/zoos/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+//-------------------------MVP----------------------------------//
+
+//------------------------Stretch------------------------------//
+server.get("/api/bears", (req, res) => {
+  dataBase2("bears")
+    .then(bear => {
+      res.status(200).json(bear);
+    })
+    .catch(err => {
+      console.log("Error: ", err);
+      res.status(500).json({ Error: "Bears  cannot be retrieved" });
+    });
+  });
+
+  server.get("/api/bears/:id", (req, res) => {
+    const { id } = req.params;
+    dataBase2("bears")
+    .select("name")
+    .where({ id })
+    .then(bear => {
+      res.status(200).json(bear);
+    })
+    .catch(err => {
+      console.log("Error: ", err);
+      res
+        .status(500)
+        .json({ Error: "Bear Name with specified id cannot be retrieved" });
+      });
+    });
+
+    server.post("/api/bears", (req, res) => {
+      const bears = req.body;
+      
+      dataBase2
+      .insert(bears)
+      .into("bears")
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+        res.status(500).json({ Error: "Pass a bear name." });
+      });
+    });
+    
+    server.put("/api/bears/:id", (req, res) => {
+      const changes = req.body;
+      const { id } = req.params;
+      
+      dataBase2("bears")
+      .where({ id })
+      .update(changes)
+      .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+  });
+  
+  server.delete("/api/bears/:id", (req, res) => {
+    const { id } = req.params;
+    
+    dataBase2("bears")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+//------------------------Stretch------------------------------//
 
 server.listen(3300, () =>
   console.log(`\n=== Web API Listening on http://localhost:3300 ===\n`)
 );
-//-------------------------MVP-----------------------//
 
 // const port = 3300;
 // server.listen(port, function() {
