70a0d8a53c4d63f8af98201edceff27273096a4b
keegn <keegan.t.burkett@gmail.com>
2018-09-11 19:19:53 -0500

    working on endpoints

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
-const dbConfig = require('./knexfile.js');
+const dbConfig = require("./knexfile.js");
 
 const db = knex(dbConfig.development);
 
@@ -11,10 +11,33 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+// server sanity check
+server.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
 // endpoints here
-server.get('/', (req, res) => {
-  res.send('API Running...')
-})
+// post - create a new zoo in the zoos table
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+  // insert into zoos
+  db.insert(zoo)
+    .into("zoos")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// get - read the array of zoo objects
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .select("name")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
