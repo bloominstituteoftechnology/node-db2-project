cd72e0fe74f677efe8dfa9617db36c2823ffbd30
Noah-Amar <ne.amar@outlook.com>
2018-09-12 11:33:21 -0500

    push changes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require("knex");
+const dbConfig = require("./knexfile");
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +10,79 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get("/", (req, res) => {
+  res.send("API Running...");
+});
+ server.get("/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(201).json(zoos);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res
+        .status(500)
+        .json({ error: "The  zoos information could not be retrieved" });
+    });
+});
+ server.get("/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id: id })
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res
+        .status(500)
+        .json({ error: "The  zoos information could not be retrieved" });
+    });
+});
+ server.post("/zoos", (req, res) => {
+  const zoo = req.body;
+  if (zoo.name) {
+    db.insert(zoo)
+      .into("zoos")
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the zoo to the database"
+        });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide name for the zoo."
+    });
+  }
+});
+ server.put("/zoos/:id", (req, res) => {
+  const zoo = req.body;
+  const { id } = req.params;
+   db("zoos")
+    .where("id", "=", id)
+    .update(zoo)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+ server.delete("/zoos/:id", (req, res) => {
+  const { id } = req.params;
+   db("zoos")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
