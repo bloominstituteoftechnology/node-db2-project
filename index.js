eaca1ada0b19359d4f7b98f61e2583642107c34d
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-09 15:14:40 -0500

    Initial commit.  Added knex, db file, and sqlite3

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
 const server = express();
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get('/', (req, res) => {
+  res.json('Server is active!');
+});
 
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+const port = 6000;
+
+server.listen(port, () => {
+  console.log(`API is listening on port ${port}`);
 });
