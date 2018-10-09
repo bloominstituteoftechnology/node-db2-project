c747764dc1c702b7f2893c49c0a4af922d812b97
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-10-09 13:11:35 -0700

    the first POST and GET endpoints added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,8 +10,32 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send("It's Alive");
+});
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get('/api/zoos', (req, res) => {
+    db('zoos')
+	.then(zoos => {
+	    res.status(200).json(zoos);
+	})
+	.catch(err  => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
