bed4ddacb2054357fd019f6211c03065c4666963
Max <maxkajiwara@gmail.com>
2018-10-09 14:06:28 -0700

    Build server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+// DEPENDENCIES
 const express = require('express');
 const helmet = require('helmet');
 
+// ROUTES
+const zooRoutes = require('./zoos/zooRoutes.js');
+
+// SERVER
 const server = express();
 
+// MIDDLEWARE
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// ENDPOINTS
+server.use('/api/zoos', zooRoutes);
 
+// PORT
 const port = 3300;
 server.listen(port, function() {
 	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
