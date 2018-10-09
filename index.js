0efef5a1f44b3b4fcd811811d386c4fc8b335ee2
Andrew <ghobrial.andrew@gmail.com>
2018-10-09 15:35:50 -0400

    added knex requirements and knexfile.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
 
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
@@ -8,6 +11,7 @@ server.use(helmet());
 
 // endpoints here
 
+/////
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
