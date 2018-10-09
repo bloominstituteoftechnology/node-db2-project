b1a997c3c6b8755e4d2879ae576522fbfc2feb0d
Eva Li <evalee904@gmail.com>
2018-10-09 17:51:40 -0500

    GET by ID done.

--- index.js
+++ index.js
@@ -38,6 +38,35 @@ server.get('/api/zoos', (req, res) => {
     });
 });
 
+//GET by ID
+server.get('/api/zoos/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+
+    if(zoo) {
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ Message: 'Zoo not found.'});
+    }
+  } catch(err) {
+      res.status(500).json(err);
+    }
+});
+// server.get('/api/zoos/:id', (req, res) => {
+//   const { id } = req.params;
+//   db('zoos')
+//     .get(id)
+//     .then(zoo => {
+//       res.status(200).json(zoo);
+//     })
+//     .catch(err => {
+//       res.status(500).json(err);
+//     });
+// });
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
