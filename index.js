7c6d72e11fa90d42cee24db5ced268dba346f01a
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-09 18:28:33 -0400

    GET Zoos Endpoint**

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+/*########################################### GET ###########################################*/
+/************************ GET Zoo's ************************/
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
 
-const port = 9000;
+const port = 9001;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+  console.log(`\n=== Web API Listening on power (port) level ${port} ===\n`);
 });
