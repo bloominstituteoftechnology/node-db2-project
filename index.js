31533db653ba4a584433853d5fe1c68c47dbdbc8
Timothy Hoang <timh1203@gmail.com>
2018-10-09 16:10:04 -0400

    Add POST for zoos route

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ server.route('/')
       .catch(err => res.status(500).json(err))
   })
 
+server.route('/zoos')
+  .post((req, res) => {
+    const zoo = req.body
+    db.insert(zoo)
+      .into('zoos')
+      .then(zoos => res.status(201).json(zoos))
+      .catch(err => res.status(500).json(err))
+  })
+
 const port = 8000
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
