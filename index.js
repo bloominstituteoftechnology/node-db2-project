46cb05cde60b5717036a647065395575101c4ae1
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-09 14:15:14 -0600

    Connect database to index.js and implement Server Test READ endpoint to make sure that everything is configured correctly.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
+const knexConfig = require('./knexfile');
+const knex = require('knex');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const db = knex(knexConfig.development);
+
 // endpoints here
+
+// Server Test READ Endpoint
+server.get('/', (request, response) => {
+  response.send('Working');
+})
+
+
 
 const port = 3300;
 server.listen(port, function() {
