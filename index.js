d1bd6a472ea58d05f4d44b4aef744848b8a44c11
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-09 18:09:48 -0400

    Bears table add and endpoints added

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+////////////////
+// ZOOS
+////////////////
 server.get('/api/zoos', (req, res) => {
   db('zoos')
     .then((zoos) => {
@@ -92,6 +95,95 @@ server.delete('/api/zoos/:id', (req, res) => {
         });
       }
       res.status(200).send({ message: `zoo with ID ${id} was removed.` });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+////////////////
+// BEARS
+////////////////
+server.get('/api/bears', (req, res) => {
+  db('bears')
+    .then((bears) => {
+      res.status(200).json(bears);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+server.get('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  db('bears')
+    .where({ id })
+    .first()
+    .then((bear) => {
+      if (!bear) {
+        return res.status(404).send({
+          message: `The bear with the specified ID ${id} does not exist.`,
+        });
+      }
+      res.status(200).json(bear);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+server.post('/api/bears', (req, res) => {
+  const { name } = req.body;
+  const bear = { name };
+  if (!name) {
+    return res.status(400).send({
+      errorMessage: 'Please provide a name for the bear.',
+    });
+  }
+  db.insert(bear)
+    .into('bears')
+    .then((ids) => {
+      res.status(201).json(ids);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+server.put('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const newbear = { name };
+  if (!name) {
+    return res.status(400).send({
+      errorMessage: 'Please provide a name for the bear.',
+    });
+  }
+  db('bears')
+    .where({ id })
+    .update(newbear)
+    .then((count) => {
+      if (!count) {
+        res.status(404).json({
+          message: 'No bear found to update',
+        });
+      }
+      res.status(200).json({ message: `${count} record updated` });
+    });
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  db('bears')
+    .where({ id })
+    .del()
+    .then((remove) => {
+      if (!remove) {
+        return res.status(404).send({
+          message: `The bear with the specified ID ${id} does not exist.`,
+        });
+      }
+      res.status(200).send({ message: `bear with ID ${id} was removed.` });
     })
     .catch((err) => {
       res.status(500).json(err);
