5e5b4af72539f7c9edb7a6924283e326daceed06
decagondev <decagondevelopment@gmail.com>
2018-10-09 22:25:28 +0100

    (POST STRETCH REFACTORING) add get endpoint route

--- zoos/zooModel.js
+++ zoos/zooModel.js
@@ -11,3 +11,14 @@ const router = express.Router();
 // use statments
 router.use(express.json());
 router.use(helmet());
+
+// routed endpoints
+
+// get endpoint
+router.get("/", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
