925f0859dcd92a0bae91f735a51e80450400426b
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-09 18:54:34 -0400

    Added POST** lol wasn't seeing any data then realiezed why :)

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+
+/*########################################### POST ###########################################*/
+
+/************************ POST Zoo's ************************/
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 /*########################################### GET ###########################################*/
+
 /************************ GET Zoo's ************************/
 server.get('/api/zoos', (req, res) => {
   db('zoos')
