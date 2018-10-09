71d5a96f9806349a043f39452384adc5d6f011e3
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-09 13:09:25 -0700

    DELETE route works.

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ server.get('/api/zoos', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+
+server.get('/api/zoos/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+
+    if (zoo) {
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).send({ error: "Zoo id does not exist. Please provide a valid zoo id." });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // Add POST ROUTE HANDLER to create a zoo
 server.post('/api/zoos', (req, res) => {
   if (!req.body.name){
@@ -45,6 +64,24 @@ server.post('/api/zoos', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//Add DELETE ROUTE HANDLER to delete a zoo
+server.delete('/api/zoos/:id', async (req, res) => {
+  const { id } = req.params;
+  // const changes = req.body;
+
+  db('zoos')
+    .where ({ id })
+    .del()
+    .then(count => {
+      if (!count || count < 1) {
+        res.status(404).json({ message: 'No records found to delete'});
+      } else {
+        res.status(200).json(count);
+      }
+    })
+    .catch(err => res.status(500).json(err));
+  });
 
 const port = 3300;
 server.listen(port, function() {
