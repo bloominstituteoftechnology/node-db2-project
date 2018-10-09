99977f0e8e5941066979108e8390468bbe4b2806
decagondev <decagondevelopment@gmail.com>
2018-10-09 22:44:01 +0100

    (POST STRETCH REFACTORING) linked up the bearModel.js to the index.js refactor complete. i left the old code in as comments just for berevity :)

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const knexConfig = require("./knexfile");
 const db = knex(knexConfig.development);
 
 const zooModel = require("./zoos/zooModel.js");
+const bearModel = require("./bears/bearModel.js");
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use("/api/zoos", zooModel);
+server.use("/api/bears", bearModel);
 // endpoints here
 // zoo endpoints
 
@@ -85,73 +87,73 @@ server.use("/api/zoos", zooModel);
 
 // bear endpoints
 
-// get bears
-server.get("/api/bears", (req, res) => {
-  db("bears")
-    .then(bears => {
-      res.status(200).json(bears);
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // get bears
+// server.get("/api/bears", (req, res) => {
+//   db("bears")
+//     .then(bears => {
+//       res.status(200).json(bears);
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
-// get bear by id
-server.get("/api/bears/:id", (req, res) => {
-  db("bears")
-    .where({ id: req.params.id })
-    .first()
-    .then(bears => {
-      res.status(200).json(bears);
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // get bear by id
+// server.get("/api/bears/:id", (req, res) => {
+//   db("bears")
+//     .where({ id: req.params.id })
+//     .first()
+//     .then(bears => {
+//       res.status(200).json(bears);
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
-// post new bear
-server.post("/api/bears", (req, res) => {
-  const bear = req.body;
-  if (!bear) {
-    res.status(400).json({ message: "provide a bear name" });
-  }
-  db.insert(bear)
-    .into("bears")
-    .then(ids => {
-      res.status(201).json(ids);
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // post new bear
+// server.post("/api/bears", (req, res) => {
+//   const bear = req.body;
+//   if (!bear) {
+//     res.status(400).json({ message: "provide a bear name" });
+//   }
+//   db.insert(bear)
+//     .into("bears")
+//     .then(ids => {
+//       res.status(201).json(ids);
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
-// put (edit bear at id)
-server.put("/api/bears/:id", (req, res) => {
-  const bear = req.body;
-  console.log(bear);
-  db("bears")
-    .where({ id: req.params.id })
-    .update(bear)
-    .then(bear => {
-      if (bear) {
-        res.status(200).json({ message: "update completed success" });
-      } else {
-        res.status(404).json({ message: "there is no bear with this ID" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "update failiure" });
-    });
-});
+// // put (edit bear at id)
+// server.put("/api/bears/:id", (req, res) => {
+//   const bear = req.body;
+//   console.log(bear);
+//   db("bears")
+//     .where({ id: req.params.id })
+//     .update(bear)
+//     .then(bear => {
+//       if (bear) {
+//         res.status(200).json({ message: "update completed success" });
+//       } else {
+//         res.status(404).json({ message: "there is no bear with this ID" });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "update failiure" });
+//     });
+// });
 
-// delete bear at id
-server.delete("/api/bears/:id", (req, res) => {
-  db("bears")
-    .where({ id: req.params.id })
-    .del()
-    .then(count => {
-      if (count) {
-        res.status(204).end();
-      } else {
-        res.status(404).json({ message: "there is no bear with this ID" });
-      }
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // delete bear at id
+// server.delete("/api/bears/:id", (req, res) => {
+//   db("bears")
+//     .where({ id: req.params.id })
+//     .del()
+//     .then(count => {
+//       if (count) {
+//         res.status(204).end();
+//       } else {
+//         res.status(404).json({ message: "there is no bear with this ID" });
+//       }
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
 const port = 3300;
 server.listen(port, function() {
