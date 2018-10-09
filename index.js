49626dde4cafbeb95ec700cd283b9be47a1ba4f8
Timothy Hoang <timh1203@gmail.com>
2018-10-09 16:48:27 -0400

    Add DELETE for zoos route for specific id

--- index.js
+++ index.js
@@ -31,10 +31,21 @@ server.route('/api/zoos/:id')
     db('zoos')
       .where({ id })
       .then(zoo => {
-        if (!zoo || !zoo.length) return res.status(404).json({ error: 'The specified zoo could not be found.'})
+        if (!zoo || zoo < 1) return res.status(404).json({ error: 'The specified zoo could not be found.'})
         return res.status(200).json(zoo)
       })
       .catch(err => res.status(500).json({ error: 'Could not get the specified zoo' }))
+  })
+  .delete((req, res) => {
+    const { id } = req.params
+    db('zoos')
+      .where({ id })
+      .delete(id)
+      .then(deletedZoo => {
+        if (!deletedZoo || deletedZoo < 1) return res.status(404).json({ error: 'The specified zoo could not be found.' })
+        return res.status(202).json(deletedZoo)
+      })
+      .catch(err => res.status(500).json({ error: 'The specifid zoo could not be deleted.' }))
   })
 
 server.listen(port, function() {
