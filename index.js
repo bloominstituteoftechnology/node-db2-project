ebd3793ef38781bb779b2806f871c21f26020caa
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-10-09 12:14:41 -0700

    Initial dependency setup up, knex init, and modified knexfile.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.use(helmet());
 
 // endpoints here
 
-const port = 3300;
+const port = 4401;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
