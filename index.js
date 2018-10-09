966e2b2b37fcccf0afb8ad80015c92a939705a60
decagondev <decagondevelopment@gmail.com>
2018-10-09 20:40:11 +0100

    (MVP) - add get zoo by id endpoint

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ server.get("/api/zoos", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+server.get("/api/zoos/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .first()
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
