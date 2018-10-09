5f4aca2ea24669710c544316693495870822afb1
decagondev <decagondevelopment@gmail.com>
2018-10-09 20:41:48 +0100

    (MVP) - add post new zoo

--- index.js
+++ index.js
@@ -21,12 +21,27 @@ server.get("/api/zoos", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+// get zoo by id
 server.get("/api/zoos/:id", (req, res) => {
   db("zoos")
     .where({ id: req.params.id })
     .first()
     .then(zoos => {
       res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// post new zoo
+server.post("api/zoos/", (req, res) => {
+  const zoo = req.body;
+  if (!zoos) {
+    res.status(400).json({ message: "provide a zoo name" });
+  }
+  db.insert(zoo)
+    .into("zoos")
+    .then(ids => {
+      res.status(201).json(ids);
     })
     .catch(err => res.status(500).json(err));
 });
