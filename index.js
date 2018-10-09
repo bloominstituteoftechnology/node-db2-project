9d5005d3bb8238ceeafaf6665f08299fa6d934a4
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-09 17:02:30 -0400

    DELETE working successfully

--- index.js
+++ index.js
@@ -38,7 +38,17 @@ server.get('/api/zoos/:id', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-// 
+// delete a zoo
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({id})
+    .del()
+    .then(count => res.status(200).json(count))
+    .catch(err => res.status(500).json(err));
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
