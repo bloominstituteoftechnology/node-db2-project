c82e4bd47134ca2f97de68eccdf87d51db5df5d7
Timothy Hoang <timh1203@gmail.com>
2018-10-09 16:53:40 -0400

    Add PUT for zoos route with specific id

--- index.js
+++ index.js
@@ -36,6 +36,18 @@ server.route('/api/zoos/:id')
       })
       .catch(err => res.status(500).json({ error: 'Could not get the specified zoo' }))
   })
+  .put((req, res) => {
+    const { id } = req.params
+    const changedZoo = req.body
+    db('zoos')
+      .where({ id })
+      .update(changedZoo)
+      .then(updatedZoo => {
+        if (!updatedZoo || updatedZoo < 1) return res.status(404).json({ error: 'The specific zoo could not be found.' })
+        return res.status(200).json(updatedZoo)
+      })
+      .catch(err => res.status(500).json({ error: 'The specified zoo could not be updated.' }))
+  })
   .delete((req, res) => {
     const { id } = req.params
     db('zoos')
