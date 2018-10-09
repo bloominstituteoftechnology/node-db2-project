5267e3c531d853a1c0db548c0228728fe6b09b5e
ezra davis davis <ezra.rr.davis@gmail.com>
2018-10-09 15:44:07 -0700

    mvps

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex')
+
+const knexConfig = require('./knexfile.js')
+
+const db = knex(knexConfig.development)
+
 
 const server = express();
 
@@ -7,6 +13,58 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/api/zoos', (req,res) => {
+  db('zoos').then(zoos => {
+    res.json(zoos)
+  })
+})
+
+server.get('/api/zoos/:id', (req,res) => {
+  const {id} = req.params
+  db('zoos')
+  .where({id:id})
+  .then(zoo => {
+    res.json(zoo)
+  })
+})
+
+server.post('/api/zoos', (req,res) => {
+  const name = req.body;
+
+  db.insert(name)
+  .into('zoos')
+  .then(ids => {
+    res.status(201).json(ids[0]);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
+
+})
+
+server.put('/api/zoos/:id', (req,res) => {
+  const NewName = req.body;
+  const { id } = req.params;
+  db('zoos').where({id: id})
+  .update(NewName).then(count => {
+    res.status(200).json(count);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+})
+
+server.delete('/api/zoos/:id', (req,res) => {
+  const { id } = req.params;
+  db('zoos').where({id: id})
+  .del().then(count => {
+    res.status(200).json(count)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
 
 const port = 3300;
 server.listen(port, function() {
