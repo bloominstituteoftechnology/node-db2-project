8c16ca82495a775cb8682ceab0c116577999a109
Timothy Hoang <timh1203@gmail.com>
2018-10-09 15:55:49 -0400

    Add knex, sqlite3, db file

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require('express')
+const helmet = require('helmet')
+const knex = require('knex')
+const knexConfig = require('./knexfile.js')
+const server = express()
+const db = knex(knexConfig.development)
 
-const server = express();
+server.use(helmet())
+server.use(express.json())
 
-server.use(express.json());
-server.use(helmet());
+server.get('/', (req, res) => {
+  res.send("Server works!")
+})
 
-// endpoints here
-
-const port = 3300;
+const port = 8000
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
-});
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
+})
