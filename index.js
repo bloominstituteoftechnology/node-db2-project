279cb607dab57f641190ad50d22d5d505fb7354d
decagondev <decagondevelopment@gmail.com>
2018-10-09 22:35:06 +0100

    (POST STRETCH REFACTORING) fixed up and connected the zooModel in index.js

--- index.js
+++ index.js
@@ -5,81 +5,83 @@ const knex = require("knex");
 const knexConfig = require("./knexfile");
 
 const db = knex(knexConfig.development);
+
+const zooModel = require("./zoos/zooModel.js");
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
-
+server.use("/api/zoos", zooModel);
 // endpoints here
 // zoo endpoints
 
-// get zoos
-server.get("/api/zoos", (req, res) => {
-  db("zoos")
-    .then(zoos => {
-      res.status(200).json(zoos);
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // get zoos
+// server.get("/api/zoos", (req, res) => {
+//   db("zoos")
+//     .then(zoos => {
+//       res.status(200).json(zoos);
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
-// get zoo by id
-server.get("/api/zoos/:id", (req, res) => {
-  db("zoos")
-    .where({ id: req.params.id })
-    .first()
-    .then(zoos => {
-      res.status(200).json(zoos);
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // get zoo by id
+// server.get("/api/zoos/:id", (req, res) => {
+//   db("zoos")
+//     .where({ id: req.params.id })
+//     .first()
+//     .then(zoos => {
+//       res.status(200).json(zoos);
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
-// post new zoo
-server.post("/api/zoos", (req, res) => {
-  const zoo = req.body;
-  if (!zoo) {
-    res.status(400).json({ message: "provide a zoo name" });
-  }
-  db.insert(zoo)
-    .into("zoos")
-    .then(ids => {
-      res.status(201).json(ids);
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // post new zoo
+// server.post("/api/zoos", (req, res) => {
+//   const zoo = req.body;
+//   if (!zoo) {
+//     res.status(400).json({ message: "provide a zoo name" });
+//   }
+//   db.insert(zoo)
+//     .into("zoos")
+//     .then(ids => {
+//       res.status(201).json(ids);
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
-// put (edit zoo at id)
-server.put("/api/zoos/:id", (req, res) => {
-  const zoo = req.body;
-  console.log(zoo);
-  db("zoos")
-    .where({ id: req.params.id })
-    .update(zoo)
-    .then(zoo => {
-      if (zoo) {
-        res.status(200).json({ message: "update completed success" });
-      } else {
-        res.status(404).json({ message: "there is no zoo with this ID" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "update failiure" });
-    });
-});
+// // put (edit zoo at id)
+// server.put("/api/zoos/:id", (req, res) => {
+//   const zoo = req.body;
+//   console.log(zoo);
+//   db("zoos")
+//     .where({ id: req.params.id })
+//     .update(zoo)
+//     .then(zoo => {
+//       if (zoo) {
+//         res.status(200).json({ message: "update completed success" });
+//       } else {
+//         res.status(404).json({ message: "there is no zoo with this ID" });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "update failiure" });
+//     });
+// });
 
-// delete zoo at id
-server.delete("/api/zoos/:id", (req, res) => {
-  db("zoos")
-    .where({ id: req.params.id })
-    .del()
-    .then(count => {
-      if (count) {
-        res.status(204).end();
-      } else {
-        res.status(404).json({ message: "there is no zoo with this ID" });
-      }
-    })
-    .catch(err => res.status(500).json(err));
-});
+// // delete zoo at id
+// server.delete("/api/zoos/:id", (req, res) => {
+//   db("zoos")
+//     .where({ id: req.params.id })
+//     .del()
+//     .then(count => {
+//       if (count) {
+//         res.status(204).end();
+//       } else {
+//         res.status(404).json({ message: "there is no zoo with this ID" });
+//       }
+//     })
+//     .catch(err => res.status(500).json(err));
+// });
 
 // bear endpoints
 
--- zoos/zooModel.js
+++ zoos/zooModel.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
 
-const dbConfig = require("../knexfile");
+const knexConfig = require("../knexfile");
 
 const db = knex(knexConfig.development);
 
@@ -27,6 +27,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   db("zoos")
     .where({ id: req.params.id })
+    .first()
     .then(zoo => {
       res.status(200).json(zoo);
     })
@@ -80,3 +81,5 @@ router.delete("/:id", (req, res) => {
     })
     .catch(err => res.status(500).json(err));
 });
+
+module.exports = router;
