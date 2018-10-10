3df5b403eccde7f2d4a6e6ee27a0df62eab32d76
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-10-09 21:06:50 -0400

    wrapping up MVP

--- index.js
+++ index.js
@@ -29,30 +29,101 @@ server.use(
 
 ////////////+++zoo Routes++++/////////////////////////////
 
+server.get('/', (req, res) => {
+  res.send("Heyo");
+});
+
+
 server.post('/api/zoos', (req, res) => {
-  const { name } = req.body;
-  const newZoo = { name };
-  console.log(newZoo);
+  const name = req.body;
+  // const newZoo = { name };
+  console.log(name);
   zoos
-  .insert(newZoo).into("zoos")
+  .insert(name).into("zoos")
   .then(zooId => {
       const { id } = zooId;
-      res.status(201).json(zooId);
+      res.status(201).json(zooId)
       })
       .catch(err => res.status(500).json(err))
   });
   
-
-
 server.get('/api/zoos', (req, res) => {
-  zoos("zoos")
+  zoos('zoos')
   .then(zoos => {
-  res
-  .status(200)
-  .json(zoos)
+    res
+      .status(200)
+      .json(zoos)
   })
-  .catch(err => res.send(err))
+    .catch(err => res.send(err))
 });
+
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+
+    const { id } = req.params;
+    const zoo = await
+      zoos('zoos')
+        .where({ id })
+        .first()
+          if(zoo) {
+            res
+            .status(200)
+            .json(zoo)
+          }
+          else {
+          res
+          .status(404)
+          .json({ message: `Nuthin' Found with ID ${id}!`})
+          }
+  } catch(err) {
+      res
+      .status(500)
+      .json(err)
+  }
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+    zoos('zoos')
+    .where({ id })
+    .update(changes)
+    .then(count => { 
+      if(!count || count < 1) {
+        res
+        .status(404)
+        .json({message: `There is no record at ID ${id} to update. Please input a valid ID.`})
+      } else {
+        res
+        .status(200)
+        .json(count);
+      }
+  })
+  .catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+
+    zoos('zoos')
+    .where({ id })
+    .del()
+    .then(count => { 
+      if(!count || count < 1) {
+        res
+        .status(404)
+        .json({message: `There is no record at ID ${id} to delete. Please input a valid ID.`})
+      } else {
+        res
+        .status(200)
+        .json(count);
+      }
+  })
+  .catch(err => res.status(500).json(err));
+  
+});
+
 
 const port = 3300;
 server.listen(port, function() {
