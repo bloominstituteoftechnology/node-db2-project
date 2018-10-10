a20b6d4f32e77cec7e5cfd54b1f50ce4384b5bd1
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-10-10 08:28:22 -0700

    endpoints created. Put not working

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ server.use(helmet());
 // endpoints here:
 server.get('/api/zoos', (req, res) => {
   db
+      //.select().table('zoos') to get full table including IDs
       .select('name').from('zoos')
       .then(response => {
           res.status(200).json(response);
@@ -23,9 +24,16 @@ server.get('/api/zoos', (req, res) => {
       })
 });
 
+server.get('/api/zoos/:id', (req, res) =>  {
+  const {id} = req.params;
+  db('zoos')
+    .select("*").where("id", "=", id)
+    .update(updatedZoo)
+    .into('zoos')
+})
+
 server.post('/api/zoos', (req, res) => {
   const newZoo = req.body;
-
   db
       .insert(newZoo)
       .into('zoos')
@@ -36,6 +44,35 @@ server.post('/api/zoos', (req, res) => {
           res.status(500).json(err);
       })
 }); 
+
+server.put('/api/zoos/:id', (req, res) => {
+  const {id} = req.params;
+  const {name} = req.body;
+  db('zoos')
+    .where('id', '=', id)
+    .update({
+      name: name
+    })
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where('id', '=', id)
+    .del()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
 
 
 
