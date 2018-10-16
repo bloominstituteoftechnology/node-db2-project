bcbd50db0938f385fdf823e0c80fa5ea2bd801ce
lyang9 <lyang9@buffalo.edu>
2018-10-16 17:48:41 -0400

    Changed filename

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/lambda.sqlite3'
     },
     useNullAsDefault: true,
   },
