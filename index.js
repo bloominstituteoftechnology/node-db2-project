5599c1e5d0e7f8c6926c6c9c54ef001a87f17209
lyang9 <lyang9@buffalo.edu>
2018-10-16 17:29:45 -0400

    Added endpoints for get and post

--- index.js
+++ index.js
@@ -12,6 +12,35 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('It is working!');
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+server.post('/api/zoos', (req, res) => {
+  // grab data from body
+  const zoo = req.body;
+
+  // save data to database
+  db.insert(zoo)
+    .into('zoos')
+    .then(id => {
+      // return id of mewly created record
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
 
 const port = 3300;
 server.listen(port, function() {
