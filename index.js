beda079e4e6a0f604c06b0db99918f576c61f639
IlanL75 <anatoly.leytman@gmail.com>
2018-10-16 14:41:51 -0700

    MVP

--- index.js
+++ index.js
@@ -27,6 +27,62 @@ server.post('/api/zoos', (req,res) => {
   });
 });
 
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const zoo = await db('zoos')
+    .where({id})
+    .first();
+    if (zoo) {
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({message: 'Zoo not found'});
+    }
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const {id} = req.params;
+  const changes = req.body;
+  db('zoos').where({id}).update(changes)
+  .then(count => {
+    if(!count || count < 1) {
+      res.status(404).json({message: 'No records found to update'});
+    } else {
+      res.status(200).json(count);
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const {id} = req.params;
+  db('zoos').where({id}).delete()
+  .then(count => {
+    if(!count || count < 1) {
+      res.status(404).json({message: 'No records found to delete'});
+    } else {
+      res.status(200).json(count);
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
 
 
  
