2cccd95565292513b94d7d89c3c2472dba8475f6
Marious Davis <mlancelot88@gmail.com>
2018-10-16 16:12:01 -0500

    initial import of dependency routes

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const helmet = require('helmet');
-
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
 const server = express();
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
+server.get("/", (req, res) => {
+  res.send("Our server is connected!");
+});
+
 // endpoints here
 
-const port = 3300;
+const port = 3500;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
