41df6fa28bc39079cbc3281511a49faa01cb4a22
verydecent <wnjhwng@gmail.com>
2018-10-16 15:13:38 -0700

    adds Post/Get requests

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+// telling it to configure an object, which is our database
+const db = knex(knexConfig.development)
+
+
 const server = express();
 
 server.use(express.json());
@@ -11,8 +17,31 @@ server.use(helmet());
 // testing server
 server.get('/', (req, res) => {
   res.json("Yahoo was good for Japanese Market place")
-  
 })
+// get
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+})
+// create 
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db.insert(zoo).into('zoos')
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+})
+// update
+
+// delete
 
 const port = 3300;
 server.listen(port, function() {
