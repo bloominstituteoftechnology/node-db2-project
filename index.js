87eb12ad7dd992678cadaad358500e985a1eaa33
verydecent <wnjhwng@gmail.com>
2018-10-16 14:33:17 -0700

    tests server

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ server.use(helmet());
 
 // endpoints here
 
+// testing server
+server.get('/', (req, res) => {
+  res.json("Yahoo was good for Japanese Market place")
+  
+})
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+  console.log(`\n=== Web API Listening on PORT ${port} ===\n`);
 });
