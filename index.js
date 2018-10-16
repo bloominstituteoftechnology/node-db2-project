ec29b51fad872a674131a58b1464debd7797a0f7
Ray Lomeli <raylom96@gmail.com>
2018-10-16 18:01:35 -0500

    added post and delete

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+//endpoints here
+
+//sanity check endpoint
+server.get("/", (req, res) => {
+  res.send("It's Alive");
+});
+
+// Get All Zoos
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+//create zoo POST request
+server.post("/api/zoos", (req, res) => {
+  const name = req.body;
+
+  db.insert(name)
+    .into("zoos")
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// delete zoo DELETE request
 
 const port = 3300;
 server.listen(port, function() {
