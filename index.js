a60c42651e13abe81a929b804580e28093f3d2a3
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-10-16 17:51:12 -0500

    set up server w knex

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js')
+const db = knex(knexConfig.development)
 
 const server = express();
 
@@ -12,3 +15,7 @@ const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+server.get('/', (req, res) => {
+  res.json('We got it active');
+})
