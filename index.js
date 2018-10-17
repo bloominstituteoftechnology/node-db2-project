dcaed4c89264c01841d81b592b66dc9f3c17a062
Sasha <taylork85@gmail.com>
2018-10-16 20:03:09 -0500

    PUT AND DELETE ROUTES FUNCTION -index.js

--- index.js
+++ index.js
@@ -12,12 +12,25 @@ server.use(helmet());
 
 server.get('/', (req,res) =>{
   res.status(200).send("You have reached the zoo api")
+
 })
 
 server.get('/api/zoos', (req, res)=>{
   db('zoos')
   .then(zoos => {
     res.status(200).json(zoos)
+  })
+  .catch(err => {
+    res.send(err.message)
+  })
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const zooId = { id };
+   db('zoos').where(zooId).first()
+   .then(zoo => {
+  res.status(200).json(zoo)
   })
   .catch(err => {
     res.send(err.message)
@@ -33,6 +46,32 @@ server.post('/api/zoos', (req, res) =>{
     })
     .catch(err => res.send(err.message))
 })
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const zooId = { id };
+  const editedZoo = req.body;
+  db('zoos')
+  .where(zooId)
+  .update(editedZoo)
+  .then(zoo => {
+    res.status(200).json(zoo)
+  })
+  .catch(err => res.send(err.message))
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const zooId = { id };
+  db('zoos')
+  .where(zooId)
+  .del()
+  .then( removed => {
+    res.status(202).json(removed)
+  })
+  .catch(err => res.send(err.message))
+})
+
 
 const port = 3300;
 server.listen(port, function() {
