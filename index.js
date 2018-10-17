973d0d6715dd0a4b7b969dc284b3cb22628d8b97
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-17 16:23:10 -0500

    routes for bears and endpoints included in index

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const zoosRoutes = require('./zoosRoutes/zoosRoutes.js')
+const bearsRoutes = require('./bearsRoutes/bearsRoutes.js')
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.use('/api/zoos', zoosRoutes)
+server.use('/api/bears', bearsRoutes)
 
 const port = 3300;
 server.listen(port, function() {
