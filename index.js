cd3a1361900a695ff0e3019b58c33944fe44cb30
Lee Len Formento <leebatef@gmail.com>
2018-11-11 19:01:42 -0500

    Setting up

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get('/', (req, res) => {
+  res.json('Home');
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
