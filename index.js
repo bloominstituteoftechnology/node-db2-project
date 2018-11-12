938b78690e31827850f544817b6264e0af85ca30
Lee Len Formento <leebatef@gmail.com>
2018-11-12 11:52:08 -0500

    Get ID

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ server.get('/', (req, res) => {
 
 server.get('/api/animals', (req, res) => {
   db('zoos')
-  .select('name')
   .then(names => {
     console.log(names)
     res.status(200).json(names)
@@ -26,19 +25,31 @@ server.get('/api/animals', (req, res) => {
   });
 });
 
+server.get('/api/animals/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .first()
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 server.post('/api/animals', (req, res) => {
   const name = req.body;
    db
-   .insert(name)
+   .insert(name) 
    .into('zoos')
   .then(name => {
-    res.status(200).json(name)
+    res.status(200).json(name[0])
   })
   .catch(err => {
     res.status(500).json(err)
   });
 })
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
