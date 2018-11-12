db7b9ac7830d8838f57a868b0810d40ca37d09bc
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-11-12 14:38:55 -0600

    added put and delete functionality

--- zoos.js
+++ zoos.js
@@ -18,4 +18,24 @@ router.post('/', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+router.put('/:id', (req, res) => {
+  const zoo = req.body;
+  const { id } = req.params;
+  db('zoos')
+    .where({ id: id })
+    .update(zoo)
+    .then(ids => res.status(201).json(ids))
+    .catch(err => res.status(500).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('zoos')
+    .where({ id: id })
+    .del()
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(500).json({ message: 'No user on that id' }));
+});
+
 module.exports = router;
