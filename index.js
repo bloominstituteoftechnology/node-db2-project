9b4c3cf0c254529de351025692f56030fd8c2fcb
Drewsup123c <drewsup123c@gmail.com>
2018-11-13 14:23:41 -0700

    fixed knexfile not finding sqlite3 file and added the post feature

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ server.get('/', (req, res) => {
   res.json({ api: 'tis running properly' });
 });
 
+server.post('/api/zoos', (req, res) => {
+  const newZoo = req.body;
+  db('zoos')
+    .insert(newZoo)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error inserting zoo ::', err });
+    });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
--- knexfile.js
+++ knexfile.js
@@ -6,8 +6,8 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       // filename: './dev.sqlite3'
-      filename : '/data/lambda.sqlite3',
-    }
+      filename : './data/lambda.sqlite3',
+    },
+    useNullasDefault : true,
   },
-useNullasDefault : true,
 };
