39df423cc19074780464f3a0d3b77ba8ac1b2409
AM_Myrick <amyrick.820@gmail.com>
2018-11-13 15:27:29 -0500

    creates knexfile, get, and post endpoints

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require("knex");
+const knexConfig = require("./knexfile")
+
+const db = knex(knexConfig.development)
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json({error: err}))
+})
 
-const port = 3300;
+server.get("/api/zoos/:id", (req, res) => {
+const {id} = req.params;
+
+  db("zoos")
+    .where({id})
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json({error: err}))
+})
+
+server.post("/api/zoos", (req, res) => {
+  const changes = req.body;
+  console.log(changes.name)
+
+  if (changes.name === "" || changes.name === undefined) {
+    return res.status(400).json({error: "Please make sure the zoo name is indexed."})
+  }
+
+  db("zoos")
+    .insert(changes)
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(500).json({errorMessage: err}))
+})
+
+const port = 9001;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
