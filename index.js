8c4e714df86644a7b872c75204edfe5e52137f03
Brandon Vilaychith <blvilaychith@outlook.com>
2018-11-13 13:57:27 -0600

    File set up

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
