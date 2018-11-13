83035c65884e660ec2f803e22703a77b6c41f73d
Matthew Wright <matthewbwright@gmail.com>
2018-11-13 13:51:48 -0700

    adds sanity check endpoint

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ server.use(helmet());
 
 // endpoints here
 
+//sanity check
+server.get('/', (req, res) => {
+  res.json({ api: "let's goooooo" });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
