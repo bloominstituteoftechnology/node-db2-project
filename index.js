6dc1a89e50850c2f4cd33cc510880c723d920331
Katie Cleary <katiecleary@airmaster.lan>
2018-11-13 16:39:26 -0500

    initial file setup

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.get('/', (req, res) => {
+  res.json({ api: 'up' });
+});
 
 // endpoints here
 
