db14b683779d7a7288ce6b07de053443a341efcb
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-13 13:36:19 -0700

    Chad Jemmett db-zoo

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development)
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.get('/', (req, res) => {
+  db('zoos').then(zoos => res.status(200).json(zoos))
+    .catch(err  => res.status(500).json(err))
+})
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db('zoos')
+    .insert(zoo)
+    // .returning('id')
+    .then(id => res.status(201).json(id))
+})
 
 // endpoints here
 
