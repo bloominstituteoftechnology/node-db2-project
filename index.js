33241b49be86bc5abde70724ba84e83f240aeed5
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-13 15:09:31 -0700

    get all zoos and get zoo by ID

--- index.js
+++ index.js
@@ -27,7 +27,27 @@ server.post('/api/zoos', (req, res) => {
     });
 });
 
+//====GET ALL ZOOS====
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err));
+});
 
+//====GET ZOO BY ID====
+server.get('/api/zoos/:id', (req, res) => {
+  let { id } = req.params;
+  // id = id - 1;
+  db('zoos').where({id}).then(zoo => {
+    if (zoo.id !== 0) {
+      res.status(200).json(zoo)
+    } else {
+      res.status(404).json({ message: "The zoo with the specified id does not exist." });
+    }
+  }).catch(error => {
+    res.status(500).json({ error: "Cant get zoo data." });
+  });
+});
 
 
 const port = 3300;
