7419a59afbbb1b810757214e4047711f843a8844
Rahul Desai <rd825@georgetown.edu>
2018-11-13 14:03:49 -0500

    Started up project

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const knex = require('knex')
 const helmet = require('helmet');
-
 const server = express();
+
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req,res) => res.send({"API": "live"}));
 
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
-});
+
+const port = process.env.PORT || 9000;
+server.listen(port, function() {console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)});
