b61e764784a0a0244460cfd94b94dd9005150116
David247365 <ds051128@gmail.com>
2018-11-13 15:47:04 -0600

    got post and get working

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -8,8 +13,23 @@ server.use(helmet());
 
 // endpoints here
 
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+
+  db("zoos")
+    .insert(zoo)
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "There was an error posting to zoos" });
+    });
+});
+
 server.get("/api/zoos", (req, res) => {
-  res.json({ message: "Im Here" });
+  db("zoos").then(zoos => {
+    res.status(200).json(zoos);
+  });
 });
 
 const port = 3300;
