62d0ab6570e5d71a6e3bf35eaf80897802ee424b
Brian G <brian.gregorius@gmail.com>
2018-11-13 16:08:59 -0500

    endpoints put, get, post, and get by id working

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -8,7 +13,53 @@ server.use(helmet());
 
 // endpoints here
 
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json({ err }))
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { zooId } = req.params;
+  db('zoos')
+    .where({ id: zooId })
+    .then(zoo => {
+      res.status(200).json(zoo)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+});
+
+server.put('/api/zoos/:id', (req, res) =>{
+  const changes = req.body;
+  const { id } = req.params;
+  db('zoos')
+    .where({id })
+    .update(changes)
+    .then(count =>{
+      res.status(200).json(count)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+});
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db('zoos')
+    .insert(zoo)
+    // .returning('id')
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => {
+      res.status(500).json({Message: 'Error inserting', err})
+    })
+});
+
 const port = 3300;
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
