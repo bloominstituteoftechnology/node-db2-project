d0ce69006af0b89276086febe3a416180ed75fee
MEBonham <programmer@mckaybonham.com>
2018-11-13 15:38:31 -0700

    Added bears table and corresponding CRUD operations

--- index.js
+++ index.js
@@ -22,11 +22,25 @@ server.get("/api/zoos", (req, res) => {
     .catch(err => res.status(500).json({ error: err }));
 });
 
+server.get("/api/bears", (req, res) => {
+  db("bears")
+    .then(bears => res.status(200).json(bears))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
 server.get("/api/zoos/:id", (req, res) => {
   const zooId = req.params.id;
   db("zoos")
     .where({ id: zooId })
     .then(zooInfo => res.status(200).json(zooInfo))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+server.get("/api/bears/:id", (req, res) => {
+  const bearId = req.params.id;
+  db("bears")
+    .where({ id: bearId })
+    .then(bearInfo => res.status(200).json(bearInfo))
     .catch(err => res.status(500).json({ error: err }));
 });
 
@@ -46,6 +60,22 @@ server.post("/api/zoos", (req, res) => {
   }
 });
 
+server.post("/api/bears", (req, res) => {
+  const bear = req.body;
+  if (bear.name) {
+    db("bears")
+      .insert(bear)
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        res.status(500).json({ error: err })
+      });
+  } else {
+    res.status(400).json({ error: "Please provide a valid name of the zoo." });
+  }
+});
+
 server.put("/api/zoos/:id", (req, res) => {
   const changes = req.body;
   const zooId = req.params.id;
@@ -54,9 +84,27 @@ server.put("/api/zoos/:id", (req, res) => {
     .update(changes)
     .then(count => {
       if (count) {
-        res.status(200).json(count);
+        res.status(200).json({ count });
       } else {
         res.status(404).json({ error: "The zoo with the provided ID was not found." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+server.put("/api/bears/:id", (req, res) => {
+  const changes = req.body;
+  const bearId = req.params.id;
+  db("bears")
+    .where({ id: bearId })
+    .update(changes)
+    .then(count => {
+      if (count) {
+        res.status(200).json({ count });
+      } else {
+        res.status(404).json({ error: "The bear with the provided ID was not found." });
       }
     })
     .catch(err => {
@@ -74,6 +122,21 @@ server.delete('/api/zoos/:id', (req, res) => {
         res.status(200).json({ count });
       } else {
         res.status(404).json({ error: "The zoo with the provided ID was not found." });
+      }
+    })
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  const bearId = req.params.id;
+  db("bears")
+    .where({ id: bearId })
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json({ count });
+      } else {
+        res.status(404).json({ error: "The bear with the provided ID was not found." });
       }
     })
     .catch(err => res.status(500).json({ error: err }));
