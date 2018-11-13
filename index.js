0fdca32378436256a9d9681cc1e5056d2f74feac
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-13 13:19:12 -0800

    added get zoo by id

--- index.js
+++ index.js
@@ -17,24 +17,41 @@ server.get('/', (req, res) => {
   res.json({ api: 'up' });
 });
 
+// get all zoos
 server.get('/api/zoos', (req, res) => {
   db('zoos')
     .then(zoo => res.status(200).json(zoo))
     .catch(err => res.status(500).json(err));
 });
 
+// add a new zoo
 server.post('/api/zoos', (req, res) => {
   const zoo = req.body;
 
   db('zoos')
     .insert(zoo)
-    .returning('id')
-    .then(ids => {
-      res.status(201).json(ids);
+    .then(id => {
+      res.status(201).json(id);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error inserting', err });
+      console.log(err)
+      res.status(500).json({ error: "Cannot post zoo" });
     });
+});
+
+//get zoo by id
+server.get('/api/zoos/:id', (req, res) => {
+  let { id } = req.params;
+  // id = id - 1;
+  db('zoos').where({id}).then(zoo => {
+    if (zoo.id !== 0) {
+      res.status(200).json(zoo)
+    } else {
+      res.status(404).json({ message: "The zoo with the specified id does not exist." });
+    }
+  }).catch(error => {
+    res.status(500).json({ error: "Cant get zoo data." });
+  });
 });
 
 const port = 3300;
