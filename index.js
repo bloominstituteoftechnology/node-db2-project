ea5ff610fe1408dd241f1975a0a3712920a30901
Brad Mortensen <b.mort86@gmail.com>
2018-11-13 13:56:30 -0700

    finished stretch

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ server.get('/api/zoos', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-server.get('/api/zoos/:id', (req, res) => {
-  const { id } = req.params;
+server.get('/api/zoos/:zooid', (req, res) => {
+  const { zooid } = req.params;
   db('zoos')
     .then(zoos => res.status(200).json(zoos))
     .catch(err => res.status(500).json(err));
@@ -72,6 +72,73 @@ server.delete('/api/zoos/:zooid', (req, res) => {
     })
     .catch(err => res.status(500).json(err));
 });
+
+//BEARS!!! RAWR!!!!
+
+
+server.get('/api/bears', (req, res) => {
+  db('bears')
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get('/api/bears/:bearid', (req, res) => {
+  const { bearid } = req.params;
+  db('bears')
+    .then(bears => res.status(200).json(bears))
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/api/bears', (req, res) => {
+  const bear = req.body;
+
+  db('bears')
+    .insert(bear)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error inserting', err });
+    });
+});
+
+server.get('/api/bears/:bearid', (req, res) => {
+  const { bearid } = req.params;
+
+  db('bears')
+    .where({ id: bearid })
+    .then(bear => {
+      res.status(201).json({ bear });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put('/api/bears/:bearid', (req, res) => {
+  const changes = req.body;
+  const { bearid } = req.params;
+
+  db('bears')
+    .where({ id: beari })
+    .update(changes)
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+
+server.delete('/api/bears/:bearid', (req, res) => {
+  const { bearid } = req.params;
+
+  db('bears')
+    .where({ id: bearid })
+    .del()
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 
 const port = 3300;
 server.listen(port, function() {
