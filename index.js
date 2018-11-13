ddfe233eebf479e53a0d8c5b1c8d8222f086fc6f
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-13 15:51:15 -0600

    Get and post

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ server.use(helmet());
 
 // endpoints here
 
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db('zoos')
+    .insert(zoo)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error creating zoo', err });
+    });
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get('/', (req, res) => {
+  res.json({ api: 'running' });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
