9efa14d1739b5ccca732b36313681daba02428b8
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-13 12:26:40 -0800

    setup repo and database

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +12,30 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/', (req, res) => {
+  res.json({ api: 'up' });
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db('zoos')
+    .insert(zoo)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error inserting', err });
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
