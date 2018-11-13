74e6f831129f4389f533fe1fd3dad99818dc9290
Riley-Brown <riley@riley.gg>
2018-11-13 16:59:34 -0500

    fixed git ignore

--- api/zooRoute.js
+++ api/zooRoute.js
@@ -5,6 +5,8 @@ const knexConfig = require('../knexfile')
 const db = knex(knexConfig.development)
 const router = express.Router();
 
+
+/* ---- GET ALL ZOOS----  */
 router.get('/', (req, res) => {
   db('zoos')
     .then(zoos => res.status(200).json(zoos))
@@ -12,6 +14,7 @@ router.get('/', (req, res) => {
 
 })
 
+/* ---- ADD NEW ZOO ----  */
 router.post('/', (req, res) => {
   const zoo = req.body
   db('zoos')
@@ -28,4 +31,20 @@ router.post('/', (req, res) => {
     })
 })
 
+/* ---- EDIT ZOO ----  */
+router.post('/:id', (req, res) => {
+  const id = req.params.id
+  db('zoos')
+    .insert(zoo)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Error inserting',
+        err
+      })
+    })
+})
 module.exports = router;
