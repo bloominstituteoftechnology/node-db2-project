a58ae6cdd8153e0eba663150d2c5d4747a29573f
Brandon Vilaychith <blvilaychith@outlook.com>
2018-11-13 14:18:18 -0600

    GET and POST finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 const knexConfig = require('./knexfile.js');
 
 const db = knex(knexConfig.development);
@@ -10,6 +11,37 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// TEST
+server.get('/', (req, res) => {
+  res.json({api: 'up'});
+});
+
+// POST
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db('zoos')
+    .insert(zoo)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({message: 'There was an error adding the zoo.', error});
+    });
+});
+
+// GET
+server.get('/api/zoos', (req, res) => {
+	db('zoos')
+	.then(zoos => {
+	  res.status(200).json(zoos);
+	})
+	.catch(error => {
+    res.status(500).json({message: 'There was an error getting the zoos.', error})
+	})
+})
 
 const port = 3300;
 server.listen(port, function() {
--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+			filename: './data/lambda.sqlite3'
     },
-		setNullAsDefault: true,
+		useNullAsDefault: true,
   },
 };
