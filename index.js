bdba649d91afa9b2adc623239c4a6fe6c2d14d4d
lola <lev@lolas-air.lan>
2018-11-13 16:51:22 -0500

    MVP complete: Zoos CRUD

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
 
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
@@ -8,7 +11,48 @@ server.use(helmet());
 
 // endpoints here
 
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json(err))
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params
+  db('zoos')
+    .where({ id })
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err))
+})
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body
+  db('zoos').insert(zoo).then(id => {
+    res.status(201).json(id)
+  }).catch(err => res.status(500).json({ message: "error"}))
+})
+
+server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body
+  const { id } = req.params
+
+  db('zoos')
+    .where({ id })
+    .update(changes)
+    .then(count => res.status(200).json(count))
+    .catch(err => res.status(500).json(err))
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params
+  db('zoos')
+    .where({ id })
+    .del(id)
+    .then(count => res.status(200).json(count))
+    .catch(err => res.status(500).json(err))
+})
+
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
 });
