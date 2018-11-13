2ac6851d06b1c0736292dc8a4b9299ad3041be1d
sean-one <basement365@gmail.com>
2018-11-13 13:52:17 -0800

    set up and server up and running

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development)
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json({ zoos })
+    })
+    .catch(err => console.log(err))
+})
+
 
 const port = 3300;
 server.listen(port, function() {
