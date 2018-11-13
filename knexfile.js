f412c96a26b705c6348367a04004e571e6f5c8ba
Drewsup123c <drewsup123c@gmail.com>
2018-11-13 13:41:53 -0700

    changed knex connection to custom endpoint

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      // filename: './dev.sqlite3'
+      filename : '/data/lambda.sqlite3',
     }
   },
-useNullasDefault : true;
+useNullasDefault : true,
 };
