09cf1b87580a83e30189c0436145bf48817c6eb7
Gannon <gkleblanc@gmail.com>
2018-11-13 15:51:14 -0500

    aded knexfile and configurations. Next going to add the endpoints

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+//bringing in dependencies
 const express = require('express');
 const helmet = require('helmet');
 
+//bringing in Knex to connect and access database
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+
+
+//making the server use the dependencies we brought in
 const server = express();
 
 server.use(express.json());
@@ -8,7 +16,8 @@ server.use(helmet());
 
 // endpoints here
 
+
 const port = 3300;
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
