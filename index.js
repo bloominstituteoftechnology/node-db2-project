5d4d40225018cd78579a3e2ce1660b9f3aba96b6
bkwillia052 <bkwillia052@gmail.com>
2018-11-13 14:25:06 -0600

    Knex configured and get/post requests functional

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -8,7 +11,50 @@ server.use(helmet());
 
 // endpoints here
 
+ server.get('/api/zoos', (req, res) => {
+  db('zoos').select().then(r => res.status(200).json(r)).catch(err => res.status(500).json({message: 'An error occurred while retrieving the data.'}))
+});
+
+ /*  server.get('/api/zoos/:id', async (req, res){
+
+}); */
+
+ server.post('/api/zoos', (req, res) => {
+   const zoo = req.body;
+  db('zoos').insert(zoo).then(zoo => res.status(200).json(zoo))
+  .catch(err => res.status(500).json({message: 'An error occurred while retrieving the data.'}))
+});
+/*
+ server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body;
+  const {id} = req.params;
+
+  db('zoos')
+  .where({id})  // or where({id: id})
+  .update(changes)
+  .then(count => {
+    res.status(200).json({count})
+  })
+  .catch(err => res.staus(500).json(err));
+});
+
+ server.delete('/api/zoos/:id', (req, res) => {
+
+  const {id} = req.params;
+
+  db('zoos')
+  .where({id})  // or where({id: id})
+  .del()
+  .then(count => {
+    res.status(200).json({count})
+  })
+  .catch(err => res.staus(500).json(err));
+}); */
+
+
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
