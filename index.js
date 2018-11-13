775fe5b225459a09abca5b1cc6f0ff86c1b9d858
Edward J <edjeong99@gmail.com>
2018-11-13 16:29:56 -0500

    init include dependecies

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+
 const server = express();
 
 server.use(express.json());
