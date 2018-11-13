de9fb36d170fee959175d5420f3c6d2c856c841d
Katie Cleary <katiecleary@airmaster.lan>
2018-11-13 16:46:32 -0500

    inserted basic code structure for project

--- index.js
+++ index.js
@@ -12,7 +12,60 @@ server.get('/', (req, res) => {
   res.json({ api: 'up' });
 });
 
-// endpoints here
+// ___________ GET _______________
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(students => res.status(200).json(students))
+    .catch(err => res.status(500).json(err));
+});
+
+/*
+// ___________ POST _______________
+server.post('/api/zoos', (req, res) => {
+  const student = req.body;
+
+  db('zoos')
+    .insert(student)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error inserting', err });
+    });
+});
+
+// ___________ PUT ______________
+
+server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body;
+  const { studentid } = req.params;
+
+  db('zoos')
+    .where({ id: studentid })
+    .update(changes)
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// ___________ DELETE _______________
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { studentid } = req.params;
+
+  db('zoos')
+    .where({ id: studentid })
+    .del()
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+*/
 
 const port = 3300;
 server.listen(port, function() {
