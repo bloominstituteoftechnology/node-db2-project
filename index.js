5c70f17df8cbec77e2692a25ad91e4366d8d8108
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-13 13:56:31 -0800

    finished adding crud for bears

--- index.js
+++ index.js
@@ -80,6 +80,71 @@ server.put('/api/zoos/:zooid', (req, res) => {
     .catch(error => res.status(500).json(error));
 })
 
+// Bears
+
+// get all zoos
+server.get('/api/bears', (req, res) => {
+  db('bears')
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err));
+});
+
+// add a new bear
+server.post('/api/bears', (req, res) => {
+  const bear = req.body;
+
+  db('bears')
+    .insert(bear)
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "Cannot post zoo" });
+    });
+});
+
+//get bear by id
+server.get('/api/bears/:id', (req, res) => {
+  let { id } = req.params;
+
+  db('bears').where({id}).then(bear => {
+    if (bear.id !== 0) {
+      res.status(200).json(bear)
+    } else {
+      res.status(404).json({ message: "The bear with the specified id does not exist." });
+    }
+  }).catch(error => {
+    res.status(500).json({ error: "Cant get bears data." });
+  });
+});
+
+// delete bear by id
+server.delete('/api/bears/:bearid', (req, res) => {
+  const { bearid } = req.params;
+
+  db('bears')
+    .where({ id: bearid })
+    .del()
+    .then(count => {
+      res.status(200).json({ count })
+    })
+    .catch(error => res.status(500).json(error));
+})
+
+server.put('/api/bears/:bearid', (req, res) => {
+  const changes = req.body;
+  const { bearid } = req.params;
+
+  db('bears')
+    .where({ id: bearid })
+    .update(changes)
+    .then(count => {
+      res.status(200).json({ count })
+    })
+    .catch(error => res.status(500).json(error));
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
