3f66015fd931b0f960d556470bf7a357bb311be6
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-13 15:07:49 -0700

    setup repo and added a post function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +12,23 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+//====POST====
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db('zoos')
+    .insert(zoo)
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "Cannot post zoo" });
+    });
+});
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
