d92912e9ea6e7aeb769c52e28b9962c6999fa270
MEBonham <programmer@mckaybonham.com>
2018-11-13 12:09:21 -0700

    Dependencies; followed along wtih lecture

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,8 +12,45 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
 
-const port = 3300;
+server.get("/api/students", (req, res) => {
+  db("students")
+    .then(students => res.status(200).json(students))
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/api/students", (req, res) => {
+  const student = req.body;
+
+  db("students")
+    .insert(student)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err })
+    });
+});
+
+server.put("/api/students/:id", (req, res) => {
+  const changes = req.body;
+  const { studentId } = req.params;
+
+  db("students")
+    .where({ id: studentId })
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+const port = 9000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
