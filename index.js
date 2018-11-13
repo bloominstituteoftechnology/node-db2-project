427dd9227aa9cd94342ac4ed5313e029edf247b0
KHollobaugh <khollobaugh@gmail.com>
2018-11-13 13:54:24 -0500

    read and update funcitonality

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 const knexConfig = require('./knexfile');
-const db = kmex(knexConfig.development);
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -13,6 +14,24 @@ server.use(helmet());
 server.get('/', (req, res) => {
   res.json({ api: 'up' });
 });
+
+server.get('/api/animals', (req, res) => {
+  db('zoos')
+  .then(zoos => res.status(200).json(zoos))
+  .catch(error => res.status(500).json({message: 'error getting names'}))
+})
+
+server.post('/api/animals', (req, res) => {
+  const name = req.body;
+  db('zoos')
+  .insert(name)
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => res.status(500).json({message: 'Error adding name'}))
+})
+
+
 
 
 const port = 3300;
