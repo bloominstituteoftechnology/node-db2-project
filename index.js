b6d51b4812ad7ab211fff47a0d43d4fb5a744078
Lajawanti <lajawanti.dhake@gmail.com>
2018-11-13 16:06:33 -0500

    MVP Done

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
 
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+//GET..
+server.get('/', (request, response) => {
+    response.send('ACTIVE...');
+    //response.json('ACTIVE...');
+})
+
+//GET../api/zoos
+server.get('/api/zoos', (request, response) => {
+    db('zoos')
+            .then(zoos => response.status(200).json(zoos))
+            .catch(error => response.status(500).json(error));
+})
+
+//GET../api/zoos:id
+server.get('/api/zoos/:id', (request, response) => {
+    db('zoos')
+            .where({ id : request.params.id })
+            .then(zoo => response.status(200).json(zoo))
+            .catch(error => response.status(500).json(error));
+})
+
+//POST ../api/zoos
+server.post('/api/zoos', (request, response) => {
+          db.insert(request.body)
+            .into('zoos')
+            .then(zoos => response.status(200).json(zoos))
+            .catch(error => response.status(500).json(error));
+})
+
+//PUT 
+server.put('/api/zoos/:id', (request, response) => {
+  db('zoos')
+        .where({ id : request.params.id })
+        .update(request.body)
+        .then(zoo => response.status(200).json(zoo))
+        .catch(error => response.status(500).json(error));
+})
+
+//DELETE 
+server.delete('/api/zoos/:id', (request, response) => {
+  db('zoos')
+        .where({ id : request.params.id })
+        .del()
+        .then(zoo => response.status(200).json(zoo))
+        .catch(error => response.status(500).json(error));
+})
 
 const port = 3300;
 server.listen(port, function() {
