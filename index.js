b9fda63e15d2a934cdac0df99772d06660a72640
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-14 08:11:12 -0600

    updates

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +10,75 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+            .then( zoos => res.status(200).json(zoos))
+            .catch( error => res.status(500).json({ message: 'an error occured while retrieving data', error }))
+})
+
+
+
+server.get('/api/zoo/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('zoos')
+    .where({id: id})
+    .then( zoos => res.status(200).json(zoos))
+  .catch(error => res.status(500).json({message: 'an error occured while retrieving data', error}))
+  
+})
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db('zoos')
+  .insert(zoo)
+  .returning('id')
+  .then( ids => {
+    res.status(201).json(ids)
+  })
+  .catch( error => {
+    res.status(500).json({ message: 'Error inserting zoo', error})
+  })
+})
+
+server.put('/api/zoo/:id', (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  db('zoos')
+    .where({ id: id})
+    .update(changes)
+    .then(count => {
+      res.status(201).json(count)
+    })
+    .catch( error => {
+      res.status(500).json({ message: 'error updating zoo', error })
+    })
+})
+
+server.delete('/api/zoo/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('zoos')
+    .where({ id: id})
+    .del()
+    .then(count => {
+      res.status(201).json(count)
+    })
+    .catch( error => {
+      res.status(500).json({message: 'error deleting zoo', error})
+    })
+
+})
+
+
+
+
+
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
