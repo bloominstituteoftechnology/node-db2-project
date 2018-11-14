deb069c7fca2739a9ab96d34c22dd6e85e6284ee
Yusuf Nafey <yusufnafey@yahoo.com>
2018-11-14 14:34:12 -0800

    get, post endpoints complete

--- index.js
+++ index.js
@@ -7,6 +7,56 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    req.statusCode(400).json({ message: "Provide name"})
+  } else {
+    db('zoos')
+      .insert(req.body)
+      .returning('id')
+      .then(ids => {
+        res.status(201).json(ids)
+      })
+      .catch(error => {
+        res.status(500).json({ message: "There was an error while saving" })
+      })
+  }
+})
+
+server.get('/', (req, res) => {
+  res.send({ message: "API is running :D" })
+})
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .where({ id: id })
+    .then(zoo => {
+      res.status(200).json(zoo)
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Information could not be retrieved" })
+    })
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params
+  db('zoos')
+    .where({ id: id })
+    .then(zoo => {
+      res.status(200).json(zoo)
+    .catch(error => {
+      res.status(500).json({ message: "Information could not be retrieved", error: error})
+    })
+})
+
+
+
+
+
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
