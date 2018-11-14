187ec57239fda76582f364cdb6ff8c3999e16300
sean-one <basement365@gmail.com>
2018-11-14 00:12:03 -0800

    cleaned up routes and added some error handling

--- index.js
+++ index.js
@@ -13,56 +13,78 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  if (zoo.name === '') {
+    res.status(400).json({ errorMessage: 'Please be sure to include all required information.' })
+  } else {
+    db('zoos')
+      .insert(zoo)
+      .then(id => {
+        res.status(201).json({ message: `${id} has been added to the table.`})
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: 'Error communicating with data base.', err })
+      })
+  }
+})
+
 server.get('/api/zoos', (req, res) => {
   db('zoos')
     .then(zoos => {
       res.status(200).json(zoos)
     })
-    .catch(err => console.log(err))
-})
-
-server.post('/api/zoos', (req, res) => {
-  const zoo = req.body;
-  db('zoos')
-    .insert(zoo)
-    .then(id => {
-      res.status(201).json({ message: `${id} has been added to the table.`})
+    .catch(err => {
+      res.status(500).json({ errorMessage: 'Error communicating with data base.', err })
     })
-    .catch(err => console.log(err))
 })
 
-server.get('/api/zoos/:zooid', (req, res) => {
+server.get('/api/zoos/:zooid', async (req, res) => {
+  try {
+    const { zooid } = req.params
+    const lookup = await db('zoos').where({ id: zooid })
+    console.log(lookup)
+    if (lookup.length <= 0) {
+      res.status(404).json({ errorMessage: 'Bad request.' })
+    } else {
+      res.status(200).json(lookup)
+    }
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'Error communicating with data base.', err })    
+  }
+})
+
+server.delete('/api/zoos/:zooid', async (req, res) => {
   const { zooid } = req.params
-  db('zoos')
-    .where({ id: zooid })
-    .then(zoo => {
-      res.status(200).json(zoo)
-    })
-    .catch(err => console.log(err))
+  try {
+    const count = await db('zoos').where({ id: zooid }).del()
+    if (count === 0) {
+      res.status(400).json({ errorMessage: 'Unable to find that record.' })
+    } else {
+      res.status(200).json({ message: 'Delete Complete.' })
+    }
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'Error communicating with data base.', err })
+  }
 })
 
-server.delete('/api/zoos/:zooid', (req, res) => {
-  const { zooid } = req.params
-  db('zoos')
-    .where({ id: zooid })
-    .del()
-    .then(count => {
-      res.status(200).json({ count })
-    })
-    .catch(err => console.log(err))
-})
-
-server.put('/api/zoos/:zooid', (req, res) => {
-  const { zooid } = req.params
-  const changes = req.body;
-
-  db('zoos')
-    .where({ id: zooid })
-    .update(changes)
-    .then(count => {
-      res.status(200).json({ count })
-    })
-    .catch(err => console.log(err))
+server.put('/api/zoos/:zooid', async (req, res) => {
+  try {
+    const { zooid } = req.params
+    const changes = req.body;
+    if (!changes.name) {
+      res.status(400).json({ errorMessage: 'Bad request.' })
+    } else {
+      const updated = await db('zoos').where({ id: zooid }).update(changes)
+      if (updated === 0) {
+        res.status(400).json({ errorMessage: 'Unable to find that record.' })
+      } else {
+        res.status(200).json(`${changes.name} has been updated.`)
+      }
+    }
+    } catch (error) {
+    res.status(500).json({ errorMessage: 'Error communicating with data base.', err })
+  }
 })
 
 
