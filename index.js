8d04abd15584e095121c94ced4b7dacdd330f9ed
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-13 17:52:58 -0700

    fixed delete and put (MVP met for real this time)

--- index.js
+++ index.js
@@ -20,25 +20,26 @@ server.get('/', (req, res) => {
 
 //====POST====
 server.post('/api/zoos', (req, res) => {
-  const zoo = req.body;
-
-  db('zoos')
-    .insert(zoo)
-    .then(id => {
-      res.status(201).json(id);
+  if (!req.body) {
+    res.status(400).json({ message: 'The request is missing required information: name of zoo' })
+  } else {
+      db('zoos')
+      .insert(req.body)
+      .into('zoos')
+      .then(id => {
+        res.status(201).json(id)
     })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ error: "Cannot post zoo" });
-    });
-});
+    .catch(err => res.status(500).json({ message: 'The zoo record could not be created.'}))
+  }
 
+})
 //====GET ALL ZOOS====
 server.get('/api/zoos', (req, res) => {
   db('zoos')
-    .then(zoo => res.status(200).json(zoo))
-    .catch(err => res.status(500).json(err));
-});
+  .then(zoos => res.status(200).json(zoos))
+  .catch(err => res.status(500).json({message: 'There was an error retrieving the Zoos information'}))
+})
+
 
 //====GET ZOO BY ID====
 server.get('/api/zoos/:id', (req, res) => {
@@ -56,17 +57,34 @@ server.get('/api/zoos/:id', (req, res) => {
 });
 
 //====REMOVE BY ID====
-server.delete('/api/zoos/:zooid', (req, res) => {
-  const { zooid } = req.params;
-   db('zoos')
-    .where({ id: zooid })
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
     .del()
     .then(count => {
-      res.status(200).json({ count })
+      if (count) {
+        res.status(200).json({ message: `${count} records deleted` })
+      } else {
+        res.status(404).json({ message: 'A zoo with that ID could not be found' })
+      }
     })
-    .catch(error => res.status(500).json(error));
+    .catch(err => res.status(500).json({ message: 'There was an error accessing the server' }))
 })
 
+//====UPDATE====
+server.put('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({ id: req.params.id })
+  .update(req.body)
+  .then(count => {
+    if (count) {
+      res.status(200).json({ message: 'The zoo information has been updated' })
+    } else {
+      res.status(404).json({ message: 'A zoo with that ID could not be found' })
+    }
+  })
+  .catch(err => res.status(500).json({ message: 'There was an error accessing the record' }))
+})
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
