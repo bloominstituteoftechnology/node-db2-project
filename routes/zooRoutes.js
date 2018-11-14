480e536fbe1b23e9209dbebf12ec5562b14c697d
samsisle <samko9522@gmail.com>
2018-11-14 04:15:58 -0800

    Add PUT edit zoo route handler

--- routes/zooRoutes.js
+++ routes/zooRoutes.js
@@ -46,8 +46,22 @@ router.delete('/:id', async (req, res) => {
       ? res.status(200).json({ message: 'Successfully deleted zoo.' })
       : res.status(404).json({ error: 'The zoo with the specified ID does not exist.' });
   } catch {
-    res.status(500).json({ error: 'There was an error deleting the zoo to the database.' });
+    res.status(500).json({ error: 'There was an error deleting the zoo in the database.' });
   }
+});
+
+router.put('/:id', async (req, res) => {
+  if (req.body.name) {
+    try {
+      const count = await zooDb.update(req.params.id, req.body);
+      if (count) {
+        const zoo = await zooDb.get(req.params.id);
+        res.status(200).json(zoo);
+      } else res.status(404).json({ error: 'The zoo with the specified ID does not exist.' });
+    } catch {
+      res.status(500).json({ error: 'There was an error updating the zoo in the database.' });
+    }
+  } else res.status(400).json({ error: 'Please provide a name for the zoo.' });
 });
 
 module.exports = router;
