c8263a2788ba9d4ea76c64c5119c052a2614eec9
Angelo Deleon <rudy209@comcast.net>
2018-11-27 15:01:18 -0800

    Post and GET requests complete

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+
 
 const server = express();
 
@@ -7,6 +12,28 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+    const name = req.body;
+    db.insert(name)
+        .into('zoos')
+        .then(id => {
+            res.status(201).json(id[0]);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Something went wrong.' });
+        });
+});
+// GET Zoos
+server.get('/api/zoos', (req, res) => {
+    db.select('*')
+        .from('zoos')
+        .then(zoos => {
+            res.status(200).json(zoos);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 const port = 3300;
 server.listen(port, function() {
