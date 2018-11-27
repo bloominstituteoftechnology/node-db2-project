5f841aaaa85ac9a9b61442f1eb7f816b15ce9f63
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-27 16:06:05 -0500

    boiler plate set up/get and post requests complete

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +12,27 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.json(zoos)
+  })
+  .catch(err => res.status(500).json({message: 'Error displaying data'}))
+});
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos').get(id)
+  .then(zoos => {
+    res.json(zoos)
+  })
+  .catch(err => res.status(500).json({message: 'Error displaying data'}))
+})
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db('zoos').insert(zoo).then(id => {
+    res.status(201).json(id)
+  }).catch(err => res.status(500).json({message: 'Error posting zoo'}))
+})
 
 const port = 3300;
 server.listen(port, function() {
