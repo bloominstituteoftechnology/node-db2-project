10196ee540961c1acb21e61a426d7efa8d0e8397
Troy Amlee <troyamlee19@gmail.com>
2018-11-27 15:36:01 -0600

    Troy Amlee [mvp] get and get by id, the piggy shows up

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.json({ api: 'running' });
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(animals => res.status(201).json(animals))
+    .catch(error => res.status(500).json({ message: "The animals escaped!", error }))
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params
+  db('zoos')
+    .where({ id })
+    .then(animal => res.status(201).json({ animal }))
+    .catch(error => res.status(500).json({ message: `Could Not Find Your ${animal}`, error }))
+});
+
+server.post('/api/zoos', (req, res) => {
+  
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  
+});
 
 const port = 3300;
 server.listen(port, function() {
