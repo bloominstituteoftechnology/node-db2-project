644ac7f3151431537c9cb8af3af5c0810d72a394
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-11-27 13:23:22 -0500

    Post

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development)
 
 const server = express();
-
 server.use(express.json());
-server.use(helmet());
+
 
 // endpoints here
+
+server.post('api/zoos', (req, res) => {
+  const zoo = req.body;
+  db('zoos')
+    .insert(zoo)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Cannot insert', err})
+    })
+})
+
 
 const port = 3300;
 server.listen(port, function() {
