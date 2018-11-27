f47beedcc7987efd602fc89dbef65d81f0f32677
Troy Amlee <troyamlee19@gmail.com>
2018-11-27 17:20:53 -0600

    [stretch] added yogi bear + table + CRUD

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ server.get('/api/zoos/:id', (req, res) => {
   const { id } = req.params
   db('zoos')
     .where({ id })
-    .then(animal => res.status(201).json({ animal }))
-    .catch(error => res.status(500).json({ message: `Could Not Find Your ${animal}`, error }))
+    .then(zoo => res.status(201).json({ zoo }))
+    .catch(error => res.status(500).json({ message: `Could Not Find The Animal With An ID Of ${id}`, error }))
 });
 
 server.post('/api/zoos', (req, res) => {
@@ -57,6 +57,47 @@ server.delete('/api/zoos/:id', (req, res) => {
       res.status(200).json({ count })
     })
     .catch(err => res.status(500).json(err))
+});
+
+server.get('/api/bears', (req, res) => {
+  db('bears')
+    .then(bears => res.status(201).json({ bears }))
+    .catch(error => res.status(500).json({ message: 'The Bears Escaped!', error }))
+});
+
+server.get('/api/bears/:id', (req, res) => {
+  const { id } = req.params
+  db('bears')
+    .where({ id })
+    .then(bear => res.status(200).json({bear}))
+    .catch(error => res.status(500).json({ message: 'That Bear Is Not At This Zoo', error }))
+});
+
+server.post('/api/bears', (req, res) => {
+  const bear = req.body
+  db('bears')
+    .insert(bear)
+    .then(bear => res.status(200).json({bear}))
+    .catch(error => res.status(500).json(error))
+});
+
+server.put('/api/bears/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  db('bears')
+    .where({id})
+    .update(changes)
+    .then(bear => res.status(201).json({bear}))
+    .catch(error => res.status(501).json({message: error}))
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  const { id } = req.params
+  db('bears')
+    .where({id})
+    .del()
+    .then(count => res.status(200).json({count}))
+    .catch(error => res.status(500).json({ message: error }))
 });
 
 const port = 3300;
