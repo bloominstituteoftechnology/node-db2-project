af2e2c194d19e9e52f228a6ce8097f800bd0ccd1
BryceEvans <evans.bryce@gmail.com>
2018-12-14 08:45:05 -0700

    server up and running.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+
 
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
@@ -9,6 +13,22 @@ server.use(helmet());
 // endpoints here
 
 const port = 3300;
+
+server.get('/', (req, res) => {
+  res.json({message: "Server up and running!!"})
+});
+
+server.get('/zoos', (req, res) => {
+  db('zoos')
+  .then(rows => {
+    res.json(rows);
+  })
+  .catch(err => {
+    res.status(500).json({ err: "Failed to find zoos" });
+  })
+})
+
+
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
