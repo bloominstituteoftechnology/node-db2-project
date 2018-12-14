bfcdabbd004e44438e71d6373179fdfaa2e09c16
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-12-14 00:07:55 -0500

    installed dependencies added knexfile.js and configured knex added route to POST new zoos added route to GET all zoos added route to GET zoo by id

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
 
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// POST /api/zoos
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db('zoos').insert(zoo)
+    .then(ids => {
+      res
+        .status(201)
+        .json(ids);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ err: 'failed to insert zoo' });
+    });
+});
+
+// GET /api/zoos
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(rows => {
+      res
+        .json(rows);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ err: 'failed to find zoos' });
+    });
+});
+
+// GET /api/zoos/:id
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos').where('id', id)
+    .then(rows => {
+      res
+        .json(rows)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ err: 'failed to find zoo' });
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
