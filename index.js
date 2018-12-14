963875dfa5e7ae1cde7002cca1c79d2f5ee59971
paulapivat <paul.apivat@gmail.com>
2018-12-14 16:12:35 +0700

    add Post call to server

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ server.get('/', (req , res) => {
   res.send(`We are live on PORT ${port}` )
 })
 
+// INSERT INTO zoos (name)
+server.post('/api/zoos', (req , res) => {
+  const zoo = req.body;
+  console.log('zoo info', zoo)
+
+  db('zoos').insert(zoo)
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: "Failed to insert zoo"});
+  })
+})
+
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
