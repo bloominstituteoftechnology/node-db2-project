bbff3ac597f7dee0249d064ca59e1f3d08f483fd
BryceEvans <evans.bryce@gmail.com>
2018-12-14 09:40:24 -0700

    CRUD operations working, still more to implement to catch errors.

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ server.get('/', (req, res) => {
   res.json({message: "Server up and running!!"})
 });
 
+// INSERT INTO zoos (name) VALUES ('Hoogle Zoo');
+server.post('/zoos', (req, res) => {
+  const zoo =req.body;
+
+  db('zoos')
+  .insert(zoo)
+  .then((ids) => {
+    res.status(201).json(ids);
+  })
+  .catch(err => {
+    // console.log(err);
+    res.status(500).json({ err: "Failed to insert zoo" });
+  });
+});
+
+// SELECT * FROM zoos;
 server.get('/zoos', (req, res) => {
   db('zoos')
   .then(rows => {
@@ -28,6 +44,47 @@ server.get('/zoos', (req, res) => {
     res.status(500).json({ err: "Failed to find zoos" });
   })
 })
+
+// SELECT * FROM crayons WHERE id = 1
+server.get('/zoos/:id', (req, res) => {
+  const id = req.params.id;
+  db('zoos').where('id', id)
+  .then(row => {
+    res.json(row);
+  })
+  .catch(err => {
+    res.status(500).json({ err: "Failed to find zoo" });
+  });
+});
+
+// UPDATE zoos SET name = 'something to update" WHERE id = 1
+server.put('/zoos/:id', (req, res) => {
+  const {id} = req.params;
+  const zoo = req.body;
+
+  db('zoos').where('id', id)
+  .update(zoo)
+  .then(rowCount => {
+    res.json(rowCount);
+  })
+  .catch(err => {
+    res.status(500).json({ err: "Failed to update zoo" });
+  });
+});
+
+// DELETE FROM crayons WHERE id = 1;
+server.delete('/zoos/:id', (req, res) => {
+  const {id} = req.params;
+
+  db('zoos').where('id', id)
+  .del()
+  .then(rowCount => {
+    res.status(201).json(rowCount);
+  })
+  .catch(err => {
+    res.status(500).json({ err: "Failed to delete zoo" });
+  });
+});
 
 
 server.listen(port, function() {
