bf62624b7d041d18afff23348a1a4c463a1273d9
paulapivat <paul.apivat@gmail.com>
2018-12-14 15:38:00 +0700

    complete initial setup, server live

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const knex = require('knex');       //require from local install
 const helmet = require('helmet');
 
+const dbConfig = require('./knexfile');    //require from knexfile.js
+
+
 const server = express();
+const db = knex(dbConfig.development);    // the only module in knexfile.js
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req , res) => {
+  res.send(`We are live on PORT ${port}` )
+})
 
 const port = 3300;
 server.listen(port, function() {
