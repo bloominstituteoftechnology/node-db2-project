7b1f33ac220521fe55499dc71f12abb15187f117
Denise Escobar <descobar1490@gmail.com>
2018-12-15 12:13:24 -0600

    I have my post method working

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ server.use(helmet());
 
 // endpoints here
 
+//POST /api/zoos
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  if(zoo.name){
+    db('zoos').insert(zoo)
+    .then( id => {
+      res
+      .status(201)
+      .json({message: `Zoo ${id} created`} )
+    })
+    .catch(err=>{
+      res
+      .status(500)
+      .json({error: "There was an error while saving your zoo to the database"})
+    })
+  } else {
+    res
+    .status(400)
+    .json({errorMessage: "Please provide a name for the zoo"})
+  }
+  
+});
+
+//GET /api/zoos
+
+
+
+//GET /api/zoos/:id
+
+//DELETE  /api/zoos/:id
+
+//PUT /api/zoos/:id
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
