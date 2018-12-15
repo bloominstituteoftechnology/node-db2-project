865403e5cfb73aed18c98095bcde6ca12847d01d
Jason Hedrick <jasonleehedrick@gmail.com>
2018-12-15 07:47:15 -0800

    CRUD end points built out for table zoos.

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/lambda.sqlite3'
     },
-    useNullByDefault,
+    useNullAsDefault: true,
   },
 
 };
--- routers/zoo-router.js
+++ routers/zoo-router.js
@@ -1,25 +1,74 @@
 const express = require('express');
+const knex = require('knex');
 
+const dbconfig = require('../knexfile');
+
+const db = knex(dbconfig.development);
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({message: "Boom Baby!"});
+    db.select().from('zoos')
+    .then((zoos) => {
+        res.status(200).json(zoos);
+    })
+    .catch((error) => {
+        res.status(500).json({error: `Server had an error of ${error} trying to get zoos.`});
+    })
 });
 
 router.get('/:id', (req, res) => {
-
+    const {id} = req.params;
+    db('zoos').where('id', id).select()
+    .then((zoo) => {
+        res.status(200).json(zoo);
+    })
+    .catch((error) => {
+        res.status(500).json({error: `Server had an error of ${error} trying to get the zoo id of ${id}`});
+    })
 });
 
 router.post('/', (req, res) => {
-
+    const zoo = req.body;
+    if (zoo.name) {
+        db('zoos').insert(zoo)
+        .then((zooID) => {
+            res.status(201).json(zooID);
+        })
+        .catch((error) => {
+            res.status(500).json({error: `Server had an error of ${error} trying to create a new zoo.`});
+        })
+    }
+    else {
+        res.status(400).json({message: 'Creating a new zoo requires a name field.'})
+    }
 });
 
 router.delete('/:id', (req, res) => {
-
+    const {id} = req.params;
+    db('zoos').where('id', id).delete()
+    .then((rowsDeletedCount) => {
+        res.status(200).json(rowsDeletedCount);
+    })
+    .catch((error) => {
+        res.status(500).json({error: `Server had an error of ${error} trying to delete the zoo with id ${id}`});
+    })
 });
 
 router.put('/:id', (req, res) => {
-    
+    const {id} = req.params;
+    const zooUpdates = req.body;
+    if (zooUpdates.name) {
+        db('zoos').where('id', id).update(zooUpdates)
+        .then((rowsModifiedCount) => {
+            res.status(200).json(rowsModifiedCount);
+        })
+        .catch((error) => {
+            res.status(500).json({error: `Server had an error of ${error} trying to update the zoo with id ${id}.`});
+        })
+    }
+    else {
+        res.status(400).json({message: 'Editing a zoo requires a name field.'})
+    }
 });
 
 module.exports = router;
