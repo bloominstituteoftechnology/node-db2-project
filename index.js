e34fdb8ed6c069ba9eac059af59816e256f026b8
Kendra Williams <kmonique.williams@gmail.com>
2018-12-14 21:16:06 -0500

    knex file added, generic request set up

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ server.use(helmet());
 
 // endpoints here
 
+server.post("/crayons", (req, res) => {
+
+});
+
+server.get("/crayons", (req, res) => {
+
+});
+
+server.get("/crayons/:id", (req, res) => {
+
+});
+
+server.put("/crayons/:id", (req, res) => {
+
+});
+
+server.delete("/crayons/:id", (req, res) => {
+
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
