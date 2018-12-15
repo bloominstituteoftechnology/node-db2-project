df8fc6a1e7242aa5b93d890ae0090d58bf426851
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-12-15 11:13:52 -0700

    Added endpoint to post new zoo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 const dbConfig = require('./knexfile.js');
 
 
@@ -10,6 +11,28 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  if (zoo.name) {
+    db('zoos').insert(zoo)
+    .then(id => {
+      res
+        .status(201)
+        .json(id)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({message: 'The zoo could not be inserted.'});
+    });
+  }
+  else {
+    res
+      .status(400)
+      .json({message: 'Please include the zoo name.'});
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
