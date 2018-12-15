e8707d747262675f3b157af9d0057f6202f1a988
Robin U Familara <robin.familara@cgius.com>
2018-12-15 09:17:50 -0600

    Updated model.js -Create get method and added mappers -Create mappers for data -Create methods to handle GET request -Update the knexfile.js sqlite3 filepath -Update the index.js with GET functions for endpoints -Make zoos functional

--- data/model.js
+++ data/model.js
@@ -1,1 +1,10 @@
 const db = require('./dbConfig');
+const mappers = require('./mappers')
+ module.exports = {
+    get: function(id){
+        let query = db('zoos');
+        return query.then(zoos => {
+            return zoos.map(zoo => mappers.zooToBody(zoo))
+        })
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const db = require('./data/model');
+
+// const knex = require('knex');
+// const knexConfig = require('./knexfile');
+// const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -8,7 +13,31 @@ server.use(helmet());
 
 // endpoints here
 
-const port = 3300;
+server.get('/', (req, res) => {
+  db.get()
+    .then(zoos => {
+      console.log('get response', zoos);
+      res.json(zoos)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Could not fetch Zoos" })
+    })
+})
+
+// server.get('/', (req, res) => {
+//   db('zoos')
+//     .then(response => {
+//       console.log('get response', response);
+//       res.json(response)
+//     })
+//     .catch(err => {
+//       console.log('error:', err);
+//       res.status(500).json({ message: "Could not fetch Zoos" })
+//     })
+// })
+
+const port = 1973;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
