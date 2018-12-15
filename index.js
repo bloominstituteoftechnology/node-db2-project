3aeea7ec4c606b3b81202a273120348187861d7f
BrandonMoll <brandonmoll85@gmail.com>
2018-12-15 12:51:08 -0600

    stretch goal complete

--- index.js
+++ index.js
@@ -83,6 +83,79 @@ server.put('/api/zoos/:id', (req, res) => {
 })
 
 
+//BEARS TABLE ENDPOINTS******
+
+server.post('/api/bears', (req, res) => {
+  const zoo = req.body;
+
+  if(zoo.name) {
+    db('bears').insert(zoo)
+    .then((id) => {
+      res.status(201).json(id)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error adding bear'})
+    })
+  } else {
+    res.status(400).json({message: 'Missing name!'})
+  }
+});
+
+server.get('/api/bears', (req, res) => {
+  db('bears')
+  .then(rows => {
+    res.json(rows)
+  })
+  .catch(err => {
+    res.status(500).json({message: 'Error getting bears'})
+  })
+});
+
+server.get('/api/bears/:id', (req, res) => {
+  const {id} = req.params;
+
+  db('bears').where('id', id)
+  .then(rows => {
+    res.json(rows)
+  })
+  .catch(err => {
+    res.status(500).json({message: 'Error gettin that bear'})
+  })
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  const {id} = req.params;
+
+  if(id) {
+    db('bears').where('id', id)
+    .delete()
+    .then(rows => {
+      res.status(201).json(rows)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error deleting bear'})
+    })
+  }
+});
+
+server.put('/api/bears/:id', (req, res) => {
+  const {id} = req.params;
+  const zoo = req.body;
+
+  if(id && zoo.name) {
+    db('bears').where('id', id)
+    .update(zoo)
+    .then(rowCount => {
+      res.json(rowCount)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error updating bear'})
+    })
+  } else {
+    res.json({message: 'Missing name or ID'})
+  }
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
