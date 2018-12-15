3ee5f7b21af7d2b8f86089962d4949477c98d042
Obed Lorisson <Obed.lorisson@gmail.com>
2018-12-15 04:42:23 -0500

    setup project with yarn added knex and sqlite3 for database access configure knex to use lambda sqlite3

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbconfig = require('./knexfile')
 
 const server = express();
+const db = knex(dbconfig.development)
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/api/zoos', (req, res) =>{
+
+})
+
+server.post('/api/zoos',(req, res) =>{
+
+})
+
+server.get('/api/zoos/:id',(req, res) =>{
+
+})
+
+server.delete('/api/zoos/:id',(req, res) =>{
+
+})
+
+server.put('/api/zoos/:id',(req, res) => {
+
+})
 
 const port = 3300;
 server.listen(port, function() {
