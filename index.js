1c1b70b7df4cc1e575bb6e3b7ec9dfa0ef6140b9
Kendra Williams <kmonique.williams@gmail.com>
2018-12-14 21:23:44 -0500

    connected index.js to lambda database

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+//general requires
 const express = require('express');
 const helmet = require('helmet');
+
+//knex requires
+const knex = require("knex");
+const dbConfig = require("./knexfile");
+const db = knex(dbConfig.development);
 
 const server = express();
 
