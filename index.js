630287fb5fa938393868e2b410755f79968430d0
Mitchell Robles <mitchellr92@live.com>
2018-12-15 10:58:24 -0800

    Post and get by id requests

--- index.js
+++ index.js
@@ -12,13 +12,43 @@ server.use(helmet());
 
 // endpoints here
 
+server.post('/api/zoos', (req, res) => {
+
+  const animal = req.body;
+
+  if (animal.name) {
+    db('zoos').insert(animal)
+      .then(animalId => {
+        res.status(201).json(animalId)
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Could not insert animal' })
+      })
+  } else {
+    res.status(400).json({ messagae: 'Missing name' })
+  }
+})
+
 server.get('/api/zoos', (req, res) => {
   db('zoos')
     .then(animals => {
       res.status(201).json(animals)
     })
     .catch(err => {
-      res.status(500).json({ message: 'Could not get animals'})
+      res.status(500).json({ message: 'Could not get animals' })
+    })
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+
+  const { id } = req.params;
+
+  db('zoos').where('id', id)
+    .then(animal => {
+      res.status(201).json(animal)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Cout not get animal'})
     })
 })
 
