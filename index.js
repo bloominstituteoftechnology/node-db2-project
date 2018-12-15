d043361fba26bf1a8173f68ffe9574981a2a638f
Willie Yturralde <willieino@hotmail.com>
2018-12-15 00:44:29 -0800

    completed get and post functions

--- index.js
+++ index.js
@@ -1,14 +1,91 @@
 const express = require('express');
+const knex = require("knex");
+const dbConfig = require("./knexfile");
 const helmet = require('helmet');
-
 const server = express();
+//const cors = require('cors');
+//const db = require('./data/dbConfig');
+const DB = knex(dbConfig.development);
+const parser = express.json();
+const PORT = 3300;
+const logger = require('morgan');
+//const projectRouter = require('./routers/projectRouter');
+//const actionRouter = require('./routers/actionRouter')
 
 server.use(express.json());
 server.use(helmet());
-
+//server.use(cors({}));
+server.use(parser);
+server.use(logger('tiny'));
+//server.use(helmet());
 // endpoints here
 
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+server.get('/api/zoos', (req, res) => {
+DB('zoos')
+    .then(zoo => {
+      res.json(zoo);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
 });
+
+server.get("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  DB("zoos")
+    .where("id", id)
+    .then(zoo => {
+      res.json(zoo);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+
+server.post("/api/zoos", (req, res) => {
+  const name = req.body;
+ 
+  DB("zoos")
+    .insert(name)
+    .then(ids => {
+      res.status(201).json({ ids });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+server.put("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  const zoo = req.body;
+  DB("zoos")
+    .where("id", id)
+    .update(zoo)
+    .then(zoo => {
+      res.json({ zoo });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  DB('zoos')
+    .where('id', id)
+    .del()
+    .then(zoo => {
+      res.json({ zoo });
+    })
+    .catch(err => {
+      res.status(500).json({ err: 'failed to update crayons' })
+    });
+});
+
+//server.listen(PORT, console.log(`Listening on PORT:${PORT}`));
+
+server.listen(PORT, function () {
+  console.log(`\n=== Web API Listening on http://localhost:${PORT} ===\n`);
+});
+
