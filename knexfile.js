5ea57af210db29f38adf75d22fb0ba0bbecbb5cc
Robin U Familara <robin.familara@cgius.com>
2018-12-15 10:40:14 -0600

    Updated the index.js -Finalize all CRUD

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+// Update with your config settings.
+
 module.exports = {
 
     development: {
@@ -6,38 +8,6 @@ module.exports = {
         filename: './data/lambda.sqlite3'
       },
       useNullAsDefault: true
-    },
-  
-    staging: {
-      client: 'postgresql',
-      connection: {
-        database: 'my_db',
-        user:     'username',
-        password: 'password'
-      },
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        tableName: 'knex_migrations'
-      }
-    },
-  
-    production: {
-      client: 'postgresql',
-      connection: {
-        database: 'my_db',
-        user:     'username',
-        password: 'password'
-      },
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        tableName: 'knex_migrations'
-      }
     }
   
   };
