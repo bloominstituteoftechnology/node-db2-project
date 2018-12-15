23b09e2cafe039c3b844b7f00badae4dd2cad204
unknown <kyleteeter12@gmail.com>
2018-12-15 12:27:46 -0600

    Get, get by id and put are all working.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex')
+
+const dbConfig = require('./knexfile')
+const db = knex(dbConfig.development)
 
 const server = express();
 
@@ -7,7 +11,56 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-server.post('/')
+server.post('/api/zoos', (req,res) => {
+  const zoo = req.body;
+  db('zoos').insert(zoo)
+  .then(ids => {
+    res.status(201).json(ids)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err:"Item was unable to post to database"})
+})
+});
+
+server.get('/api/zoos', (req, res) => {
+   db('zoos')
+   .then(zoo => {
+     res.json(zoo)
+   })
+   .catch(err => {
+     res.status(500).json({err: "Unable to fetch data."})
+   })
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  const {id} = req.params
+  db('zoos')
+  .where('id', id)
+  .then(zoo => {
+    res.json(zoo)
+  })
+  .catch(err => {
+    res.status(500).json({err:`Failed to find zoo with the id of ${id}`})
+  })
+})
+
+server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body;
+  const {id} = req.params;
+
+  db('zoos')
+  .where('id', '=', id)
+  .update(changes)
+  .then(count => {
+    res.status(200).json(count);
+  })
+  .catch(err => {
+    res.status(500).json({err: `Item with id of ${id} was unable to update.`})
+  })
+})
+
+
 
 const port = 3300;
 server.listen(port, function() {
