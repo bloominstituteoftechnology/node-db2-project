13d3fea2258bcf62bed75e3a919d1babac6dca7b
Rob Salzberg <robsalzberg@gmail.com>
2018-12-15 14:33:57 -0500

    Added bears database and endpoints for stretch problem

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// zoo endpoints here
 
 server.post("/api/zoos", (req, res) => {
   const name = req.body;
@@ -73,13 +73,88 @@ server.put("/api/zoos/:id", (req, res) => {
     })
     .catch(err => {
       res.status(500).json(err);
-    }); 
+    });
 });
 
 server.delete("/api/zoos/:id", (req, res) => {
   const { id } = req.params;
 
   db("zoos")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// bear endpoints here
+
+server.post("/api/bears", (req, res) => {
+  const name = req.body;
+
+  db.insert(name)
+    .into("bears")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get("/api/bears", (req, res) => {
+  db("bears")
+    .select()
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get("/api/bears/:id", (req, res) => {
+  const bearId = req.params.id;
+
+  db("bears")
+    .where("id", bearId)
+    .select()
+    .then(bear => {
+      if (bear.length) {
+        res.status(200).json(bear);
+      } else {
+        res
+          .status(404)
+          .json({ message: `Could not find a bear with id ${bearId}` });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not get any bears." });
+    });
+});
+
+server.put("/api/bears/:id", (req, res) => {
+  const updates = req.body;
+  const { id } = req.params;
+
+  db("bears")
+    .where("id", "=", id)
+    .update(updates)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete("/api/bears/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("bears")
     .where({ id })
     .del()
     .then(count => {
