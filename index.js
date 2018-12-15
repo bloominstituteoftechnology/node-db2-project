9394413942fe772d3dd6bad26cc1305384b181a7
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-15 11:59:35 -0500

    configured app and did a post request to make sure the connection is live

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
 const helmet = require('helmet');
 
 const server = express();
@@ -7,6 +12,17 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/animals', (req, res) => {
+  const animal = req.body;
+  db('zoos').insert(animal)
+    .then(animalsId => {
+      res.status(201).json(animalsId);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: 'Failed to insert animal' })
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
