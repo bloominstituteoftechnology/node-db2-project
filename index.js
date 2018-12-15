2e65aafab3f1b774c44b8b2a88402185f03eb328
Mitchell Robles <mitchellr92@live.com>
2018-12-15 10:25:19 -0800

    Setting up knex configuration

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
 
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
