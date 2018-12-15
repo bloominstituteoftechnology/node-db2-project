70ce1be66dc8a47fc5212296440d4f084a6e10b3
Emmanium <pradoe.ep@gmail.com>
2018-12-15 12:55:52 -0700

    added knex file wrote endpoints configured get and post endpoint

--- README.md
+++ README.md
@@ -58,5 +58,3 @@ When the client makes a `PUT` request to this endpoint passing an object with th
 ## Stretch Problem
 
 Add a new _bears_ table to the database and add endpoints to perform CRUD operations on it. Each bear should have an `id` and `name` property similar to the _zoos_ table.
-
-initial commit
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const knex = require('knex');
 const helmet = require('helmet');
+
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +11,39 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoos', (req, res) => {
+  const name = req.body;
+  db('zoos').insert(name)
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to add animal' })
+    })
+})
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to find animals' })
+    })
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+  
+})
+
+server.put('/api/zoos/:id', (req, res) => {
+  
+})
 
 const port = 3300;
 server.listen(port, function() {
