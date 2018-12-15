48322dafbdd7fbe537f4ae14fcd183d20e6f9d4d
cferrell2 <cferrell12182@gmail.com>
2018-12-15 12:48:12 -0500

    clean up code

--- middleware.js
+++ middleware.js
@@ -1,14 +1,15 @@
 const verify = {
-    //Verify that zoo.name exists within request body
+    //Verify that name exists within request body
     checkName: (req,res, next) => {
-        errMsg = (req.baseUrl.includes('zoos')) 
-            ? "Name of zoo is missing"
-            : "Name of bear is missing"
-        // const reqObject = req.body;
+        
         if(req.body.name){
             next();
         }
         else{
+            const errMsg = (req.baseUrl.includes('zoos')) 
+                ? "Name of zoo is missing"
+                : "Name of bear is missing";
+                
             res.status(400)
             res.json({error: errMsg})
         }
