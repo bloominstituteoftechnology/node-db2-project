51f4c27042935bccda2556b0c195c7bf10d9e66a
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-12-15 12:26:45 -0600

    GET, POST, PUT, and DELETE methods built.

--- index.js
+++ index.js
@@ -1,12 +1,89 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const dbConfig = require('./knexfile');
+
+const knex = require('knex');
+const db = knex(dbConfig.development);
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('It is working!');
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos').then(zoos => {
+    res.json(zoos);
+  });
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.json(zoo);
+      } else {
+        res.status(404).json({ error: `Zoo with the id of ${id} not found.` });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db('zoos')
+    .insert(zoo)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db('zoos')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      if (count) {
+        res.status(201).json(count);
+      } else {
+        res
+          .status(404)
+          .json({ error: `zoo with the id of ${id} could not be found.` });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count) {
+        res.json(count);
+      } else {
+        res.status(404).json({ error: 'Zoo could not be found.' });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
