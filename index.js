84ddfa596e931f8cf14926bfd7468ef6f57f1229
jbrock1981 <jbrock1981@gmail.com>
2018-12-15 11:53:12 -0600

    finished MVP, working stretch

--- index.js
+++ index.js
@@ -1,14 +1,89 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbConfig = require('./knexfile');
 
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  db('zoos').insert(zoo)
+  .then(ids => {
+      res
+          .status(201)
+          .json(ids)
+  })
+  .catch(err => {
+      res
+          .status(500)
+          .json({ message: "Unable to add zoo."})
+  })
+});
 
+server.get('/api/zoos', (req, res) => {
+    db('zoos')
+      .then(rows => {
+        res
+          .status(201)
+          .json(rows)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: "Unable to load zoos" })
+      })
+})
 
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos').where('id', id)
+      .then(zoo => {
+          res
+              .status(201)
+              .json(zoo)
+      })
+      .catch(err => {
+          res
+              .status(500)
+              .json({ message: "failed to retrieve requested zoo" })
+      })
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const zoo = req.body;
+  db('zoos').where('id', id).update(zoo)
+    .then(rowCount => {
+      res
+        .status(201)
+        .json(rowCount)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Unable to update specified zoo"})
+    })
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos').where('id', id).del()
+  .then(rowsDeleted => {
+    res
+        .status(201)
+        .json(rowsDeleted)
+})
+.catch(err => {
+    res
+        .status(500)
+        .json({ message: "Unable to delete crayon" })
+})
+})
 
 
 const port = 3300;
--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,8 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/lambda.sqlite3'
-    }
+    },
+    useNullAsDefault: true
   }
 
 };
