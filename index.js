85ab62b88906a63b1e3ef4e2c603dbf4decdaf4d
Denise Escobar <descobar1490@gmail.com>
2018-12-15 11:34:22 -0600

    connected knex to my data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbConfig = require('./knexfile');
 
+const db =knex(dbConfig.development)
 const server = express();
 
 server.use(express.json());
