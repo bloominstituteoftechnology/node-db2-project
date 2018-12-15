45ad63983adc4a00e8f0feb31206de1884a9403d
cferrell2 <cferrell12182@gmail.com>
2018-12-15 10:14:28 -0500

    require and use knex to create database and knex connection

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+//create express server 
 const express = require('express');
-const helmet = require('helmet');
-
 const server = express();
 
+//Create db / knex connection
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
+
+//Built in and 3rd party middleware
 server.use(express.json());
+const helmet = require('helmet');
 server.use(helmet());
 
 // endpoints here
 
+
+//Listener
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
