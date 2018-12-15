bb8eb377e84d0c1044e498b1dee3c14bfc2b3b13
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-12-15 12:14:10 -0600

    set up index.js file and finished get and post request

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex'); 
 const server = express();
+
+const dbConfig = require('./knexfile')
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get('/zoos', (req, res) => {
+  db('zoos').then(rows => {
+    res.json(rows); 
+  })
+  .catch(err => { res.status(500).json({err: "we've encountered an error"})
+  })
+})
+
+server.get('/zoos/:id', (req, res) => {
+  const {id} = req.params; 
+  db('zoos').where('id', id)
+    .then( rows => {
+      res.json(rows)
+    })
+    .catch( err => { res.status(500).json({err: "we've encounterd an error"})
+    })
+})
+
+server.post('/zoos', (req, res) => {
+  let name = req.body; 
+  db('zoos').insert(name)
+    .then( ids => {
+      res.status(201).json(ids); 
+    })
+    .catch( err => { res.status(500).json({err: "we've encountered an error"})})
+})
+
+server.delete('/zoos/:id', (req, res) => {
+
+})
+
+server.put('/zoos/:id', (req, res) => {
+
+})
 
 const port = 3300;
 server.listen(port, function() {
