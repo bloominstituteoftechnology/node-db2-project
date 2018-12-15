41f22d3ad7cdd592b10ee37ce976c7e15ab1aa99
Freekeyboy <jjhutton81@gmail.com>
2018-12-15 10:25:04 -0800

    Created boilerplate endpoints.

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ server.use(helmet());
 
 // endpoints here
 
+server.post("/api/zoos", (req, res) => {});
+server.get("/api/zoos", (req, res) => {});
+server.get("/api/zoos", (req, res) => {});
+server.put("/api/zoos", (req, res) => {});
+server.delete("/api/zoos", (req, res) => {});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
