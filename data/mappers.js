f19597db493c3afeedd1829c25c8a4436756149a
Robin U Familara <robin.familara@cgius.com>
2018-12-15 09:24:36 -0600

    Modified index.js -Added id condition at data model -Removed Boolean condition from data mapper

--- data/mappers.js
+++ data/mappers.js
@@ -1,13 +1,11 @@
 module.exports = {
-    intToBoolean,
+   
     zooToBody
 }
- function intToBoolean(int) {
-    return int === 1 ? true : false;
-}
+ 
  function zooToBody(zoo) {
     return {
-        ...zoo,
-        completed: intToBoolean(zoo.completed)
+        ...zoo
+        
     };
 }
