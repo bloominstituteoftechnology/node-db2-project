90531e665075cd1e3dfbc43c10626f1a91c4f006
Robin U Familara <robin.familara@cgius.com>
2018-12-15 09:27:10 -0600

    Finalize index.js -Get ID error handle -Added it in order to test if response is an empty object for valid id

--- index.js
+++ index.js
@@ -16,27 +16,31 @@ server.use(helmet());
 server.get('/', (req, res) => {
   db.get()
     .then(zoos => {
-      
       res.json(zoos)
     })
     .catch(err => {
-      
       res.status(500).json({ message: "Could not fetch Zoos" })
     })
-})
+});
+
 
 server.get('/:id', (req, res) => {
   const {id} = req.params;
   db.get(id)
     .then(zoo => {
-      console.log('get zoo', zoo);
-      res.json(zoo)
+      if(Object.keys(zoo).length === 0){
+        res.status(404).json({ message: "Invalid Zoo ID" })
+      } else {
+        res.json(zoo)
+      }
     })
     .catch(err => {
-      console.log('zoo error', err)
       res.status(500).json({ message: "Could not find that Zoo" })
     })
-})
+});
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
