9b03099db17a1eb9cd9b0c25d9ca726455acd012
Chris Roth <c.roth.lambda@gmail.com>
2018-12-19 21:52:47 -0600

    2 Endpoints done: get for all zoos and post for zoos need to add the name check on post.

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ const helmet = require('helmet');
 const knex = require('knex');
 
 const dbConfig = require('./knexfile');
-const db = knex(dbConfig.development)
 const server = express();
+const db = knex(dbConfig.development)
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/zoos', (req, res)=>{
+  const zoo = req.body;
+  db('zoos').insert(zoo)
+    .then(id=>{
+      res.status(201).json(id)
+    })
+    .catch(err=>{
+      res.status(500).json({err: "Something happend while adding your Data"})
+    })
+})
+
 server.get('/zoos', (req, res)=>{
-  db('zoos').get
+  db("zoos").then(zoos=>{
+    res.json(zoos)
+  })
+  .catch(err=>{
+    res.status(500).json({err: "Something went wrong looking for Zoos"})
+  })
+})
+
+server.get('/zoos/:id', (req, res)=>{
+  
 })
 
 const port = 3300;
