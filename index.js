e030f3ed52e2ddc42f548333b01e7222803bd71f
Chris Roth <c.roth.lambda@gmail.com>
2018-12-19 22:27:22 -0600

    Delete request: completed with multiple error handlers.

--- index.js
+++ index.js
@@ -10,40 +10,71 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-server.post('/zoos', (req, res)=>{
+server.post('/zoos', (req, res) => {
   const zoo = req.body;
-  zoo.name?
-  db('zoos').insert(zoo)
-    .then(id=>{
+  zoo.name ?
+    db('zoos').insert(zoo)
+    .then(id => {
       res.status(201).json(id)
     })
-    .catch(err=>{
-      res.status(500).json({err: "Something happend while adding your Data"})
-    }):res.status(400).json({err: "Please add a name and try again"})
+    .catch(err => {
+      res.status(500).json({
+        err: "Something happend while adding your Data"
+      })
+    }) : res.status(400).json({
+      err: "Please add a name and try again"
+    })
 })
 
-server.get('/zoos', (req, res)=>{
-  db("zoos").then(zoos=>{
-    res.json(zoos)
-  })
-  .catch(err=>{
-    res.status(500).json({err: "Something went wrong looking for Zoos"})
-  })
+server.get('/zoos', (req, res) => {
+  db("zoos").then(zoos => {
+      res.json(zoos)
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: "Something went wrong looking for Zoos"
+      })
+    })
 })
 
-server.get('/zoos/:id', (req, res)=>{
-  const {id} = req.params;
-  
-  db('zoos').where('id', id).then(zoo=>{
-    zoo[0]?res.json(zoo)
-    :res.status(404).json({err: "No zoo with this ID found"})
-  })
-  .catch(err=>{
-    res.status(500).json("We experienced a problem looking for that ID")
-  })
+server.get('/zoos/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
+
+  db('zoos').where('id', id).then(zoo => {
+      zoo[0] ? res.json(zoo) :
+        res.status(404).json({
+          err: "No zoo with this ID found"
+        })
+    })
+    .catch(err => {
+      res.status(500).json("We experienced a problem looking for that ID")
+    })
+})
+
+server.delete('/zoos/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
+  db('zoos').where('id', id).del()
+    .then(numAlt => {
+      numAlt ? res.status(201).json({
+          message: "This zoo is now demolished"
+        }) :
+        res.status(404).json({
+          message: "Zoo with this ID doesn't exsist"
+        })
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: "We are having trouble getting rid of this Zoo"
+      })
+    })
+
 })
 
 const port = 3300;
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
