303f3c0915ac10e4a6fa373133db74537943d8be
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-12-20 00:00:33 -0600

    Created all endpoints and handled all errors Finished MVP

--- index.js
+++ index.js
@@ -5,10 +5,104 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
-
-// endpoints here
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+
+server.post('/api/zoos', (req, res) => {
+  const { name } = req.body;
+  !name
+    ? res.status(400).json({ error: 'Property "name" is required!' })
+    : db('zoos')
+        .insert({ name })
+        .then(ids => res.status(201).json(ids[0]))
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: 'Something went wrong adding your new zoo!' });
+          console.error(err);
+        });
 });
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(results =>
+      !results.length
+        ? res.json({ error: 'There is no zoo yet, please try again later.' })
+        : res.json(results)
+    )
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'Something went wrong retrieving your zoos!' });
+      console.error(err);
+    });
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where('id', id)
+    .then(result =>
+      result.length
+        ? res.json(result)
+        : res.status(404).json({
+            error: `We couldn't find any zoo with ID: ${id}. Please check the information submitted.`
+          })
+    )
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'Something went wrong retrieving your zoo.' }),
+        console.error(err);
+    });
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  !name
+    ? res.status(400).json({ error: 'Property "name" is required!' })
+    : db('zoos')
+        .where('id', id)
+        .update({ name })
+        .then(count =>
+          count
+            ? res.json(count)
+            : res.status(404).json({
+                error: `We couldn't find any zoo with ID: ${id}. Please check the information submitted.`
+              })
+        )
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: 'Something went wrong updating your zoo.' });
+          console.error(err);
+        });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where('id', id)
+    .del()
+    .then(count =>
+      count
+        ? res.status(201).json(count)
+        : res.status(404).json({
+            error: `We couldn't find any zoo with ID: ${id}. Please check the information submitted.`
+          })
+    )
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'Something went wrong deleting your zoo.' });
+      console.error(err);
+    });
+});
+
+server.listen(port, () =>
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
+);
