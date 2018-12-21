f264a011ec793e875caaabf907445e05927edea1
vy3191 <venkyafhs@gmail.com>
2018-12-20 23:51:31 -0500

    Added get method

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
+const dbConfig = require('./knexfile');
 const server = express();
-
+const db = knex(dbConfig.development);
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-
 const port = 3300;
+server.get('/api/zoos', (req,res) => {
+   db('zoos').then(ids => {
+        res.json(ids);
+   })
+});
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
