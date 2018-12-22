bf4c2f92d92ecd6af3408ca6d6933cf3d1bcdbed
hraya <hraya@lssu.edu>
2018-12-22 13:19:30 -0600

    Began crreating endpoints
    The POST and GET endpoints are created
    
    The endpoints have not yet been tested due to
    problems with postman, but will test to confirm
    that they are functioning properly before final commit

--- index.js
+++ index.js
@@ -12,15 +12,46 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-server.get('/api/zoos', (req, res)=>{
-  db.('')
+server.post('/api/zoos', (req, res)=>{
+  const zoo = req.body;
+  if(!zoo.name){res.status(400).json({message:"Please provide a name"})}
+  db('zoos').insert(zoo)
+  .then(ids=>{
+    res.status(201).json(ids)
+  })
+  .catch(err=>{
+    res.status(404).json({error:"An error occurred in adding a zoo! "})
+  })
 })
 
-server.get('/api/zoos/:id', (req, res)=>{
+server.get('/api/zoos', (req, res)=>{
+  db('zoos')
+  .then(rows=>{
+    res.json(rows)
+  })
+  .catch(err =>{
+    console.log(err)
+    res.status(500).json({error:"Failed to find the Zoos!"})
+  })
   
 })
 
-server.post('/api/zoos', (req, res)=>{
+server.get('/api/zoos/:id', (req, res)=>{
+  const { id } = req.params;
+  db('zoos').where('id', id)
+  .then(zoo=>{
+    if(zoo){
+      res.status(200).json(zoo)
+    }else{
+      res.status(404).json({error:"The Zoo with the specific id does not exist"})
+    }
+  }).catch(err=>{
+    res.status(500).json({error:"Trouble getting the Zoo!"})
+  })
+  
+})
+
+server.delete('/api/zoos/:id', (req, res)=>{
   
 })
 
@@ -28,9 +59,6 @@ server.put('/api/zoos/:id', (req, res)=>{
   
 })
 
-server.delete('/api/zoos/:id', (req, res)=>{
-  
-})
 
 
 server.listen(port, function() {
