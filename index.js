58d8e405961eef071387914866a393840ff35a0c
marcusbass323 <marcus@bassconsultantgroup.com>
2018-12-21 23:12:14 -0800

    Post endpoint created

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
-
 const server = express();
-
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+  console.log('zoo info', zoo)
+  db('zoo').insert(zoo)
+      .then(ids => {
+          res.status(201).json(ids);
+      })
+      .catch(err => {
+          res.status(500).json({
+              err: 'Failed to insert zoo'
+          });
+      });
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
