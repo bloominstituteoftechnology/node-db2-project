17d42f0f033c3369919673745230d986c9a6e9f5
hraya <hraya@lssu.edu>
2018-12-22 12:36:15 -0600

    Initial Commit
    installed all dependencies and began endpoints

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
+const knex = require('knex');
 const helmet = require('helmet');
 
+const dbConfig = require('./knexfile')
+
 const server = express();
+const db = knex(dbConfig.development);
+const port = 3300;
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/api/zoos', (req, res)=>{
+  db.('')
+})
 
-const port = 3300;
+server.get('/api/zoos/:id', (req, res)=>{
+  
+})
+
+server.post('/api/zoos', (req, res)=>{
+  
+})
+
+server.put('/api/zoos/:id', (req, res)=>{
+  
+})
+
+server.delete('/api/zoos/:id', (req, res)=>{
+  
+})
+
+
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
