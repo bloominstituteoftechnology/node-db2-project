82c7b3c7f751c619575d75bd74875c0e8e538d1f
vy3191 <venkyafhs@gmail.com>
2018-12-21 22:02:09 -0500

    Completed deleted method

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ server.use(helmet());
 
 // endpoints here
 const port = 3300;
+
 server.get('/api/zoos', (req,res) => {
    db('zoos').then(zoos => {
         res.status(200).json(zoos);
+   }).catch(err => {
+    res.status(500).json({err: 'Failed to find crayon'});
    })
 });
 
@@ -22,7 +25,7 @@ server.get('/api/zoos/:id', (req,res) => {
                res.status(200).json(zoo);
            }).catch(err => {
                 res.status(500).json({err: 'Failed to find crayon'});
-              })
+    })
 });
 server.post('/api/zoos', (req, res) => {
     const name = req.body;
@@ -39,6 +42,32 @@ server.post('/api/zoos', (req, res) => {
           res.status(400).json({err: 'Please enter the name'});
     }        
 });
+
+server.put('/api/zoos/:id', (req,res)=> {
+     const {id} = req.params;
+     const name = req.body;
+     if(id && name) {
+          db('zoos').where('id', id).update(name)
+                    .then( newName => {
+                        res.status(200).json(newName);
+                    }).catch( err => {
+                        res.status(500).json({err:'Failed to update the name this time'})
+                    })
+      } else {
+          res.status(400).json({"error": "Please enter a valid name"})
+      }         
+});
+
+server.delete('/api/zoos/:id', (req,res) => {
+       const {id} = req.params;
+       db('zoos').where('id', id).del()
+              .then( response => {
+                 res.json({"Message": "Deleted Successfully"})
+              }).catch(err => {
+                res.status(500).json({err: 'Failed to delete crayon'});
+              });
+});
+
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
