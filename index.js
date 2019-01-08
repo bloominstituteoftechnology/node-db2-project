2b0b5913a4258a788e4b600ff369f8c60c24bc8c
louislee7 <7.louislee@gmail.com>
2019-01-07 18:08:19 -0800

    added knexfile.js and completed post request

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbConfig = require('./knexfile')
 
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
@@ -9,6 +12,19 @@ server.use(helmet());
 // endpoints here
 
 const port = 3300;
+
+server.post('./data/lambda.sqlite3', (req, res) => {
+  const zoo = req.body;
+  db('zoos').insert(zoo)
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({err: 'Failed to insert new zoo'})
+  })
+})
+
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
