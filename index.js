8aff32b1638533f6cb860d3479c12f49ae20ce09
Andres Rivera <andres.rivera.toro@gmail.com>
2019-01-08 19:16:04 -0500

    Add dependencies and configure knex to connect to db

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
 
 const server = express();
+const port = 3300;
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
 
-const port = 3300;
-server.listen(port, function() {
+server.listen(port, () => {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
