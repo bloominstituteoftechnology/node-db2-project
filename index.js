fb60ab66eff818e17ab3b704b2814e10a4c80009
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-09 23:11:45 -0500

    Set up knex, writing out endpoints now.

--- index.js
+++ index.js
@@ -18,23 +18,33 @@ server.use(
 const port = process.env.port || 3300;
 
 server.get('/api/zoos', (req, res) => {
-
+ const zoo = req.body
+ DB
+ .select()
+ .then(rows => {
+  res
+   .json(rows)
+ })
 })
 
 server.get('/api/zoos/:id', (req, res) => {
-
+ const zoo = req.body
 })
 
 server.post('/api/zoos', (req, res) => {
+ const zoo = req.body
+ DB
+
 
 })
 
 server.put('/api/zoos:id', (req, res) => {
-
+ const zoo = req.body
 })
 
 server.delete('/api/zoos/:id', (req, res) => {
-
+ const zoo = req.body
+ 
 })
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
