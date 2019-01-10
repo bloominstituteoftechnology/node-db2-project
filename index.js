ffaf9c256891a8757959b1b72b431f147be17462
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-09 22:55:39 -0500

    Added knexfile,linked it to lambda.sqlite3 db, wrote basic skeleton for endpoints.

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ server.use(helmet());
 // endpoints here
 
 const port = 3300;
+
+server.get('/api/zoos', (req, res) => {
+
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+
+})
+
+server.post('/api/zoos', (req, res) => {
+
+})
+
+server.put('/api/zoos:id', (req, res) => {
+
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+ 
+})
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
