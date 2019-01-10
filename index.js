bce71089547d5e4e8e5b6f5e8577d304342552e1
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-09 23:02:03 -0500

    Configuring knex connection to DB to write endpoints.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
+const logger = require("morgan")
+const knex = require("knex")
+const knexDB = require("./knexfile")
+const DB = knex(knexDB.development)
 
 const server = express();
 
-server.use(express.json());
-server.use(helmet());
+server.use(
+ express.json(),
+ helmet(),
+ logger('tiny')
+)
 
 // endpoints here
 
-const port = 3300;
+const port = process.env.port || 3300;
 
 server.get('/api/zoos', (req, res) => {
 
@@ -27,7 +34,7 @@ server.put('/api/zoos:id', (req, res) => {
 })
 
 server.delete('/api/zoos/:id', (req, res) => {
- 
+
 })
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
