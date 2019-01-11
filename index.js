a3bbc96222d5f654cdea08ecb4ade8933d3e00c5
cocoitali <cmburatto@gmail.com>
2019-01-10 22:40:20 -0700

    MVP/FIX : POST tested/fixed db error in knexfile

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const express = require('express');
+const express = require('express')
 const knex = require('knex')
 const knexConfig = require('./knexfile')
 const db = knex(knexConfig.development)
-const helmet = require('helmet');
+const helmet = require('helmet')
 
-const server = express();
+const server = express()
 
-server.use(express.json());
-server.use(helmet());
+server.use(express.json())
+
+server.use(helmet())
 
 // endpoints here
 server.post('/api/zoos', (req, res) => {
-
+  const zoo = req.body
+  if (zoo.name) {
+    db('zoos')
+      .insert(zoo)
+      .then(ids => {
+        res.status(201).json(ids)
+      })
+      .catch((err) => {
+        console.log(err)
+        res
+          .status(500)
+          .json({ err: 'Failed to insert the zoo into the database.' })
+      })
+  } else {
+    res.status(400).json({ error: 'Please provide a name for the zoo' })
+  }
 })
 
 server.get('/api/zoos', (req, res) => {
 
 })
 
-server.get('/api/zoos/:id', (req, res) => {
+server.get('/api/zoos/:id', (req, res) => {})
 
-})
+server.delete('/api/zoos/:id', (req, res) => {})
 
-server.delete('/api/zoos/:id', (req, res) => {
+server.put('/api/zoos/:id', (req, res) => {})
 
-})
-
-server.put('/api/zoos/:id', (req, res) => {
-  
-})
-
-const port = 3300;
+const port = 3300
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
-});
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
+})
--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './lambda.sqlite3'
+      filename: './data/lambda.sqlite3'
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true
   },
 }
