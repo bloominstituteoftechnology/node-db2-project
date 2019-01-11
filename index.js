8a648fc4ddf789f9c91667e878a4615819813084
cocoitali <cmburatto@gmail.com>
2019-01-10 20:52:55 -0700

    MVP : created skeletons for CRUD endpoints

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+
+})
+
+server.get('/api/zoos', (req, res) => {
+
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+
+})
+
+server.delete('/api/zoos/:id', (req, res) => {
+
+})
+
+server.put('/api/zoos/:id', (req, res) => {
+  
+})
 
 const port = 3300;
 server.listen(port, function() {
