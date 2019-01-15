ed76b15e67c2fbf9c0a9f63ac82b3b5ced6113f1
ToMMaaY <thuynh629@gmail.com>
2019-01-14 20:07:44 -0800

    Hooked up the Post endpoint

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile");
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +11,27 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get("/api/zoos", async (req, res) => {
+  res.send(`It's working`);
+  // const zoos = await db.get("zoos");
+  // try {
+  //   res.json(zoos);
+  // } catch (err) {
+  //   res
+  //     .status(500)
+  //     .json({ error: "Unable to retrieve the Zoos data from the server" });
+  // }
+});
+
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+
+  db.insert(zoo)
+    .into("zoos")
+    .then(id => res.status(201).json(id))
+    .catch(err => res.json({ error: "Unable to add a new zoo." }));
+});
 
 const port = 3300;
 server.listen(port, function() {
