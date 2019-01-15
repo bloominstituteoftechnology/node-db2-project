c0775d4dcf85be15dea5f27ffd6ec2315ce0129f
chasefulks <chase.fulks@gmail.com>
2019-01-15 17:48:31 -0600

    working

--- index.js
+++ index.js
@@ -1,12 +1,83 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const knex = require("knex");
+const helmet = require("helmet");
 
+const dbConfig = require("./knexfile");
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post("/api/zoos", (req, res) => {
+  const animal = req.body;
+  db("zoos")
+    .insert(animal)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ err: "Failed to insert animal" });
+    });
+});
+
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(rows => {
+      res.json(rows);
+    })
+    .catch(err => {
+      res.status(404);
+      res.json(`Couldn't find any animals`);
+    });
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  const [id] = req.params.id;
+  db("zoos")
+    .where("id", id)
+    .then(rows => {
+      res.json(rows);
+    })
+    .catch(err => {
+      res.status(404);
+      res.json(`Couldn't find that specific animal`);
+    });
+});
+
+server.put("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  const animal = req.body;
+
+  db("zoos")
+    .where("id", id)
+    .update(animal)
+    .then(rowCount => {
+      res.status(201).json(rowCount);
+    })
+    .catch(err => {
+      res.status(500);
+      res.json(`update failed`);
+    });
+});
+
+server.delete("/api/zoos/:id", (req, res) => {
+
+  const { id } = req.params;
+  const animal = req.body;
+
+  db("zoos")
+    .where("id", id)
+    .del()
+    .then(rowCount => {
+      res.status(201).json(rowCount);
+    })
+    .catch(err => {
+      res.status(500).json("failed to delete");
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
