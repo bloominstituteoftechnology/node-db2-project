3ccbaa7a85ad569e5caaae3533b589deaac0d025
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-01-14 21:16:04 -0600

    Express route for zoos table set up

---
+++ data/routes/zoosRoute.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const knex = require('knex')
+const dbConfig = require('../../knexfile')
+const db = knex(dbConfig.development)
+
+//endpoints
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex')
+
+const dbConfig = require('./knexfile')
+const db = knex(dbConfig.development)
+
+const zoosRoute = require('./data/routes/zoosRoute')
 
 const server = express();
-
 server.use(express.json());
 server.use(helmet());
 
+
 // endpoints here
+//Zoos Route
+server.use('/api/zoos', zoosRoute)
 
 const port = 3300;
 server.listen(port, function() {
