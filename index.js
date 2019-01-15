ffc2a31d1c46c364f2bc3d1927b33caff5ca08d5
Lidii <lidiiany92@gmail.com>
2019-01-15 15:28:06 -0500

    Configured knexfile and added get zoos endpoint

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+
+
+//connection to the data base
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +13,23 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+//POST /api/zoos
+server.post('/api/zoos', async (req, res) => {
+
+});
+
+
+//GET /api/zoos
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos  = await db('zoos');
+    res.status(200).json(zoos);
+  }
+  catch (err) {
+    res.status(500).json({message: "There was an error while trying to connect to the data base"});
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
