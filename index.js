4caa9f3f565b6d4a720b6886d548b4ada243b3c7
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-01-15 13:30:17 -0800

    Initial commit, server running

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
 
 const server = express();
 
@@ -7,6 +9,35 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+// conect to the database
+const db = knex(knexConfig.development);
+
+server.get('/', (req, res) => {
+	res.send('api working');
+});
+
+// list bears
+server.get('/api/bears', (req, res) => {
+	db('bears')
+		.then(bears => {
+			res.status(200).json(bears);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+// add pandas
+server.post('/api/bears', (req, res) => {
+	db('bears')
+		.insert(req.body)
+		.then(ids => {
+			res.status(201).json(ids);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
+
 
 const port = 3300;
 server.listen(port, function() {
