d2a57519e096550c98c2b83726536960df281829
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-01-15 15:05:59 -0800

    Add MVP and stretch

--- index.js
+++ index.js
@@ -8,28 +8,19 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
-
 // conect to the database
 const db = knex(knexConfig.development);
 
+//endpoints
 server.get('/', (req, res) => {
 	res.send('api working');
 });
 
-// list bears
-server.get('/api/bears', (req, res) => {
-	db('bears')
-		.then(bears => {
-			res.status(200).json(bears);
-		})
-		.catch(err => res.status(500).json(err));
-});
-
-// add pandas
-server.post('/api/bears', (req, res) => {
-	db('bears')
-		.insert(req.body)
+// add whatever to whatever table
+server.post('/api/:table', (req, res) => {
+	const body = req.body;
+	db(req.params.table)
+		.insert(body)
 		.then(ids => {
 			res.status(201).json(ids);
 		})
@@ -38,8 +29,51 @@ server.post('/api/bears', (req, res) => {
 		});
 });
 
+// list whatever
+server.get('/api/:table', (req, res) => {
+	db(req.params.table)
+		.then(things => {
+			res.status(200).json(things);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+// list whatever by ID
+server.get('/api/:table/:id', (req, res) => {
+	const id = req.params.id;
+	db(req.params.table)
+		.where({id: id})
+		.then(thing => {
+			res.status(200).json(thing);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+// delete whatever
+server.delete('/api/:table/:id', (req, res) => {
+	const id = req.params.id;
+	db(req.params.table)
+		.where({id: id})
+		.del()
+		.then(zoos => {
+			res.status(200).json(zoos);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+// update whatever
+server.put('/api/:table/:id', (req, res) => {
+	const id = req.params.id;
+	db(req.params.table)
+		.where({id: id})
+		.update(req.body)
+		.then(zoos => {
+			res.status(200).json(zoos);
+		})
+		.catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 	development: {
 		client: 'sqlite3',
 		connection: {
-			filename: './data/lambda.splite3'
+			filename: './data/lambda.sqlite3'
 		}
 	}
 };
