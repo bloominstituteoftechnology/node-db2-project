9b571a73edcf996d960bd5671314674dc1323d26
hraya <hraya@lssu.edu>
2019-01-15 23:54:43 -0600

    Completed Stretch
    Added bears database
    
    completed the CRUD operations for the bears db
    
    will attempt to make routes and Models file

--- index.js
+++ index.js
@@ -88,8 +88,83 @@ server.put('/api/zoos/:id', (req, res) =>{
   }).catch(err =>{
     res.status(500).json({error:"Could not update the Zoo!"})
   })
+})
+
+// beginning the bears endpoints
+server.post('/api/bears', (req, res) =>{
+  const bear = req.body;
+  if(!bear.name){res.status(400).json({error:"Please include a name!"})}
+    db('bears').insert(bear)
+  .then(ids=>{
+    res.status(201).json(ids)
+  })
+  .catch(err =>{
+    res.status(500).json({error:"Unable to add the bear."})
+  })
+})
+
+server.get('/api/bears', (req, res) =>{
+  db('bears')
+  .then(bears =>{
+    if(bears.length > 0){
+      res.json(bears)
+    }else{
+      res.status(404).json({error:"No bears have been added"})
+    }
+  })
+  .catch(err =>{
+    res.status(500).json({error:"Unable to retrieve the bears from the database!"})
+  })
+  
+})
+
+server.get('/api/bears/:id', async (req, res) =>{
+ try {
+   const { id } = req.params
+
+   const bear = await db('zoos')
+   .where({ id })
+   .first()
+   if(bear){
+     res.json(bear)
+   }else{
+     res.status(404).json({error: "The bear with the specified id does not exist!"})
+   }
+ } catch(err) {
+    res.status(500).json({error:"Could not retrieve information from the database!"})
+ }
 
   
+})
+
+server.delete('/api/bears/:id', (req, res) =>{
+  const { id } = req.params;
+  db('bears').where({ id }).del()
+  .then(deletedBear =>{
+    if(deletedBear){
+      res.status(201).json({message:"Bear deleted"})
+    }else{
+      res.status(404).json({error:"Unable to delete specified Bear!"})
+    }
+  })
+})
+
+server.put('/api/bears/:id', (req, res) =>{
+  const { id } = req.params;
+  const bear = req.body;
+
+  db('bears').where({ id }).update(bear)
+  .then(count =>{
+    if(count){
+      db('bears').where({ id }).then(updated =>{
+        res.status(201).json(updated)
+      }).catch(err =>{
+        res.status(404).json({error:"No records found to updated"})
+      })
+    }
+  }).catch(err =>{
+    res.status(500).json({error:"Could not update the bear!"})
+  })
 })
 
 server.listen(port, function() {
