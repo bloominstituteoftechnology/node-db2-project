c5cd8d778cb9030a5ee4a82d2e8c1e80b08ad1c9
Victor G <victorgordian103@gmail.com>
2019-01-23 20:58:22 -0800

    MVP done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -7,6 +10,64 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoo', (req,res)=>{
+  const zoo = req.body;
+  db('zoos').insert(zoo)
+  .then(ids =>{
+    res.status(201).json(ids);
+  })
+  .catch(err=>{
+    res.status(500).json({err: 'Failed to insert zoo animal'})
+  })
+});
+
+server.get('/api/zoo', (req,res)=>{
+  db('zoos').then(row =>{
+    console.log(row)
+    res.json(row);
+  }).catch(err =>{
+    res.status(500).json({err: 'failed to find zoo animals'})
+  })
+});
+
+
+server.get('/api/zoo/:id', (req,res)=>{
+  const {id} = req.params;
+  db('zoos').where('id',id)
+  .then(rows =>{
+    console.log(rows)
+    res.json(rows);
+  }).catch(err=>{
+    res.status(500).json({ err: 'Failed to find zoo animal'})
+  });
+});
+
+
+server.put('/api/zoo/:id', (req,res) =>{
+  const {id} = req.params;
+  const name = req.body;
+
+  db('zoos').where('id',id).update(name)
+  .then(rowCount =>{
+    res.status(200).json(row)
+  })
+  .catch(err =>{
+    res.status(500).json({err: 'Failed to update zoo'})
+  });
+});
+
+
+server.delete('/api/zoo/:id', (req, res)=>{
+  const {id} = req.params;
+  db('zoos').where('id',id).del()
+  .then(row =>{
+    res.status(200).json(row)
+  })
+  .catch(err=>{
+    res.status(500).json({err: 'Failed to delete animal'})
+  });
+});
 
 const port = 3300;
 server.listen(port, function() {
