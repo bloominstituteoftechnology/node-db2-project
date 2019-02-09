94871fc8c3fbdbcda5a84e7d2fcfb0cb57d22beb
Jesse Theisen <theiseja@gmail.com>
2019-02-09 08:03:18 -0600

    MVP: Project Setup and knex configuration complete

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const helmet = require('helmet');
+const logger = require('morgan');
+const knex = require('knex');
+const knexDB = require('./knexfile');
+const DB = knex(knexDB.development);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(logger('tiny'));
 
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+  DB('zoos')
+  .then(zoos => {
+    res.json(zoos)
+  })
+  .catch(() => {
+    res.status(500).json({error: 'Unable to retrieve list of zoos from the DB.'})
+  })
+})
+
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params
+  DB('zoos')
+  .where('id', id)
+  .then(rows => {
+    res.json(rows)
+  })
+  .catch(() => {
+    res.status(500).json({ error: 'Failed to find a zoo with this ID in the DB.'})
+  })
+})
+
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body
+  if (zoo.name) {
+    DB('zoos')
+      .insert(zoo)
+      .then(ids => {
+        res.status(201).json(ids)
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ error: 'Failed to insert the zoo into the database.' })
+      })
+  } else {
+    res.status(400).json({ error: 'Please provide a name for the zoo' })
+  }
+})
+    
 
 const port = 3300;
 server.listen(port, function() {
