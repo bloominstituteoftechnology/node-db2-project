43e8d6da270817f7d42b10e73e990d3dc62ecfbc
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-19 15:42:18 -0500

    Successful DELETE at /api/zoos/:id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-const knexConfig = require('./knexfile');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 
 const db = knex(knexConfig.development);
 
@@ -11,11 +11,12 @@ server.use(express.json());
 server.use(helmet());
 
 //POST /api/zoos
-server.post('/api/zoos', async (req, res) => {
+server.post("/api/zoos", async (req, res) => {
   try {
-    const [id] = await db('zoos').insert(req.body); //this works
-    
-    const zoo = await db('zoos').where({ id }).first();
+    const [id] = await db("zoos").insert(req.body);
+    const zoo = await db("zoos")
+      .where({ id })
+      .first();
 
     res.status(201).json(zoo);
   } catch (error) {
@@ -24,9 +25,9 @@ server.post('/api/zoos', async (req, res) => {
 });
 
 //GET /api/zoos
-server.get('/api/zoos', async (req, res) => {
+server.get("/api/zoos", async (req, res) => {
   try {
-    const zoos = await db('zoos');
+    const zoos = await db("zoos");
     res.status(200).json(zoos);
   } catch (error) {
     res.status(500).json(error);
@@ -34,11 +35,13 @@ server.get('/api/zoos', async (req, res) => {
 });
 
 //GET /api/zoos/:id
-server.get('/api/zoos/:id', async (req, res) => {
+server.get("/api/zoos/:id", async (req, res) => {
   try {
-    const zoo = await db('zoos').where({id: req.params.id})
+    const zoo = await db("zoos").where({ id: req.params.id });
     if (zoo.length === 0) {
-      res.status(404).json({error: "The specified ID does not exist in the database."})
+      res
+        .status(404)
+        .json({ error: "The specified ID does not exist in the database." });
     } else {
       res.status(200).json(zoo);
     }
@@ -48,17 +51,25 @@ server.get('/api/zoos/:id', async (req, res) => {
 });
 
 //DELETE /api/zoos/:id
-server.delete('/api/zoos/:id', (req, res) => {
+server.delete("/api/zoos/:id", async (req, res) => {
+  try {
+    const records = await db('zoos').where({id: req.params.id}).del();
+    if (records === 0) {
+      res.status(404).json({error: "A record with the specified ID does not exist and cannot be deleted."})
+    } else {
+      res.status(204).end();
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 
 });
 //PUT /api/zoos/:id
-server.put('/api/zoos/:id', (req, res) => {
-
-});
+server.put("/api/zoos/:id", (req, res) => {});
 
 const errors = {
-
-}
+  "19": "A record with that name already exists. Please provide a unique name."
+};
 
 const errorMessage = "There was an error.";
 
