abf8169b76f354081a6894d0b50a395eac03c3f3
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-19 12:04:43 -0500

    Create knexfile and import to index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
 const server = express();
 
 server.use(express.json());
