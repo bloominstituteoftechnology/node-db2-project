21bf86b6a742884abd25e92318080c44edcc17e8
john2796 <jbmiranda22796@gmail.com>
2019-02-19 12:38:45 -0800

    init

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const logger = require("morgan");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
-
+server.use(logger("dev"));
 // endpoints here
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+  console.log(`\n
+  -----------------------------------------------------
+   Web API Listening on http://localhost:${port} ===\n
+  -----------------------------------------------------
+   `);
 });
