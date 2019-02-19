e8afeeefcaf1963e6488e13cbf0d4205e9e3a266
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-19 15:14:03 -0500

    Successful GET at /api/zoos

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const helmet = require('helmet');
-
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 
@@ -12,6 +11,41 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+//POST /api/zoos
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const id = await db('zoos').insert(req.body);
+    console.log(id);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//GET /api/zoos
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//GET /api/zoos/:id
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos').where({id: req.params.id})
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//DELETE /api/zoos/:id
+server.delete('/api/zoos/:id', (req, res) => {
+
+});
+//PUT /api/zoos/:id
+server.put('/api/zoos/:id', (req, res) => {
+
+});
 
 const port = 3300;
 server.listen(port, function() {
