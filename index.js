59d7487b5d8429dfd53821fb3b982e746de35120
john2796 <jbmiranda22796@gmail.com>
2019-02-19 12:43:44 -0800

    adding knex config

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const helmet = require("helmet");
 const logger = require("morgan");
-
+const cors = require("cors");
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(logger("dev"));
-// endpoints here
+server.use(cors());
 
-const port = 3300;
+const port = 4000;
 server.listen(port, function() {
   console.log(`\n
   -----------------------------------------------------
