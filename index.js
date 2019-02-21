ddebdeb0efba3072a608186ead7243d37a4b4144
unknown <jerrardsmith215@gmail.com>
2019-02-20 23:15:07 -0500

    Server configured

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const helmet = require('helmet');
-
-const server = express();
-
-server.use(express.json());
-server.use(helmet());
-
-// endpoints here
-
-const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+// basic setup for server
+const express = require('express'); // call in express
+const server = express(); //
+server.use(express.json()); //
+const port = 8000; // create port for server to listen on
+server.listen(port, function() { // server listens on port 8000
+  console.log(`===Web API Listening on http://localhost:${port}===`); // notification via console of server activity
 });
+
+// endpoints 
+server.post('/characters', (req, res) => { // create "characters" endpoint
+  //to be filled
+});
+
