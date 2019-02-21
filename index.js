aea36d34ac1bc3b04ad97ccfbe8130b3cdcc8b6a
unknown <jerrardsmith215@gmail.com>
2019-02-21 18:07:11 -0500

    basic CRUD functionality

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ const logger = require('morgan')
 server.use(parser, logger('tiny'), helmet()) // Apply middlewares + parser
 
 /* initialization of CRUD logic */
+//200 - Successful update, send back updated action
+//201 - Created  
+//400 - body information is missing, 
+//404 - Invalid id, item not found
+//500 - Something went wrong, internal error
+//more codes @ https://httpstatuses.com/
 
-//  POST/insert logic
+//  Create logic
 server.post('/api/zoos', (req, res) => { 
   const zoo = req.body 
   if (req.body.name) {
@@ -27,37 +33,65 @@ server.post('/api/zoos', (req, res) => {
   }
 
   else {
-          res
-            .status(500)
-            .json( { message: "Invalid user input (name is required & must be unique!)" })}
+        res
+          .status(500)
+          .json( { message: "Invalid user input (name is required & must be unique!)" })
+        }
 })
 /*END*/
 
-// GET logic
+// Read logic
 server.get('/api/zoos', (req, res) => {
+  db.select()
+    .from('zoos')
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json({ message: 'Server error! Technical error: ' + err}))
 })
+/*END*/
 
-server.listen(port, function() { console.log(`===Web API Listening on http://localhost:${port}/===`)})
+// Read by ID logic
+server.get('/api/zoos/:id', (req, res) => {
+  const { id } = req.params
+  
+    db('zoos')
+      .where({ id: id })
+      .first()
+      .then(zoos => { 
+        if (zoos) { res.status(200).json(zoos) } 
+        else { res.status(404).json({ message: `404 - zoo with id ${id} not found.` })  } 
+      })
+      .catch(err => res.status(500).json(err))
+})
+/*END*/
 
-/*
-server.put('./zoos/id', (req, res) => {
+// Update by ID logic
+server.put('/api/zoos/:id', (req, res) => {
   const changes = req.body  
   const { id } = req.params 
-
+  
   db('zoos')  
-    .where('id', '=', id) 
+    .where({ id: id }) 
     .update(changes)      
-    .then(count => { res.status(200).json(count) }) 
-    .catch(err => { res.status(500).json({ message: "" + err})  
-})                                                
+    .then(count => res.status(200).json(count)) 
+    .catch(err =>  res.status(500).json({ message: "!!! " + err})) 
+})
+/*END*/
 
-server.delete('./zoos/:id', (req, res) => {
+//  Delete by ID logic
+server.delete('/api/zoos/:id', (req, res) => {
   const { id } = req.params
 
   db('zoos')
-    .where({ id }) 
+    .where({ id: id }) 
+    .first()
     .del()
-    .then(count => { res.status(200).json(count) })
+    .then(count => { 
+      if (count) { res.status(200).json(count) } 
+      else { res.status(404).json({ message: `404 - zoo with id ${id} not found.` })  } 
+    })
     .catch(err => { res.status(500).json(err) })
 })
-*/
+
+server.listen(port, function() { 
+  console.log(`===Web API Listening on http://localhost:${port}===`)
+})
