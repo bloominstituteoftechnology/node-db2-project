7264438201b8be2eebebf1e6fdb49919321fbe57
unknown <jerrardsmith215@gmail.com>
2019-02-20 23:42:17 -0500

    basic setup of server and db

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// basic setup for server
+// **basic setup for server & database**
 const express = require('express'); // call in express
 const server = express(); //
 server.use(express.json()); //
@@ -7,7 +7,24 @@ server.listen(port, function() { // server listens on port 8000
   console.log(`===Web API Listening on http://localhost:${port}===`); // notification via console of server activity
 });
 
-// endpoints 
+const knex = require('knex') // initialize knex
+const knexConfig = require('./knexfile')// use 'knexfile' config file to configure knex
+const db = knex(knexConfig.development) //  intialize server with its configuration according to the 
+                                        //  'knexConfig' object's developement key
+
+/* classical way of intitializing db without using 'knexfile' configuration file
+
+const db = knex({ // intialize database
+  client: 'sqlite3',
+  connection: { // define database connection
+    filename: './data/db.sqlite3' // define database file location
+  },
+  useNullAsDefault: true,
+}); // Moving forward, the 'db' constant will denote our database!
+
+*/
+
+// *
 server.post('/characters', (req, res) => { // create "characters" endpoint
   //to be filled
 });
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/lambda.sqlite3'
+      filename: './data/db.sqlite3'
     },
 	useNullAsDefault: true, //new configuration for SQLite
   },
