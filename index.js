c4d29f8845964cc39c343b0e2e133bb6e282c5f8
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-26 16:47:45 -0500

    Initial Commit
    - added dependencies
    - set up server.js, added zoos-router.js, reconfigured router to use zoos-router instead of index
    - set up knex config
    - GET to root is functional and connects to zoos table

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const express = require('express');
-const helmet = require('helmet');
+const server = require("./api/server.js");
 
-const server = express();
-
-server.use(express.json());
-server.use(helmet());
-
-// endpoints here
-
-const port = 3300;
+const port = process.env.PORT || 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
---
+++ zoos/zoos-router.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  }
+};
+
+const db = knex(knexConfig);
+// endpoints here
+
+router.get("/", (req, res) => {
+  // get the roles from the database
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
