934971139ed26cbfea89a83333da32f05c5e165d
Jesse Theisen <theiseja@gmail.com>
2019-03-16 12:36:54 -0500

    MVP: GET, GET BY ID and post work and tested

--- index.js
+++ index.js
@@ -18,10 +18,45 @@ zooDB('zoos')
 .then(zoos => {
   res.json(zoos);
 })
-})
+.catch((error) => {
+  res.status(500).json({ error: 'Unable to retrieve list of zoos from the DB.'});
+});
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  zooDB("zoos")
+    .where("id", id)
+    .then(rows => {
+      res.json(rows);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "Failed to find a zoo with this ID in the DB." });
+    });
+});
+
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+  if (zoo.name) {
+    zooDB("zoos")
+      .insert(zoo)
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ error: "Failed to insert the zoo into the database." });
+      });
+  } else {
+    res.status(400).json({ error: "Name required to insert zoo into the DB." });
+  }
+});
 
 
-const port = 3300;
+const port = 9090;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
