c7b3b9f334563692492c8e9429b04668990435a1
erscriber <erscriber@gmail.com>
2019-03-15 22:25:20 -0400

    Stretch goals complete

--- index.js
+++ index.js
@@ -121,6 +121,114 @@ server.delete('/api/zoos/:id', async (req, res) => {
 	}
 });
 
+
+
+
+// STRETCH - BEARS ENDPOINTS
+
+// GET
+server.get('/api/bears', async (req, res) => {
+	try {
+		const allBears = await zooDb('bears');
+		res
+			.status(200)
+			.json(allBears);
+	}
+	catch (error) {
+		res
+			.status(500)
+			.json(error);
+	}
+});
+
+
+// GET ALL by ID
+server.get('/api/bears/:id', async (req, res) => {
+	try {
+		const bearById = await zooDb('bears')
+			.where({ id: req.params.id })
+			.first();
+		res
+			.status(200)
+			.json(bearById);
+	}
+	catch (error) {
+		res
+			.status(500)
+			.json({ message: 'No entry with specified ID.' });
+	}
+});
+
+// POST
+server.post('/api/bears', async (req, res) => {
+	try {
+		const [id] = await zooDb('bears')
+			.insert(req.body);
+
+		const newBear = await zooDb('bears')
+			.where({ id })
+			.first()
+
+		res
+			.status(201)
+			.json(newBear);
+	}
+	catch (error) {
+		res
+			.status(500)
+			.json({ message: 'New bear cannot be saved at this time.' });
+	}
+});
+
+// PUT
+server.put('/api/bears/:id', async (req, res) => {
+	try {
+		const count = await zooDb('bears')
+			.where({ id: req.params.id })
+			.update(req.body);
+
+		if(count > 0) {
+			const updatedBear = await zooDb('bears')
+				.where({ id: req.params.id })
+				.first();
+			res
+				.status(200)
+				.json(updatedBear);
+		}
+		else {
+			res
+				.status(404)
+				.json({ message: 'Bear with specified ID cannot be found.' });
+		}
+	}
+	catch (error) {
+
+	}
+});
+
+// DELETE
+server.delete('/api/bears/:id', async (req, res) => {
+	try {
+		const count = await zooDb('bears')
+			.where({ id: req.params.id })
+			.del();
+
+		if(count > 0) {
+			res
+				.status(204)
+				.end();
+		}
+		else {
+			res
+				.status(404)
+				.json({ message: 'Bear with specified ID cannot be found.' });
+		}
+	}
+	catch (error) {
+
+	}
+});
+
 const port = 9090;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on port ${port} ===\n`);
