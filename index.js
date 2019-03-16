1d506a4f8d20df160a14e2e51d97f60c3a6fac86
Jesse Theisen <theiseja@gmail.com>
2019-03-16 13:14:49 -0500

    Stretch: MVP & Stretch complete & tested

--- index.js
+++ index.js
@@ -145,6 +145,51 @@ server.post("/api/bears", (req, res) => {
     });
 });
 
+server.put("/api/bears/:id", (req, res) => {
+  const bear = req.body;
+  const { id } = req.params;
+  if (!bear.name || !id) {
+    res
+      .status(404)
+      .json({ error: "Please provide bear information and/or ID." });
+    return;
+  }
+  zooDB("bears")
+    .where("id", id)
+    .update(bear)
+    .then(id => {
+      if (id) {
+        res.status(201).json(id);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The bear with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The bear could not be updated", err });
+    });
+});
+
+server.delete("/api/bears/:id", (req, res) => {
+  const { id } = req.params;
+  zooDB("bears")
+    .where("id", id)
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The bear with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The bear could not be removed" });
+    });
+});
+
 const port = 9090;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
