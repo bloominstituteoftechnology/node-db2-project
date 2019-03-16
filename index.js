6bd96e123113f4832688adf6ac375977cf9a30e1
Taylorbelk1 <belktaylor12@gmail.com>
2019-03-16 11:56:54 -0500

    finished all CRUD operations

--- index.js
+++ index.js
@@ -12,18 +12,64 @@ server.use(helmet());
 
 // endpoints here
 
-// GET /api/zoos
+// GET test
   server.get('/', (req, res) => {
     res.send('api is running');
   })
 
+// GET /api/zoos
+  server.get('/api/zoos', (req, res) => {
+    db('zoos')
+      .then(zoos => {
+        res.status(200).json(zoos)
+      })
+      .catch(err => res.status(500).json(err))
+  })
 //GET /api/zoos/:id
+  server.get('/api/zoos/:id', (req, res) => {
+    db('zoos')
+      .where({ id: req.params.id })
+      .then(ids => {
+        res.status(200).json(ids)
+      })
+      .catch(err => res.status(500).json(err))
+  })
 
 //POST /api/zoos
-  server.post
+  server.post('/api/zoos', (req, res) => {
+
+    db('zoos')
+      .insert(req.body)
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => res.status(500).json('Unable to add the user'));
+  });
+
 //DELETE /api/zoos/:id
+  server.delete('/api/zoos/:id', (req, res) => {
+    db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => res.status(500).json(err))
+  })
+    
 
 //PUT /api/zoos/:id
+server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body;
+
+  db('zoos')
+  .where({ id: req.params.id })
+  .update(changes)
+  .then(count => {
+    res.status(200).json(count);
+  })
+  .catch(err => res.status(500).json(err))
+})
 
 const port = 3300;
 server.listen(port, function() {
