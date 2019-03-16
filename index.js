43515e6b009188ca735a6dd6f16b299bc6aa692b
Jesse Theisen <theiseja@gmail.com>
2019-03-16 11:54:28 -0500

    MVP: Project Setup, created new branch & added deps

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const helmet = require('helmet');
+const logger = require('morgan');
+const knex = require('knex');
+const knexDB = require('./knexfile');
+const zooDB = knex(knexDB.development);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(logger('tiny'));
 
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+zooDB('zoos')
+.then(zoos => {
+  res.json(zoos);
+})
+})
+
 
 const port = 3300;
 server.listen(port, function() {
