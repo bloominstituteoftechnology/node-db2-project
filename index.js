e7b2e1375793a78c9976613fba88fd0f05234506
Dan Sherman <dvsherman@protonmail.com>
2019-03-26 16:01:39 -0700

    setting up endpoints, added /api/zoos get

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  debug: true
+};
+
+const db = knex(knexConfig);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(roles => {
+      res.status(200).json(roles);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 const port = 3300;
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
