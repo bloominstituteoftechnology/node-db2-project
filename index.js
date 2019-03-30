d5254614a5f41a4f990e7ec7688a08b3273b0736
iradsanchez <ira.d.sanchez@gmail.com>
2019-03-29 19:17:33 -0700

    Add POST

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const knexConfig = require("./knexfile");
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +11,27 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post("/api/zoos", (req, res) => {
+  const zoo = req.body;
+
+  if (zoo.name) {
+    db.insert(zoo)
+      .into("zoos")
+      .then(id => {
+        res.status(201).json(id);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({
+      error: `The zoo doesn\'t even have a name? That's sad. Let's give it one and try again`
+    });
+  }
+});
+
+//
 
 const port = 3300;
 server.listen(port, function() {
