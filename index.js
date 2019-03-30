f24dd0fa3a0169e1eb8b07b5bdc8f8e75193f4a1
Ken Barlow <kenbrlow@gmail.com>
2019-03-29 19:10:44 -0700

    Setup the requirements for knex. Added knexConfig and created db from knexConfig

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 
 const server = express();
 
