47723125404c6c3f4b28c13094206dae41a5cb57
Ken Barlow <kenbrlow@gmail.com>
2019-03-29 19:34:35 -0700

    Created zoos table

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ server.use(helmet());
 
 // endpoints here
 
+// GET
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {res.status(200).json(zoos)})
+  .catch((err) => {
+    res.status(500).json({ error: 'could not load zoos', err});
+  });
+})
+
+// POST
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+//   db.insert(zoo)
+//   .into('zoos')
+//   .then( zoo => {
+//     res.status(201).json({message: 'successfully created zoo'})
+//   })
+//   .catch( err => {
+//     res.status(500).json({error: 'error creating new zoo', err})
+//   })
+});
+
+
+// PUT
+
+
+// DELETE
+
+
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
