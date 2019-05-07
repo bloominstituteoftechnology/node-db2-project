5d6eddfd329544ff979f5a729a5db7e8f737f6bf
sriley <sarahriley.dev@gmail.com>
2019-05-07 17:17:43 -0400

    all endpoints working

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ server.use(helmet());
 // endpoints here
 
 server.get('/api/zoos', (req, res) => {
-  // get the roles from the database
   db('zoos')
   .then(zoos => {
     res.status(200).json(zoos)
@@ -30,6 +29,78 @@ server.get('/api/zoos', (req, res) => {
   })
 });
 
+server.get('/api/zoos/:id', (req, res) => {
+db('zoos')
+.where({id: req.params.id})
+.then(zoo => {
+  if(zoo) {
+    res.status(200).json(zoo);
+
+  }else {
+    res.status(404).json({message: 'Zoo id not found'})
+  }
+})
+.catch(error => {
+  res.status(500).json(error);
+})
+});
+
+server.post('/api/zoos', (req, res) => {
+ 
+  db('zoos')
+  .insert(req.body)
+  .then(role => {
+    const [id] = role;
+
+    db('zoos')
+    .where({id})
+    .first()
+    .then(zoo => {
+      res.status(200).json(zoo)
+    })
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+ 
+  db('zoos')
+  .where({id: req.params.id})
+  .update(req.body)
+  .then(count => {
+    if(count > 0){
+      db('zoos')
+      .where({id: req.params.id})
+      .first()
+      .then(zoo => {
+        res.status(200).json(zoo)
+      })
+    }else{
+      res.status(404).json({message: 'Zoo id not found'})
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({id: req.params.id})
+  .del() 
+  .then(count => {
+    if(count > 0) {
+      res.status(204).end();
+    }else{
+      res.status(404).json({message: 'Zoo id not found'})
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+});
 
 
 const port = 3300;
