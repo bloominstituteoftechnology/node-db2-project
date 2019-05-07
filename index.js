beb1814d00c0538faf53b2f6934d2da133009748
sriley <sarahriley.dev@gmail.com>
2019-05-07 16:52:47 -0400

    server running and Get request working

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
 const server = express();
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+}
+
+const db = knex(knexConfig);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+  // get the roles from the database
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
