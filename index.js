dbe2438bda325500b323f083f4d005b83fbd23c8
claridiva2000 <claridiva2000@gmail.com>
2019-05-25 12:54:50 -0500

    server keeps crashing until i comment oout knex

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+// const db = knex(knexConfig.developement)
+
 const server = express();
 
 server.use(express.json());
@@ -8,7 +12,66 @@ server.use(helmet());
 
 // endpoints here
 
-const port = 3300;
+//add
+server.post('/api/zoos', (req, res)=>{
+  db('zoos').insert(req.body)
+  .then(zoo => res.status(201).json(zoo))
+  .catch(err => res.status(500).json(err))
+})
+
+//get all
+server.get('api/zoos', (req, res)=>{
+  db.select().from('zoos')
+  .then(zoos => res.status(200).json(zoos))
+  .catch(err => res.status(500).json(err))
+})
+
+//get by id
+server.get('api/zoos/:id', (req, res) =>{
+  const id = req.params.id;
+
+  db.select().where({id}).first().from('zoos')
+  .then(zoo => {
+    if(zoo) {
+      res.status(200).json(zoo)
+    }else {
+      res.status(404).json({message: 'zoo not found'})
+    }
+  })
+  .catch(err => res.status(500).json(err))
+})
+
+//delete
+server.delete('/api/zoos/:id', (req, res) =>{
+  const id = req.params.id;
+  db('zoo').where('id', id).del()
+  .then(del =>{
+    if(del) {
+      res.status(204)
+    } else {
+      res.status(404).json({message: 'zoo not found'})
+    }
+  })
+  .catch(err => res.status(500).json(err))
+})
+
+//update
+server.put('/api/zoos/:id', (req, res) =>{
+const id = req.params.id;
+
+db('zoo').where({ id }).update(req.body)
+.then(zoo => {
+  if(zoo) {
+    res.status(201).json({message: 'zoo updated'})
+  } else {
+    res.status(404).json({message: 'zoo not found'})
+  }
+})
+.catch(err => console.log(err))
+})
+
+
+const port = 5000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lambda.sqlite3'
+    },
+    useNullAsDefault: true,  
+  },
+
+};
