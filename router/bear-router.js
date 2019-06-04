dfb1944b13fb3ef18a2dc59b08cdb81a62a68041
Manju KC <Manju KC>
2019-06-04 19:58:03 -0400

    getbyid and updated endpoints are working

--- router/bear-router.js
+++ router/bear-router.js
@@ -21,24 +21,7 @@ router.get("/", (req, res) => {
     });
 });
 
-// router.post('/', (req, res) => {
-//   db('bears')
-//   .insert(req.params.body)
-//   .then(ids => {
-   
-//     const [id] = ids;
 
-//     db(bears)
-//       .where({ id })
-//       .first()
-//       .then(bear => {
-//         res.status(200).json(bear);
-//       });
-//   })
-//   .catch(error => {
-//     res.status(500).json(error);
-//   });
-// });
 
 router.post("/", (req, res) => {
   if (!req.body.name) {
@@ -75,6 +58,41 @@ router.delete("/:id", (req, res) => {
         });
       } else {
         res.status(400).json({ message: "no such id exists" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("bears")
+    .where({ id: req.params.id })
+    .first()
+    .then(bear => {
+      if (bear) {
+        res.status(200).json(bear);
+      } else {
+        res.status(404).json({ message: "no such id exists" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
+router.put("/:id", (req, res) => {
+  db("bears")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: `${count} ${count > 1 ? "records" : "record"} updated`
+        });
+      } else {
+        res.status(400).json({ message: "this zoo does not exist" });
       }
     })
     .catch(err => {
