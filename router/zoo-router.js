33080fba6488815ed1c26a8a4b1fafb20ddeb78f
Manju KC <Manju KC>
2019-06-04 17:02:40 -0400

    getbyid and post is working

--- router/zoo-router.js
+++ router/zoo-router.js
@@ -22,4 +22,44 @@ const knexConfig = {
       });
   });
 
+  router.get("/:id", (req, res) => {
+    db("zoos")
+      .where({ id: req.params.id })
+      .first()
+      .then(zoo => {
+        if (zoo) {
+          res.status(200).json(zoo);
+        } else {
+          res.status(404).json({ message: "no such id exists" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+  router.post("/", (req, res) => {
+    if (!req.body.name) {
+      res.status(400).json({ msg: "please provide a name" });
+    } else {
+      db("zoos")
+        .insert(req.body, "id")
+        .then(ids => {
+          db("zoos")
+            .where({ id: ids[0] })
+            .first()
+            .then(zoo => {
+              res.status(200).json(zoo);
+            })
+            .catch(err => {
+              res.status(500).json(err);
+            });
+        })
+        .call(err => {
+          res.status(500).json(err);
+        });
+    }
+  });
+
+
 module.exports = router;
