31437e3da9ad2ca42683c4fc32b36f31adc62c35
Ernesto Pena <ernestopenad@gmail.com>
2019-08-09 23:31:52 -0500

    All API end points completed

--- api/carsRouter.js
+++ api/carsRouter.js
@@ -16,29 +16,35 @@ carRouter.get('/' , async (req,res) => {
 
 carRouter.post('/' , async (req,res) => {
     try {
-
+        const dataBody = req.body;
+        const newCar = await db('cars').insert(dataBody);
+        res.status(200).json(newCar);
     }
     catch (err) {
-
-
+        res.status(500).json({message: `There was a problem with your POST request`});
     }
 })
 
 carRouter.put('/:id' , async (req,res) => {
     try {
-
+        const updateCar = req.body;
+        const id = req.params.id;
+        const updatedCar = await db('cars').update(updateCar).where('id' , '=' , id);
+        res.status(203).json(updatedCar);
     }
     catch (err) {
-
+        res.status(500).json({message: 'There was a problem with your PUT request'});
     }
 })
 
-carRouter.delete('/:id' , (req,res) => {
+carRouter.delete('/:id' , async (req,res) => {
     try {
-
+        const id = req.params.id;
+        const carDeleted = await db('cars').del().where('id' , '=' , id);
+        res.status(200).json(carDeleted);
     }
     catch (err) {
-
+        res.status(500).json({message:'There was a problem with your DELETE request'})
     }
 })
 
