a368ea50aaa1814c86781f19147cfd962e73c8ec
sweyome <sweyome1@gmail.com>
2019-08-25 19:36:30 -0400

    cars migration

---
+++ migrations/20190825193454_cars-table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments('id');
+        tbl.text('make', 30).notNullable();
+        tbl.text('model' , 30).notNullable();
+        tbl.text('VIN').unique().notNullable();
+        tbl.integer('mileage',10);
+        tbl.text('Transmission');
+        tbl.text('title');
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+  };
+  
