2cd7d7df03f0cdc9e8f873dbf06f0bf93a9e346f
Wayne Mitt <wmitt85@gmail.com>
2019-08-25 12:50:07 -0500

    populated 2 records into cars db, installed knex and sqlite3, ran knex init and edited knexfile

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/car-dealer.db3'
+    },
+    useNullAsDefauly: true
+  },
+
+};
