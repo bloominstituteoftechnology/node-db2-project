98a6e2f1c325da0739226b77ab5b0c3c2fd2ace6
Brandon-Harris <brandonharris177@gmail.com>
2019-10-08 13:44:43 -0600

    starting migrations

---
+++ cars/cars-router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const knex = require('knex');
+
+const knexConfig = require('../knexfile')
+
+const db = knex(knexConfig.development);
+
+const router = express.Router();
+
+module.exports = router;
