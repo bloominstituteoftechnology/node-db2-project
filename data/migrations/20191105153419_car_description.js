831018e98c3e7d0e6d7cfc69f9a9bb80fbc7b57b
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-11-05 17:24:37 -0500

    full crud functionality

--- data/migrations/20191105153419_car_description.js
+++ data/migrations/20191105153419_car_description.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('cars', function(table){
         table.increments();
-        table.string(`vin`, 128).notNullable();
+        table.string(`vin`, 128).notNullable().unique();
         table.string(`make`, 128).notNullable();
         table.string(`model`, 128).notNullable();
         table.integer(`mileage`).notNullable();
--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-const server = require('./server.js');
+const express = require('express');
+const knex = require('./data/dbConfig.js');
+
+
+const server = express();
+// const carRouter = require('./routers/carRouter')
+server.use(express.json());
+
+// server.use('/cars', carRouter)
+
+
+
+
+server.get('/', (req, res) => {
+    res.send('<h3>DB migrations</h3>');
+});
+
+server.get('/cars', (req, res) => {
+    knex('cars').then(carss => {
+        res.status(200).json(carss)
+    })
+    .catch(err => {
+        res.status(500).json({error:"failed to get cars"});
+    })
+});
+
+server.post('/cars',(req,res) => {
+    knex.insert(req.body).into('cars').then(obj => {
+        res.status(200).json(obj)
+    })
+    .catch(err => {
+        res.status(500).json({error:"failed to insert car"});
+    })
+});
+
+server.put('/:id', (req,res) => {
+        const id = req.params.id;
+        const update = req.body;
+
+        knex('cars').where({id:id}).update(update).then(object => {
+            res.status(200).json(object)
+        })
+        .catch(err => {
+            res.status(500).json({error:"failed to update car"});
+        })
+});
+
+server.delete('/:id', (req,res) => {
+    const id = req.params.id;
+    knex('cars').where({id:id}).del().then(count => {
+        res.status(200).json(count)
+    })
+    .catch(err => {
+        res.status(500).json({error:"failed to update post"});
+    })
+})
 
 const PORT = process.env.PORT || 7777;
-
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+    console.log(`Listening on port ${PORT}...`);
 });
