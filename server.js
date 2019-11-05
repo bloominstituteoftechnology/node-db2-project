119dba0c03ad2515d324ac4652886f7c94664ebd
ericbanker12 <ericbanker12@gmail.com>
2019-11-05 15:14:26 -0600

    add GET to receive an array of all cars

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const helmet = require('helmet')
+const knex = require('knex')
 
+const knexfile = require('./knexfile')
+
+const db = knex(knexfile.development)
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
+
+server.get('/api/cars', (req, res) => {
+    db('cars')
+    .then(resp => {
+        // console.log(resp)
+        res.json(resp)
+    })
+    .catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+    })
+})
 
 module.exports = server
