c85f3276bd903e7e69672fa075bdc7bb5cdea8b9
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-05 10:16:51 -0600

    GET for all cars complete

--- car/car-router.js
+++ car/car-router.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
+const db = require('../data/db-config.js')
+router.get('/', async (req, res) => {
+    try {
+        cars = await db('cars')
+        if(cars){
+            res.status(200).json(cars)
+        }
+    }
+    catch(error){
+        res.status(500).json({message: `error in retrieving all cars`})
+    }
+})
 
 module.exports = router;
