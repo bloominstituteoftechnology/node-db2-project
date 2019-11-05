53385a5472410f1265964694680c3668e6a0855e
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-05 10:08:59 -0600

    fixed typo in index and server is up and running

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server.js')
 const port = process.env.PORT || 7777
 
 server.listen(port, () => {
-    console.log(`\n=== Server listening on port ${PORT} ===\n`)
+    console.log(`\n=== Server listening on port ${port} ===\n`)
 })
