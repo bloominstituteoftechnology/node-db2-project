e8500df54435e56eaf9304da45f83ce829d0bb6c
Jashele T <jashele@Jasheles-Air.domain>
2019-11-15 20:10:18 -0700

    getting an error res is not defined

--- seeds/01-cars.js
+++ seeds/01-cars.js
@@ -1,5 +1,5 @@
 
-exports.seed = function(knex) {
+exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('cars').truncate()
     .then(function () {
@@ -9,24 +9,8 @@ exports.seed = function(knex) {
         // transmission type and status of the title (clean, salvage, etc.),
         
         {
-          VIN: 'K0139DKS9382DKSL',
-          Make: 'Acura',
-          Model: 'MDX',
-          Mileage: '400',
-        },
-        {
-          VIN: 'T29039VSJDK3932L',
-          Make: 'Ford',
-          Model: 'Edge',
-          Mileage: '10',
-        },
-        {
-          VIN: 'KDLS909202FDLSL2',
-          Make: 'Ford',
-          Model: 'Transit',
-          Mileage: '200',
+          VIN: 'K0139DKS9382DKSL', MAKE: 'Acura', MODEL: 'MDX',  MILEAGE: '400',
         }
-
 
       ]);
     });
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const carRouter = require('./cars/cars-router.js')
+const carsRouter = require('./cars/cars-router.js')
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/accounts', carRouter)
+server.use('/api/cars', carsRouter)
 
 server.get("/", (req, res) => {
    res.send(` 
