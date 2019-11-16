15cb826d6a4ef0923f72be83ee1c514e51f97a4c
seongck <seongk@pm.me>
2019-11-15 19:56:39 -0800

    Add PUT and DELETE methods

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -26,11 +26,39 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/', async (req, res) => {
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
   const carChanges = req.body;
 
   try {
+    const count = await db('cars').where({ id }).update(carChanges);
+
+    if( count > 0 ) {
+      const updatedCarEntry = await db('cars').where({ id });
+      res.status(201).json(updatedCarEntry);
+    } else {
+      res.status(404).json({ message: 'Car data was not updated' });
+    }
   } catch (error) {
+    console.log('Put error', error);
+    res.status(500).json({ message: 'Failed to update data', error });
+  }
+
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const count = await db('cars').where({ id }).del();
+    if( count > 0) {
+      res.status(200).json({ message: 'Car data successfully deleted' });
+    } else {
+      res.status(404).json({ message: 'Failed to delete car data' });
+    }
+  } catch(err) {
+    console.log('Delete error', error);
+    res.status(500).json({ message: 'Failed to delete data', error });
   }
 
 });
