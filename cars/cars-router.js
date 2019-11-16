dcfde1cd4b26059a3b01e255c2d3c5f5d98cbbee
Jashele T <jashele@Jasheles-Air.domain>
2019-11-15 20:15:46 -0700

    redid router file, api running in postman

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -2,29 +2,19 @@ const express = require("express");
 const db = require("../data/dbConfig.js");
 const router = express.Router();
 
-
-
-// READ /api/cars
-router.get("/", () => {
-   db('cars')
-   .select('*')
-   .then(cars => {
-      res.status(200).json(cars)
-   })
-   .catch(err => {
+router.get("/", (req, res) => {
+  db("cars")
+    .select("*")
+    .then(carData => {
+      res.status(200).json(carData);
+    })
+    .catch(err => {
       res.status(500).json({
-         Message: "Problem with database."
-      })
-   })
+        Message: "Problem with the database. "
+      });
+    });
 });
 
-
-// CREATE  /api/cars
-router.post("/", () => {
-   const carData = req.body;
-
-});
-
-
+router.post("/", (req, res) => {});
 
 module.exports = router;
