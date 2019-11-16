e9c67575db9c0e010c33f4a6fae51dda612d56f9
seongck <seongk@pm.me>
2019-11-15 18:21:50 -0800

    Set up index.js and server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const carsRouter = require('./cars/cars-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/cars', carsRouter);
+
+module.exports = server;
