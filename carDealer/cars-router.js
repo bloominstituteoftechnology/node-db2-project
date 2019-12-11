287cf80aeaa1f586bc288565c3cbdd7716073c6f
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-10 20:27:31 -0600

    set up routes for reading and adding data

--- carDealer/cars-router.js
+++ carDealer/cars-router.js
@@ -11,4 +11,52 @@ const db = knex({
 
 const router = express.Router();
 
+// get all cars
+router.get('/', (req, res) => {
+  db('cars')
+  .then(cars => {
+    res.status(200).json(cars);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ message: 'failed to retrieve cars'})
+  })
+})
+
+// get cars by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+
+  db('cars')
+  .where({ id })
+  .first()
+  .then(cars => {
+    res.status(200).json(cars)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ errorMessage: 'failed to retrieve car by id'})
+  })
+})
+
+router.post('/', (req, res) => {
+  const carData = req.body;
+  if (carData.vin && carData.make && carData.modle && carData.mileage) {
+    db('cars')
+    .insert(carData)
+    .then(cars => {
+    return db('cars')
+      .where({ id: cars[0] })
+      .then(newCar => {
+        res.status(201).json(newCar)
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: 'failed to insert car' })
+    })
+  }
+
+})
+
 module.exports = router;
