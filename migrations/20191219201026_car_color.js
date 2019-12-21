e2aeff33590cc0814ba5dd5d0653969965b95afb
spettigrew <sara.pettigrew5@gmail.com>
2019-12-20 19:29:57 -0700

    Migration file setup

--- migrations/20191219201026_car_color.js
+++ migrations/20191219201026_car_color.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+  await knex.schema.createTable("care", (cars) => {
+      table.increments("id")
+      table.float("VIN").notNull().unique()
+      table.text("make")
+      table.text("model")
+      table.integer("mileage")
+      table.text("color")
+  })
 };
 
 exports.down = function(knex) {
-  
+  await.knex.schema.dropTableIfExists("cars")
 };
