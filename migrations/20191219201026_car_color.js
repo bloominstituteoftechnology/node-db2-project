6233e75bfbb45f2a577152b7450a8e8bcf8297aa
spettigrew <sara.pettigrew5@gmail.com>
2020-01-02 16:55:21 -0700

    Knex migrations updated

--- migrations/20191219201026_car_color.js
+++ migrations/20191219201026_car_color.js
@@ -1,15 +1,11 @@
 
-exports.up = function(knex) {
-  await knex.schema.createTable("cars", (table) => {
+exports.up = async function(knex) {
+  await knex.schema.createTable("car color", (table) => {
       table.increments("id")
-      table.float("VIN").notNull().unique()
-      table.text("make").notNull()
-      table.text("model").notNull()
-      table.integer("mileage").notNull()
       table.text("color")
   })
 };
 
-exports.down = function(knex) {
-  await.knex.schema.dropTableIfExists("cars")
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("car color")
 };
--- migrations/20200102164019_cars.js
+++ migrations/20200102164019_cars.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable("cars", (table) => {
+        table.increments("id")
+        table.float("VIN").notNull().unique()
+        table.text("make").notNull()
+        table.text("model").notNull()
+        table.integer("mileage").notNull()
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("cars")
 };
