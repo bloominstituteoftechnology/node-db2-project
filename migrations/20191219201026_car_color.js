6c951e38db99fcf9f3e911ed258f0b9533a7d64e
spettigrew <sara.pettigrew5@gmail.com>
2020-01-02 17:12:01 -0700

    Migration table updated

--- migrations/20191219201026_car_color.js
+++ migrations/20191219201026_car_color.js
@@ -1,11 +1,12 @@
 
 exports.up = async function(knex) {
-  await knex.schema.createTable("car color", (table) => {
-      table.increments("id")
+  await knex.schema.alterTable("cars", (table) => {
       table.text("color")
   })
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists("car color")
+  await knex.schema.alterTable("cars", (table) => {
+    table.dropColumn("color")
+  })
 };
