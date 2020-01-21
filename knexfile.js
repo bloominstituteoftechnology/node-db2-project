494198c2ff8ef7f592b00b560365d80c1f834203
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-21 17:59:25 -0500

    table work

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/car-dealer.db3'
+    },
+    useNullAsDefault: true,
+
+    migrations: {
+      directory: "./data/migrations"
+    },
   },
 
   staging: {
