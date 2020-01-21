ac1dc3b520121be3eeb7eaa71718e5eaf138c6be
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-21 16:32:42 +0000

    Changed column names

--- data/migrations/20200121144736_create-cars-table.js
+++ data/migrations/20200121144736_create-cars-table.js
@@ -2,8 +2,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable('cars', table => {
         table.increments() // create column named "id", primary key, auto-incrementing
+        table.decimal('vin').unique().notNullable()
         table.text('make', 128).unique().notNullable()
-        table.decimal('avgSpeed').notNullable()
+        table.text('model').notNullable()
+        table.decimal('millage')
         table.boolean('expensive')
     })
 };
--- data/seeds/01-put-cars-in-cars-table.js
+++ data/seeds/01-put-cars-in-cars-table.js
@@ -1,14 +1,14 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('cars').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('cars').insert([
-        { make: 'BMW1', avgSpeed: 120, expensive: 'true'  },
-        { make: 'Audi1', avgSpeed: 110, expensive: 'false'  },
-        { make: 'toyota1', avgSpeed: 100, expensive: 'false'  },
-        { make: 'Ford1', avgSpeed: 100, expensive: 'false'  }
+        {vin: 1234, make: 'BMW1', model: 'opop', millage: 120, expensive: 'true'  },
+        {vin: 12345, make: 'Audi1',model: 'opol',  millage: 110, expensive: 'false'  },
+        {vin: 12346, make: 'toyota1', model: 'opo',  millage: 100, expensive: 'false'  },
+        {vin: 12347, make: 'Ford1',model: 'op',  millage: 100, expensive: 'false'  }
       ]);
     });
 };
+
