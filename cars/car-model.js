f503dcabae7efa25e81444a066abfd723fdf26b8
Annette Odingo <odingoannette@gmail.com>
2020-01-21 12:42:14 +0000

    Create a post endpoint that adds a car to thethe  database

--- cars/car-model.js
+++ cars/car-model.js
@@ -7,7 +7,12 @@ function getCars(){
 function getCarById(id){
     return db('cars').where({id}).first();
 }
+function addNewCar({VIN, make, model, milleage, transmission, status}){
+    return db('cars').insert({ VIN, make, model, milleage, transmission, status
+    })
+}
 module.exports={
     getCars,
-    getCarById
+    getCarById,
+    addNewCar
 }
--- cars/car-router.js
+++ cars/car-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { getCars , getCarById } = require('./car-model');
+const { getCars , getCarById, addNewCar } = require('./car-model');
 const router = express.Router();
 
 router.get('/', async(req, res)=>{
@@ -20,6 +20,16 @@ router.get('/:id', async (req,res)=>{
 
         const car = await getCarById(id);
         res.status(200).json(car)
+    }
+    catch(e){
+        console.log(e);
+    }
+})
+router.post('/', async(req, res)=>{
+    try{
+        const {VIN, make, model, milleage, transmission, status} = req.body;
+    const id = await addNewCar({VIN, make, model, milleage, transmission, status});
+    res.status(200).json(`Car with id ${id} was created`)
     }
     catch(e){
         console.log(e);
