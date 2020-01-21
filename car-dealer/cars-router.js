992e9bc662c1558f555e725fd3d637efa5f60574
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-21 15:45:43 +0000

    post request working

--- car-dealer/cars-router.js
+++ car-dealer/cars-router.js
@@ -25,4 +25,19 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  const carData = req.body;
+  db('cars').insert(carData)
+  .then(ids => {
+    db('cars').where({ id: ids[0] })
+    .then(newCarEntry => {
+      res.status(201).json(newCarEntry);
+    });
+  })
+  .catch (err => {
+    console.log('POST error', err);
+    res.status(500).json({ message: "Failed to store data" });
+  });
+});
+
 module.exports = router;
