1149b57b448aa2f4ad429239160f81378d87d692
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-02-18 11:29:22 -0700

    table created through migration

--- migrations/20200218111145_cars_table.js
+++ migrations/20200218111145_cars_table.js
@@ -1,7 +1,24 @@
 
 exports.up = function(knex) {
   return knex.schem.createTable("cars", tbl => {
-      
+        tbl.increment();
+
+        tbl.integer("VIN Number")
+            .notNullable()
+            .index();
+
+        tbl.string("Make", 128)
+            .notNullable();
+        
+        tbl.string("Model", 128)
+            .notNullable();
+        
+        tbl.integer("Mileage")
+            .notNullable();
+        
+        tbl.string("Transmission Type", 128)
+
+        tbl.string("Title Status", 128)
   })
 };
 
