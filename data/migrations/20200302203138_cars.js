98d3245cdfaa585df5d3c59bb4a5e6e2f95a7beb
CLYPR Technologies <cbanks@clypr.co>
2020-03-02 20:34:04 -0500

    created migrations file

---
+++ data/migrations/20200302203138_cars.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.string('VIN', 17).unique().notNullable();
+        tbl.string('make').notNullable();
+        tbl.string('model').notNullable();
+        tbl.integer('mileage').notNullable();
+        tbl.boolean('automaticTransmission');
+        tbl.string('titleStatus');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+};
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+server.listen(port, () => console.log(`\n** Server is running on port ${port} **\n`));
