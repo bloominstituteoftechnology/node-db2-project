8a7690849d20e25665c871afba810b43455edaac
Anna Morris <annapatton444@gmail.com>
2020-03-03 18:19:42 -0600

    create cars db

---
+++ migrations/20200303180324_cars.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', table => {
+        //ID
+        table.increments('id');
+        // VIN
+        table.integer('VIN').notNull().unique();
+        // Make
+        table.text('make').notNull();
+        // Model
+        table.text('model').notNull();
+        // Mileage
+        table.integer('mileage').notNull();
+        // Status
+        table.text('title')
+      });
+};
+
+exports.down = function(knex) {
+    //Delete
+    return knex.schema.dropTableIfExists('cars')
+};
