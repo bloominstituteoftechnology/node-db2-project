39495e05ff261274e4c8d05834fbaee89c5bd447
Your name <a.m.verdine@gmail.com>
2020-04-27 02:08:12 -0400

    Update migrations files by adding  column to  table

---
+++ migrations/20200427020406_color.js
@@ -0,0 +1,11 @@
+exports.up = async function (knex) {
+  await knex.schema.alterTable("cars", (table) => {
+    table.text("Color");
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.alterTable("cars", (table) => {
+    table.dropColumn("Color");
+  });
+};
