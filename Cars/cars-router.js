3aeb4e4b30a732fb2a98f7f03647384847a74000
Your name <a.m.verdine@gmail.com>
2020-04-27 13:22:52 -0400

    Write GET BY ID & POST requests/Successflly test with Insomnia

--- Cars/cars-router.js
+++ Cars/cars-router.js
@@ -15,4 +15,40 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("cars")
+    .where({ id })
+    .then((cars) => {
+      res.status(200).json(cars);
+    })
+    .catch((err) => {
+      errorMessage: "The selected data could not be retrieved";
+    });
+});
+
+router.post("/", (req, res) => {
+  const newCar = {
+    vin: req.body.vin,
+    year: req.body.year,
+    make: req.body.make,
+    model: req.body.model,
+    mileage: req.body.mileage,
+    price: req.body.price,
+    color: req.body.color,
+  };
+
+  db("cars")
+    .insert(newCar)
+    .then((cars) => {
+      res.status(201).json(cars);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage: "The new entry could not be created",
+      });
+    });
+});
+
 module.exports = router;
