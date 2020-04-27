18272acdab20a0a4ad94fccfdc54a5695d7de9a1
Your name <a.m.verdine@gmail.com>
2020-04-27 02:47:33 -0400

    Write GET request for cars-router/Test successfully in Insomnia

--- Cars/cars-router.js
+++ Cars/cars-router.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const db = require("../Data/db-config");
 
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  db("cars")
+    .then((cars) => {
+      res.status(200).json(cars);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage: "There was an error retreiving this data",
+      });
+    });
+});
+
 module.exports = router;
