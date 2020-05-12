f07b4091f5bb802185e449ac22f15f6c8302dd12
robertelias <robert.elias@live.com>
2020-05-12 12:21:51 -0700

    cars table completed

--- migrations/20200512122008_cars_table.js
+++ migrations/20200512122008_cars_table.js
@@ -1,8 +1,27 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('cars', (tbl) => {
+    //add an id column that auto-increments
+    tbl.increments(); //primary key
+    //VIN
+    tbl.text('vinNumber', 17).unique().notNullable();
+    //make
+    tbl.string('make', 255).notNullable();
 
-exports.up = function(knex) {
-  
+    //model
+    tbl.string('model', 255).notNullable();
+
+    //mileage
+    tbl.string('mileage', 255).notNullable();
+
+    //transmission
+    tbl.string('transmission', 255);
+
+    // title
+    tbl.string('title', 255);
+  });
 };
 
-exports.down = function(knex) {
-  
+// how to undo changes => knex migrate:rollback
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars');
 };
