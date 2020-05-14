c179c17d7f53d0dbb0dcd6067357e8fad3e4f1db
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-05-13 19:03:36 -0700

    completed MVP and stretch (added knex seed data)

--- api/carsRouter.js
+++ api/carsRouter.js
@@ -2,7 +2,31 @@ const express = require("express");
 const db = require("../data/db");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    db("cars")
+    .then(cars => {
+        res.json(cars);
+    })
+    .catch(err => {
+        res.status(500).json(err.message);
+    })
+})
 
+router.post("/", (req, res) => {
+    db("cars")
+    .insert(req.body)
+    .then(id => {
+        console.log(id);
+        db("cars")
+        .where({id: id[0]})
+        .then(car => {
+            res.status(201).json(car);
+        })
+    })
+    .catch(err => {
+        res.status(500).json(err.message);
+    })
+})
 
 
 
