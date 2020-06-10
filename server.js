5aa4debae099d1b7dc241450747feaf6a99da87d
Kristian Correa <kristiangcorrea@live.com>
2020-06-10 16:16:50 -0400

    mvp

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const carsRouter = require('./cars/carsRouter');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api/cars', carsRouter);
+
+module.exports = server;
