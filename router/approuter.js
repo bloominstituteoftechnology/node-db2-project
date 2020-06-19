6418dc692ed179c2157ddff906075ba1c01b1a36
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-06-18 23:33:03 -0400

    get getbyid delete working

--- router/approuter.js
+++ router/approuter.js
@@ -6,25 +6,75 @@ server.use(express.json());
 
 const db = require('../data/dbconfig')
 
-//Route Server
-
-// add any models you will use
-server.get('/', (req, res) => {
-    db('cars')
-        .then(cars => {
-            res.json(cars)
-        })
-        .catch(err => console.log(err))
-} )
-
-// Asycn Wait call for Knex
-server.get('/async', async (req, res, next) => {
+//GET Request async await ✅🏄🏽‍♂️
+server.get("/async", async (req, res, next) => {
     try {
-        const data = await db.select("*").from('accounts')
-        res.status(200).json({data: data})
+      const data = await db.select("*").from("cars");
+      res.status(200).json({ data: data });
     } catch (error) {
-        next(error)
+      next(error);
     }
-})
+  });
+
+
+//GET by :id async await✅🏄🏽‍♂️
+server.get("/async/:id", async (req, res, next) => {
+    try {
+      const data = await db("cars").where({ id: req.params.id });
+      if (data.length > 0) {
+        res.json(data);
+      } else {
+        res.status(404).json({
+          message: `User ${req.params.id} does not exist`,
+        });
+      }
+    } catch (error) {
+      next(err);
+    }
+  });
+
+//POST async await 
+server.post("/async", async (req, res, next) => {
+    try {
+      const payload = {
+        VIN: req.body.VIN,
+        Make:req.body.Make,
+        Model:req.body.Model,
+        Mileage:req.body.Mileage
+      };
+      const [dataID] = await db("cars").insert(payload, "id");
+      const displayData = await db("cars").where({ id: dataID });
+      res.status(201).json(displayData);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+//PUT async await
+server.put("/async/:id", async (req, res, next) => {
+  try {
+    const payload = {
+        VIN: req.body.VIN,
+        Make:req.body.Make,
+        Model:req.body.Model,
+        Mileage:req.body.Mileage
+    };
+    await db("cars").update(payload).where({ id: req.params.id });
+    res.json({
+      message: `Record succesfullyupdated`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE with asycn await
+server.delete("/async/:id", async (req, res, next) => {
+    try {
+      await db("cars").where({ id: req.params.id }).del();
+      res.json({message: `User ${req.params.id} successfully deleted`})
+    } catch (error) { next(error)}
+  });
+
 //export your router
 module.exports = server
--- seeds/01-cars.js
+++ seeds/01-cars.js
@@ -1,20 +1,8 @@
 const randomVin = () => {
   return Math.floor(100000000 + Math.random() * 1700000000000000000)
 }
-
-// exports.seed = function(knex) {
-//   // Deletes ALL existing entries
-//   return knex('cars').truncate()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('cars').insert([
-//         {VIN:25132151515151, Make:"Mazda", Model:"cx-30", Mileage:1003},
-
-//       ]);
-//     });
-// };
-
-
+// how can I add transmission type to seed data?
+// how do I get the database to refresh
 exports.seed = async function(knex){
   const testData = [
     {VIN:randomVin(), Make:"Mazda", Model:"cx-30", Mileage:1003},
@@ -34,13 +22,3 @@ exports.seed = async function(knex){
   return knex('cars').insert(testData)
 }
 
-// {VIN:randomVin(), Make:"Mazda", Model:"cx-30", Mileage:1003},
-// {VIN:randomVin(), Make:"volvo", Model:"xc-50", Mileage:84003},
-// {VIN:randomVin(), Make:"BMW", Model:"x8", Mileage:42000},
-// {VIN:randomVin(), Make:"volvo", Model:"xc-40", Mileage:36200, "Transmission Type":"automatic"}, ,
-// {VIN:randomVin(), Make:"Mercedes", Model:"c class", Mileage:17800},
-// {VIN:randomVin(), Make:"Acura", Model:"tlx", Mileage:22654, "Transmission Type":"automatic"},
-// {VIN:randomVin(), Make:"Ford", Model:"f150", Mileage:95587, "Transmission Type":"manual"},
-// {VIN:randomVin(), Make:"Kia", Model:"rio", Mileage:103335, "Title Status":"unknown"},
-// {VIN:randomVin(), Make:"Hyundai", Model:"elantra", Mileage:54225, "Title Status":"unknown", "Transmission Type":"automatic"},
-// {VIN:randomVin(), Make:"Mazda", Model:"3", Mileage:67114},
