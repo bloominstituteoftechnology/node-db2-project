9962a634fa79778102276eb78edcc295dc35164b
Ashley <ashley.bergsma42@gmail.com>
2020-09-09 12:48:46 -0700

    Ran knex migrate:latest and pulled up db in sqlitestudio. Created a cars router and wrote the POST endpoint

---
+++ cars/carsRouter.js
@@ -0,0 +1,30 @@
+//* Import Express and the db config/connection *// 
+const express = require('express'); 
+const db = require('../data/db-config'); 
+
+//* create router *// 
+const router = express(); 
+
+//* Now let's add some data to the cars table! *// 
+//! POST a new car! 
+//* For a POST we navigate to the /api/cars/ route, no ID is in the path because the new car doesn't have one yet! We will need a payload - the new car, which is found on req.body from the front end. Then we will INSERT the new car into the previously empty array *// 
+router.post('/', (req, res) => {
+    const newCar = req.body; 
+    db('cars')
+        .insert(newCar, 'id')
+        .then(ids => {
+            db('cars')
+                .where({id:ids[0]})
+                .first()
+                .then(car => {
+                    res.status(201).json({ data: car })
+                });
+        })
+        .catch(err => {
+            res.status(500).json({ message: "This car could not be added" })
+        });
+}); 
+
+//* export the router *// 
+module.exports = router; 
+//! Don't forget to update server.js to reflect this new router! !// 
