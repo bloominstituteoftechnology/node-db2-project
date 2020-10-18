201aa36eb29b70050ea6e30917784a05cdd94f69
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-10-18 16:14:17 -0500

    hit mvp

--- data/car-router.js
+++ data/car-router.js
@@ -4,9 +4,20 @@ const db = require("./config")
 
 const router = express.Router()
 
-router.get("/cars", async (req,res, next) => {
+router.get("/car", async (req,res, next) => {
     try {
-        res.json(await db("cars"))
+        res.json(await db("car"))
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/car", async (req,res,next) => {
+    try {
+        const [VIN] = await db("car").insert(req.body)
+        const newCar = await db("car").where({VIN}).first()
+
+        res.status(201).json(newCar)
     } catch(err) {
         next(err)
     }
