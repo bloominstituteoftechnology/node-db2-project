9f70b31ede90c503e79f03026425bbfccc80bf64
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-10-19 15:58:21 -0400

    cars seed

--- seeds/cars.js
+++ seeds/cars.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('cars').insert([
-        {vin: "ve7676", make: 'Ford', model: "f-150", mileage: 150000, transType: "auto", titleStatus: "clean" },
-        {vin: "vhuy74647", make: 'Chevey', model: "s-10", mileage: 34000, transType: "stand", titleStatus: "one acc" },
-        {vin: "v76yhdteg", make: 'Chrystler', model: "Grand Jeroeke", mileage: 15000, transType: "auto", titleStatus: "clean" }
+        {vin: "ve7676", make: 'Ford', model: "f-150", mileage: 150000, transType: "auto", titleStatus: "clean", sold: false, img: "" },
+        {vin: "vhuy74647", make: 'Chevey', model: "s-10", mileage: 34000, transType: "stand", titleStatus: "one acc", sold: false, img: ""  },
+        {vin: "v76yhdteg", make: 'Chrystler', model: "Grand Jeroeke", mileage: 15000, transType: "auto", titleStatus: "clean", sold: false, img: ""  }
       ]);
     });
 };
