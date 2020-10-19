6e413f40458cf43c5d98e408ebd918aea2160c56
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-10-18 22:07:41 -0400

    built cars router

---
+++ cars/cars-router.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const db = require("../data/config")
+
+const router = express.Router()
+
+router.get("/cars", async (req, res, next) => {
+	try {
+		res.json(await db("cars"))
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.get("/cars/:id", async (req, res, next) => {
+	try {
+		const { id } = req.params
+		const car = await db("cars").where({ id }).first()
+		
+		res.json(car)
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.post("/cars", async (req, res, next) => {
+	try {
+		const [id] = await db("cars").insert(req.body)
+		const newFruit = await db("cars").where({ id }).first()
+
+		res.status(201).json(newFruit)
+	} catch(err) {
+		next(err)
+	}
+})
+
+
+module.exports = router
