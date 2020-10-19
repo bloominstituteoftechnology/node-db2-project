bba024f248c085ea541fced9a00ba3cb4838a80f
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-10-18 22:08:26 -0400

    buillt in some seeds

---
+++ seeds/cars.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('cars').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cars').insert([
+        {vin: "ve7676", make: 'Ford', model: "f-150", mileage: 150000, transType: "auto", titleStatus: "clean" },
+        {vin: "vhuy74647", make: 'Chevey', model: "s-10", mileage: 34000, transType: "stand", titleStatus: "one acc" },
+        {vin: "v76yhdteg", make: 'Chrystler', model: "Grand Jeroeke", mileage: 15000, transType: "auto", titleStatus: "clean" }
+      ]);
+    });
+};
