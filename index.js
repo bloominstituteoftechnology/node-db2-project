703522527304a4d06f946b4dfc169271dbe62abb
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-10-18 23:56:24 -0400

    added sales

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const helmet = require("helmet")
 const welcomeRouter = require("./welcome/welcome-router")
 const cars = require("./cars/cars-router")
+const sales = require("./sales/sales-router")
 
 const server = express()
 const port = process.env.PORT || 5000
@@ -11,6 +12,7 @@ server.use(express.json())
 
 server.use(welcomeRouter)
 server.use(cars)
+server.use(sales)
 
 server.use((err, req, res, next) => {
 	console.log(err)
