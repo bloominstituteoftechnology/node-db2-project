dddf1eead8500304afdabeb178bf1fa54a629c5a
Esteban <esteban@estebans-mbp.lan>
2020-11-16 00:21:32 -0600

    added GET POST 2x. api supports delete car, add car, and get all acars

--- api/server.js
+++ api/server.js
@@ -6,9 +6,12 @@ const db = require("../data/dbConfig.js");
 
 //middlewares
 const cors = require('cors');
+const { json } = require('express');
 server.use(express.json(), cors());
 
-//Initial Server Request
+// ENDPOINTS
+
+
 //GET
 server.get('/api/vehicles', async (req, res, next)=>{
     try{
@@ -33,13 +36,52 @@ server.post('/api/add_vehicle', async (req, res, next) => {
             Mileage: `${newVehicle.Mileage}`,
             titleIsClean: newVehicle.titleIsClean
         })
-        .into('car-dealer')
+        .into('car-dealer');
 
-        res.json({ message: `Youre ${newVehicle.Make} has been added to the database`})
+        res.json({ message: `Your ${newVehicle.Make} has been added to the database`})
 
     } catch (err) {
         next(err);
     }
+});
+
+//DELETE or sold vehicle
+
+server.post('/api/rm_vehicle', async (req, res, next) =>{
+   
+
+    try {
+        const inputVehicle = await (req.body)
+        const dataBaseVehicle = await db
+        .select(`id`,'Make', 'VIN').from('car-dealer')
+        .where(`id`, `=`, inputVehicle.id);
+        
+
+        console.log(`Input Vehicle :` ,inputVehicle, `Database Vehicle:` , dataBaseVehicle[0])
+
+        
+        if (
+            dataBaseVehicle[0].VIN === inputVehicle.VIN
+            &&
+            dataBaseVehicle[0].id === inputVehicle.id 
+            &&
+            dataBaseVehicle[0].Make === inputVehicle.Make
+        ){
+            await db('car-dealer')
+            .where('id', dataBaseVehicle[0].id)
+            .del();
+
+            res.json({message: `Your ${dataBaseVehicle[0].Make} has been removed from the database.`})
+        }else{
+            res.json({message: `Double check your input` })
+        }
+       
+
+    } catch (err) {
+        next(err)
+    }
+
+    
 })
 
 
