ae10bfe01fed004aff61740deac1a6d759d49e07
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-30 13:54:27 -0500

    created first migration

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,10 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations:{
+      directory:'./data/migrations'
     }
   },
 
