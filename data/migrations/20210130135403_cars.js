1278667802503db606cb8f61d5bac4bf311456e2
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-30 13:58:49 -0500

    knex createSchema and table

--- data/migrations/20210130135403_cars.js
+++ data/migrations/20210130135403_cars.js
@@ -1,8 +1,16 @@
+const { table } = require("../db-config");
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars',tbl=>{
+      tbl.increments('VIN');
+      tbl.string('make',128);
+      tbl.string('model',128);
+      tbl.integer('mileage');
+      tbl.text('transmission');
+      tbl.string('title_status',128);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('cars');
 };
