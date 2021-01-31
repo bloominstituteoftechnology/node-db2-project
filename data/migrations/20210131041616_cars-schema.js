5bb1c0f26c482844422590c7b9793dba0f924347
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-01-31 05:04:28 -0800

    Start off

---
+++ data/migrations/20210131041616_cars-schema.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars',table =>{
+      table.increments('id');
+      table.string('name', 50).notNullable().unique();
+      table.string('model',15).notNullable();
+      table.string('condition').notNullable();
+      table.float('price',6).notNullable();
+      table.string('description',150);
+      table.string('color',15);
+      table.string('producer',15);
+      table.integer('wheels',1);
+      table.string('motor',10);
+      table.string('vin',15);
+      table.string('liscense plate',10);
+      table.string('addons',50);
+
+    
+
+
+
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
