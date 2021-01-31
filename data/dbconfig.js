092beb48ef2f5844396ab10c1787b413ba8a796b
Rex Zibit <rex@localhost.localdomain>
2021-01-31 00:46:39 -0800

    Learning Migrations

---
+++ data/dbconfig.js
@@ -0,0 +1,5 @@
+const knex = require('knex');
+const config = require('../knexfile.js')
+
+const configuredKnex =knex(config.development);
+module.exports = configuredKnex;
---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/produce.db3'
+    },
+    useNullAsDefault: true
+  },
+
+  
+
+};
