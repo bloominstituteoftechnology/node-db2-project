a85babafb772fa1f05ea5f9df1d5f53c38a6729e
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2021-01-31 15:17:40 -0800

    Server Runs

---
+++ api/cars/cars-router.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+
+const db = require('../../data/db-config');
+
+
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    db('cars')
+    .then(cars => {
+        res.json(cars);
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Failed to retrieve data'})
+    })
+})
+
+module.exports = router;
---
+++ data/db-config.js
@@ -0,0 +1,9 @@
+const knex = require('knex');
+
+const config = require('../knexfile.js');
+
+const configuredKnex = knex(config.development);
+ 
+module.exports = configuredKnex;
+
+
