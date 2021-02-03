5411fa30451bb440ba02f4d4437d9ccafed0ff67
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-02-02 17:42:14 -0700

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const server = require(""./api/server.js")
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`\n*** server running on http://localhost:${port} ***\n`)
+})
