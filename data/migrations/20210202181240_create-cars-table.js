253feb1d887b710fe7d94520b7cd4fefcfe7a916
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-02-02 18:28:02 -0700

    added database

--- data/migrations/20210202181240_create-cars-table.js
+++ data/migrations/20210202181240_create-cars-table.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('cars', (tbl) => {
+        tbl.increments();
+        tbl.integer('VIN').unique().notNullable();
+        tbl.text('make', 128).notNullable();
+        tbl.text('model', 128).notNullable();
+        tbl.integer('mileage').notNullable();
+        tbl.text('transmissionType', 128);
+        tbl.text('titleStatus');
+
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
   
 };
