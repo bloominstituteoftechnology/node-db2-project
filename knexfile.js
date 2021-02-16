e028bd09d627bf37af0c39a8a8112d0ceedf99f3
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-16 13:10:24 -0600

    added a knex file, deleted staging and production specs

---
+++ knexfile.js
@@ -0,0 +1,27 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault:true,
+    migrations:{
+      directory:''
+    },
+    seeds:{
+      directory:''
+    }
+  },
+
+  staging: {
+    
+  },
+
+  production: {
+    
+  }
+
+};
