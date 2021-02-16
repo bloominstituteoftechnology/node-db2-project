08e6cfa179a92649dcb10e61fe221b81e80c64f1
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-16 16:10:22 -0600

    added CREATE and READ functionality to routers and models, MVP met

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -3,6 +3,13 @@ const db = require('../../data/dbConfig')
 const get = ()=>{
     return db("cars")
 } 
+const create = (newCar)=>{
+    return db("cars").insert(newCar)
+        .then(([id])=>{
+            return db("cars").where("id",id).first();
+        })
+}
 module.exports={
-    get
+    get,
+    create
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -10,4 +10,13 @@ router.get('/',async(req,res)=>{
         res.status(400).json({message:`${error}`})
     }
 })
+router.post('/', async(req,res)=>{
+    try{
+        const car = req.body
+        const data = Cars.create(car)
+        res.status(200).json(data)
+    } catch(error){
+        res.status(400).json({message:`${error}`})
+    }
+})
 module.exports=router;
