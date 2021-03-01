ba5d1ab0525b06323b5859db76c06c881a4b17e4
Eric Della <ericdella13@gmail.com>
2021-02-28 19:37:18 -0800

    cars schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+exports.up = async function(knex) {
+    await knex.schema.createTable("cars", (table) => {
+        table.increments("id")
+        table.text("vin").notNull().unique()
+        table.text("make").notNull()
+        table.text("model").notNull()
+        table.text("mileage").notNull()
+        table.text("title")
+        table.text("transmission")
+    })
+}
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("cars")
+}
