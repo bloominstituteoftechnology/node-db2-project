fec45d2b442836207873ea021ce9d2b225b0d3f1
Eli <elize.san10@gmail.com>
2021-03-16 17:43:04 -0500

    add seed data

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
 // STRETCH
+
+exports.seed = function (knex) {
+  return knex("cars")
+    .truncate()
+    .then(function () {
+      return knex("cars").insert([
+        {
+          vin: "SCBFR7ZA5CC072256",
+          make: "Ford",
+          model: "Bronco",
+          mileage: 123456,
+          title: "salvage",
+          transmission: "manual",
+        },
+        {
+          vin: "SCBFR7ZA5CC072257",
+          make: "Toyota",
+          model: "Camry",
+          mileage: 654321,
+          title: "clean",
+          transmission: "manual",
+        },
+        {
+          vin: "SCBFR7ZA5CC072258",
+          make: "Lexus",
+          model: "ES",
+          mileage: 987654,
+          title: "clean",
+          transmission: "automatic",
+        },
+      ]);
+    });
+};
