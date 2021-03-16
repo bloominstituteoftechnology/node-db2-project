9f58b6fec698795342c4707b44329117185dbcf3
paulstgermain <paulg.stgermain@gmail.com>
2021-03-16 15:55:39 -0400

    adds initial migration & seeds database

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+exports.up = knex => {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.string('vin').unique().notNullable();
+        tbl.string('make').notNullable();
+        tbl.string('model').notNullable();
+        tbl.integer('mileage').notNullable();
+        tbl.string('title').defaultTo(null);
+        tbl.string('transmission').defaultTo(null);
+    });
+};
+
+exports.down = knex => {
+    return knex.schema.dropTableIfExists('cars');
+};
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+exports.seed = knex => {
+    return knex('cars').truncate()
+        .then(() => {
+            return knex('cars').insert([
+                {
+                    vin: '0B28',
+                    make: 'PT Cruiser',
+                    model: '2007',
+                    mileage: 2,
+                    title: 'N/A',
+                    transmission: '?'
+                },
+                {
+                    vin: '0V32',
+                    make: 'Jaguar',
+                    model: '2006',
+                    mileage: 10000,
+                    title: 'Testy McTesterton'
+                },
+                {
+                    vin: '0BV1',
+                    make: 'Car',
+                    model: '2167',
+                    mileage: 69,
+                    transmission: 'Sure'
+                }
+            ]);
+        });
+};
