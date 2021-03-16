20fcfb74217c1c015eda7f281533fdb1b9fa1c69
Eli <elize.san10@gmail.com>
2021-03-16 17:49:42 -0500

    write model functions

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,23 @@
+const db = require("../../data/db-config");
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return db("cars");
+};
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
-}
+  return db("cars").where({ id }).first();
+};
 
-const create = () => {
+const create = async (car) => {
   // DO YOUR MAGIC
-}
+  const [id] = await db("cars").insert(car);
+  return getById(id);
+};
