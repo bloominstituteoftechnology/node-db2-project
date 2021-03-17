ee78b6dfee9c975f3a269fd54ac88d5170e36476
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-17 16:12:09 -0700

    set up car schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,23 @@
 // DO YOUR MAGIC
+
+// does the structural changes to the db
+exports.up = function (knex) {  
+    return knex.schema
+      .createTable('cars', table => {
+        table.increments('id') // primary key
+        table.text('vin').unique().notNullable()
+        table.text('make').notNullable()
+        table.text('model').notNullable()
+        table.integer('mileage').notNullable()
+        table.text('title')
+        table.text('transmission')
+      })
+  };
+  
+  // undoes those changes
+  exports.down = function (knex) {
+  
+    return knex.schema
+      .dropTableIfExists('cars')
+  };
+  
