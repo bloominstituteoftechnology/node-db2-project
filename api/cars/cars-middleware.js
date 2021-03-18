14e7e4ac1c7756ebd4957b123c6643cba233f09f
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-17 17:01:45 -0700

    checkCarId done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,20 @@
+const Cars = require('./cars-model')
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
+  const {id} = req.params
+  Cars.getById(id)
+    .then(car => {
+      if (!car) {
+        res.status(404).json({message: `car with id ${id} is not found`})
+      } else {
+        req.car = car;
+        next()
+      }
+    }).catch(err => {
+      next(err)
+    })
+
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +27,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
