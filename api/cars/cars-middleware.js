125e2b045fbea8a2749f706bf131f8c69ffab1c6
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-17 17:23:31 -0700

    checkCarPayload done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -19,6 +19,18 @@ const checkCarId = (req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const {vin, make,model,mileage} = req.body;
+  if (!vin) {
+    res.status(400).json({message: `vin is missing`})
+  } else if (!make) {
+    res.status(400).json({message: `make is missing`})
+  } else if (!model) {
+    res.status(400).json({message: `model is missing`})
+  } else if (!mileage) {
+    res.status(400).json({message: `mileage is missing`})
+  } else {
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
