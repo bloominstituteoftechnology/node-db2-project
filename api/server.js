f9ee4e51d01b2a087961041ca2ee2245d5005b50
kato hawkins <kristhahawkins@icloud.com>
2021-03-28 14:24:48 -0400

    server.js ccomplete

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const helmet = require('helmet')
 
+const carsRouter = require('./cars/cars-router')
 const server = express()
 
 // DO YOUR MAGIC
-
+server.use(helmet())
+server.use(express.json())
+server.use('/api/cars', carsRouter)
 module.exports = server
