76659daa1a8864f8583085f9ce605c4f1a836666
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-04-03 11:42:59 -0600

    added server

--- api/server.js
+++ api/server.js
@@ -2,6 +2,13 @@ const express = require("express")
 
 const server = express()
 
+const carsRouter= require("./cars/cars-router");
+
 // DO YOUR MAGIC
+
+
+server.use(express.json());
+
+server.use("/api/cars", carsRouter);
 
 module.exports = server
