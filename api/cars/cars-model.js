23a3cb958966ecc2c0b21f986e21a9464b47aba7
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-04-03 12:05:04 -0600

    model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,21 @@
+const db = require("../../data/db-config");
+
+
 const getAll = () => {
   // DO YOUR MAGIC
+return db("cars");
+
 }
 
 const getById = () => {
   // DO YOUR MAGIC
+  return db("cars").where("car_id", id).first();
+
 }
 
 const create = () => {
   // DO YOUR MAGIC
+  const [id] = await db("cars").insert(car);
+  return getById(id);
+
 }
