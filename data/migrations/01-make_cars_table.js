ca1dd67503a6847d785b7b793664150b6041ddea
Matthew Ellis <mbellistx@gmail.com>
2021-04-13 17:09:22 -0500

    migration and seed done

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', table => {
+    // turns out that sqlite does not enforce lengths
+    // it also is dynamic typed and has no problem putting '12' into an integer column
+    table.increments()
+    table.specificType('vin', 'CHAR(17)').unique().notNullable()
+    table.string('make', 128).notNullable()
+    table.string('model', 128).notNullable()
+    table.integer('mileage').notNullable()
+    table.string('title', 128)
+    table.string('transmission', 9)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars')
+};
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,12 @@
-// STRETCH
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('cars').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cars').insert([
+        {vin: '12345678901234567', make: 'honda', model: '2005 oddesy', mileage: 120000},
+        {vin: '22345678901234567', make: 'ford', model: 'the flying one', mileage: 0, title: 'idk', transmission: 'automatic'},
+      ]);
+    });
+};
