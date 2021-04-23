5094ac35c4afb4924b46839bba3d3ca0664d13b5
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-22 19:49:15 -0600

    wrote the cars schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,15 @@
-// DO YOUR MAGIC
+exports.up = async function(knex) {
+    await knex.schema.createTable("cars", (table) => {
+        table.increments("id")
+        table.text("vin").notNull().unique()
+        table.text("make").notNull()
+        table.text("model").notNull()
+        table.integer("mileage").notNull()
+        table.text("title")
+        table.text("transmission")
+    })
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("cars")
+}
