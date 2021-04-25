72f6f865c0585a4fc0222e20792b3f1b2b2d5f93
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-25 17:49:18 -0600

    finished middleware, need to test still

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,89 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const cars = require("../cars/cars-model")
+const vinValidator = require("vin-validator")
 
+const checkCarId = (req, res, next) => {
+  return () => {
+    cars.getById(req.params.id)
+      .then(car => {
+        if(car){
+          req.car = car
+          next()
+        } else {
+          res.status(404).json({
+            message: `car with id ${req.params.id} is not found`
+          })
+        }
+      })
+      .catch(next)
+  }
+}
+//vin make model mileage
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  return () => {
+    console.log(req.body)
+
+    if(!req.body.vin){
+      const fieldName = vin
+
+      return res.status(400).json({
+        message: `${fieldName} is missing`
+      })
+    } else if(!req.body.make){
+        const fieldName = make
+
+        return res.status(400).json({
+        message: `${fieldName} is missing`
+      })
+    } else if(!req.body.model){
+      const fieldName = model
+
+      return res.status(400).json({
+        message: `${fieldName} is missing`
+      })
+    } else if(!req.body.mileage){
+      const fieldName = mileage
+      return res.status(400).json({
+        message: `${fieldName} is missing`
+      })
+    }
+
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  return () => {
+    if(vinValidator.validate(`${req.body.vin}`) = false){
+      res.status(400).json({
+        message: `Vin ${req.body.vin} is invalid`
+      })
+    }
+    next()
+  }
 }
 
 const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  return () => {
+    cars.getAll()
+      .then(allCars => {
+        Array.from(allCars)
+
+        allCars.find(car => {
+          if(car.body.vin === req.body.vin){
+            return res.status(400).json({
+              message: `Vin ${req.body.vin} already exists`
+            })
+          }
+        })
+      })
+    
+    next()
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
