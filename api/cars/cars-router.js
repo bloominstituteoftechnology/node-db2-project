2db966b7b2702b97904ef6237a9c4785cf37faa9
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-25 18:08:24 -0600

    finished router and server. testing now.

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,40 @@
-// DO YOUR MAGIC
+const router = require("express").Router()
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require("./cars-middleware")
+const db = require("./cars-middleware")
+
+router.get("/api/cars", async (req, res, next) => {
+    try {
+        const cars = await db.getAll()
+        res.status(200).json(cars)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get("/api/cars/:id", checkCarId(), async (req, res, next) => {
+    try {
+        const car = await db.getById(req.car.id)
+        res.status(200).json(car)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/api/cars", checkCarPayload, checkVinNumberValid(), checkVinNumberUnique(), (req, res, next) => {
+    try {
+        const car = db.create()
+        res.status(201).json(car)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => {
+    console.log(err)
+  
+    res.status(500).json({
+      message: "Something went wrong",
+    })
+  })
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require("express")
+const carsRouter = require("./cars/cars-router")
 
 const server = express()
-
-// DO YOUR MAGIC
+server.use(express.json())
+server.use(carsRouter)
 
 module.exports = server
