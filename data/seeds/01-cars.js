fc8374c4d7921097bd88387108502677b7e8d4b9
yonathan Admasu <admyoni4@yahoo.com>
2021-06-18 04:33:26 -0700

    installed the depndecies like vin validator

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
 // STRETCH
+const cars = [
+    {
+    vin:'1111111111111',
+    make: 'toyota',
+    model: 'prius',
+    milege: 215000,
+    title: 'clean'
+    },
+    {
+        vin:'1111111111111',
+        make: 'toyota',
+        model: 'corola',
+        milege: 215000,
+        title: 'salvage'
+        },
+        {
+            vin:'1111111111111',
+            make: 'ford',
+            model: 'focus',
+            milege: 215000,
+            title: 'clean'
+            }
+]
+exports.seed = function (knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
+// exports.seed = async function (knex) {
+//     await knex('cars').truncate()
+//     await knex('cars').insert(cars)
+// }
