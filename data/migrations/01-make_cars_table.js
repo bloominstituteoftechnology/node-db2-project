8137de1b7d4d279387ec1c58cd822e985fef90c4
Saeed Khan <saeedkhan@ghost.hsd1.co.comcast.net>
2021-06-18 10:22:04 -0600

    database added

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+exports.up = function(knex) {
+    return knex.schema.createTable("cars", table => {
+        table.increments();
+        table.text("vin", 17).unique().notNullable();
+        table.text("make", 20).notNullable();
+        table.text("model", 20).notNullable();
+        table.decimal("mileage").notNullable();
+        table.text("title")
+        table.text("transmission")
+    });
+}
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("cars");
+}
