c9f5cb1ce3f5da40f75c103962ca164a13319e5e
yonathan Admasu <admyoni4@yahoo.com>
2021-06-18 15:30:52 -0700

    migration for make cars done

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,15 @@
 // DO YOUR MAGIC
+exports.up = function (knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increaments()
+        tbl.string('vin', 17).notNullable().unique()
+        tbl.string('make', 128).notNullable()
+        tbl.string('model', 128).notNullable()
+        tbl.numeric('mileage').unsigned().notNullable()
+        tbl.string('title',128)
+        tbl.string('transmission',128)
+    })
+}
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('cars')
+}
