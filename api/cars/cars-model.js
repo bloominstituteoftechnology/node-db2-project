de00cb9dfc6a38f54b63981d85a30dbd0627003f
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-06-19 12:29:57 -0400

    created cars-model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,20 @@
+const db = require("../../data/db-config.js");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  return db("cars");
+};
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+  return db("cars").where("id", id).first();
+};
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async (account) => {
+  const[id] = await db("accounts").insert(account);
+  return getById(id);
+};
+
+module.exports {
+getAll,
+getById,
+create
 }
