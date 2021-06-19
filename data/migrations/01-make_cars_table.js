7b7966b49bd822b1bf5bc4765b772e1ce765ccb2
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-06-19 12:23:08 -0400

    created cars table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,14 @@
-// DO YOUR MAGIC
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (table) => {
+    table.increments();
+    table.text("vin").unique().notNullable();
+    table.text("make").notNullable();
+    table.integer("mileage").notNullable();
+    table.text("title");
+    table.text("transmission");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropIfTableExists("cars");
+};
