9dcc49df64e1d83cc4327931143438f09e760f78
Molly Brown <mollybrown0516@gmail.com>
2021-06-20 22:32:17 -0400

    yarn.lock and migrations

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,23 @@
 // DO YOUR MAGIC
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cars', function(table) {
+        // id int unsigned not null auto_increment primary key
+        // @see http://knexjs.org/#Schema-increments
+        table.increments();
+        table.string('vin').notNullable();
+        table.string('make').notNullable();
+        table.string('model').notNullable();
+        table.integer('mileage').notNullable();
+        table.string('title');
+        table.string('transmission');
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+        table.timestamp('updated_at').defaultTo(knex.fn.now())
+
+        // Constraints
+        table.unique('vin');
+    })
+}
+  
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('cars');
+}
