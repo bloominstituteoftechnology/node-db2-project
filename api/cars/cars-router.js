a7ade829dd3cb6b6c4dbb66a185631a3e134979a
abemm7787 <mirambeaux7@gmail.com>
2021-07-14 00:36:21 -0500

    middleware & knexfile

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,49 @@
 // DO YOUR MAGIC
+const express = require("express");
+const router = require('express').Router();
+
+const Cars = require('./cars-model');
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+
+} = require('./cars-middleware');
+
+
+
+router.get('/', (req, res, next) => {
+  Cars.getAll()
+    .then(cars => {
+        res.status(200).json(cars);
+    })
+    .catch(next);
+});
+
+
+router.get('/:id', checkCarId, (req, res, next) => {
+    res.status(200).json(req.car)
+    .catch(next)
+})
+
+
+router.put('/:id', checkCarId, checkCarPayload, checkVinNumberValid, checkVinUniqueOrMatch, (req, res, next) => {
+  Cars.update(req.params.id, req.body)
+    .then(updated => {
+      res.json(updated);
+    })
+    .catch(next);
+});
+
+router.delete('/:id', checkCarId, (req, res, next) => {
+  const deletedCar = req.car;
+  Cars.remove(req.params.id)
+    .then(deletedNum => {
+      res.json(deletedCar);
+    })
+    .catch(next);
+});
+
+module.exports = router;
+
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,38 @@
+
+// lets buid the up migration
+// are creatting a blueprint of the database that is ging to build
+
+
 exports.up = function (knex) {
   // DO YOUR MAGIC
+
+  return knex.schema.createTable("cars" ,(table) => {
+table.increments("id")
+table.text("vin")
+.unique()
+.notNullable()
+  
+table.text("make")
+.notNullable()
+
+table.text("model")
+.notNullable()
+
+table.text("title")
+.notNullable()
+
+table.integer("mileage")
+.notNullable()
+
+table.text("transmission")
+.notNullable();
+  
+  })
 };
 
+
+// this deletes our table
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars");
 };
