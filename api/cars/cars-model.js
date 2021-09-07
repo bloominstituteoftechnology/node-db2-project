8bc9f113f6eec5dd4c1fd293ce23ae29c6d4c501
Leu Family <romanianfobjr@gmail.com>
2021-09-07 15:36:10 -0500

    getall, getbyid, and create working

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -2,14 +2,18 @@ const db = require('../../data/db-config')
 
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars')
 }
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
+  return db('cars').where('id', id).first()
 }
 
-const create = () => {
+const create = async (car) => {
   // DO YOUR MAGIC
+  const [id] = await db('cars').insert(car)
+  return getById(id)
 }
 
 module.exports = {
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -4,5 +4,31 @@ const Car = require('./cars-model')
 
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+    try {
+        const all = await Car.getAll()
+        res.json(all)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const car = await Car.getById(req.params.id)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newCar = await Car.create(req.body)
+        res.status(201).json(newCar)
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router
