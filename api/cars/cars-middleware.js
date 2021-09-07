c55ca8f7229586a03fbfa1fe377aec6b32bdf0ad
Leu Family <romanianfobjr@gmail.com>
2021-09-07 16:06:52 -0500

    middlewares written

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -19,6 +19,24 @@ const checkCarId = async (req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const error = { status: 400 }
+  // { message: "<field name> is missing" }
+  const { vin, make, model, mileage } = req.body
+  if (vin === undefined) {
+    error.message = 'vin is missing'
+  } else if (make === undefined) {
+    error.message = 'make is missing'
+  } else if (model === undefined) {
+    error.message = 'model is missing'
+  } else if (mileage === undefined) {
+    error.message = 'mileage is missing'
+  }
+
+  if (error.message) {
+    next (error)
+  } else {
+    next()
+  }
 }
 
 const checkVinNumberValid = async (req, res, next) => {
