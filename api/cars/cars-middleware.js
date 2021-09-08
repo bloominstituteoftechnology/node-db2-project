510b8814ced2d245bbd41d721d9665c24c246860
ferengitooth <ehitsamill@gmail.com>
2021-09-08 01:28:24 -0700

    finished all middlewars

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -43,8 +43,17 @@ const checkVinNumberValid = (req, res, next) => {
   }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  next()
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+    const existing = await Car.getByVin(req.body.vin)
+    if (!existing) {
+      next()
+    } else {
+      next ({ status: 400, message: `vin ${req.body.vin} already exists`})
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = {
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -8,6 +8,10 @@ const getById = (id) => {
   return db('cars').where('id', id).first()
 }
 
+const getByVin = (vin) => {
+  return db('cars').where('vin', vin).first()
+}
+
 const create = () => {
   // DO YOUR MAGIC
 }
@@ -16,5 +20,6 @@ const create = () => {
 module.exports = {
   getAll,
   getById,
-  create
+  create,
+  getByVin
 }
