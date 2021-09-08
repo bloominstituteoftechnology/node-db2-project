f845cda1dea7c19b93747a5f8966e622d9d43041
Johanna Rodriguez <>
2021-09-07 19:33:37 -0500

    add up function

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,21 @@
+const { Knex } = require("knex");
+
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return Knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).notNullable().unique()
+
+    tbl.string('make', 128).notNullable()
+
+    tbl.string('model', 128).notNullable()
+
+    tbl.string('mileage').unsigned().notNullable()
+
+    tbl.string('title', 128)
+
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: '57566876999',
+        make: 'toyota',
+        model: 'rav4',
+        mileage: 250000,
+        title: 'clean',
+        transmission: 'Automatic',
+    },
+    {
+        vin: '57566876999',
+        make: 'toyota',
+        model: '4Runner',
+        mileage: 50000,
+        title: 'salvage',
+    },
+    {
+        vin: '57566876999',
+        make: 'jeep',
+        model: 'grand cherokee',
+        mileage: 5000,
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+  }
