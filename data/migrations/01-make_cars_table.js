116e77190d0afe0a8d8b23602916f7b77dec4380
ferengitooth <ehitsamill@gmail.com>
2021-09-07 23:23:41 -0700

    made migration tables

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+    tbl.string('vin', 17).notNullable().unique()
+    tbl.string('make', 128).notNullable()
+    tbl.string('model').notNullable()
+    tbl.numeric('mileage').unsigned().notNullable()
+    tbl.string('title')
+    tbl.string('transmission')
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
