abf52ccaff6407d205246e644bde1c0a71ba9d80
ferengitooth <ehitsamill@gmail.com>
2021-09-07 23:16:46 -0700

    added seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage'
+    },
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000
+    },
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .insert(cars)
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
