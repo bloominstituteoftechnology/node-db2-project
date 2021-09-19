bf117caa99b93eb66ba6a604a13a1c8d7c76dc7d
Stan <stan2dor@gmail.com>
2021-09-19 11:48:50 -0700

    Project & Server setup

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
-const express = require("express")
+const express = require("express");
+// const carsRouter = require("./cars/cars-router");
+const server = express();
 
-const server = express()
+server.use(express.json());
+// server.use("/api/cars", carsRouter);
 
-// DO YOUR MAGIC
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "is working" });
+});
 
-module.exports = server
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "Not found" });
+});
+
+// eslint-disable-next-line
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ apimessage: err.message });
+});
+
+module.exports = server;
