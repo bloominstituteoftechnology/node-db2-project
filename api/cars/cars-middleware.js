468e38abad67de6956fde2b928b8e5b42a2c993d
Stan <stan2dor@gmail.com>
2021-09-19 14:25:05 -0700

    BUG on POST - cannot find the vin module

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,5 @@
 const Car = require("./cars-model");
-// const vin = require("./cars-model");
+const vin = require("./cars-model");
 
 const checkCarId = async (req, res, next) => {
   try {
@@ -19,28 +19,28 @@ const checkCarPayload = (req, res, next) => {
   if (!req.body.vin)
     return next({
       status: 400,
-      message: `vin is missing`,
+      message: "vin abc is missing",
     });
   if (!req.body.make)
     return next({
       status: 400,
-      message: `make is missing`,
+      message: "make is missing",
     });
   if (!req.body.model)
     return next({
       status: 400,
-      message: `model is missing`,
+      message: "model is missing",
     });
   if (!req.body.mileage)
     return next({
       status: 400,
-      message: `mileage is missing`,
+      message: "mileage is missing",
     });
   next();
 };
 
 const checkVinNumberValid = (req, res, next) => {
-  if (req.body.vin) {
+  if (vin.validate(req.body.vin)) {
     next();
   } else {
     next({
