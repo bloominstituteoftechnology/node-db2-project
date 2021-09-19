e50484ff269da250f24eda42a515d41a691857f5
Stan <stan2dor@gmail.com>
2021-09-19 12:06:00 -0700

    GET Car by Id

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
 const Car = require("./cars-model");
 
+const { checkCarId } = require("./cars-middleware");
+
 router.get("/", (req, res, next) => {
   Car.getAll()
     .then((cars) => {
       res.json(cars);
     })
     .catch(next);
+});
+
+// eslint-disable-next-line
+router.get("/:id", checkCarId, async (req, res, next) => {
+  res.json(req.car);
 });
 
 module.exports = router;
