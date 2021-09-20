1b35421ebafe821eaf58cce78fb7b4297f7069b7
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-20 16:03:33 +0200

    initializing

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+
+const cars = [
+    {
+        vin: '111111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '111111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: '111111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    }
+]
+
+// exports.seed = function (knex) {
+//     return knex('cars')
+//     .truncate().then( ()=> {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const server = require('./api/server')
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
+
+
+// "migrate": "knex migrate:latest",
+// "seed": "knex seed: run"
+
+// --> might need npx knex migrate:latest @ script
