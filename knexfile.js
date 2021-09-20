8bfbcca8c2451b0a09dd895f4a3b658c45df7e45
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-20 16:59:03 +0200

    knexfile notes

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
 // no need to change this file
 const sharedConfig = {
   client: 'sqlite3',
-  useNullAsDefault: true,
+  useNullAsDefault: true, //line for sqlite, not needed for e.g. postgress
   migrations: {
     directory: './data/migrations',
   },
   seeds: {
     directory: './data/seeds',
   },
-  pool: {
+  pool: { //another sqlite thing
     afterCreate: (conn, done) => {
       conn.run('PRAGMA foreign_keys = ON', done)
     },
