531b389ba6a16cef8446850b9fc88ec5845109ff
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-20 19:34:27 +0200

    scaffolding endpoints, rest

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router')
 
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/cars', carsRouter)
+
+
 
 server.use('*', (req,res,next) =>{
     next({ status: 404, message: "not found!"})
