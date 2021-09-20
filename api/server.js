ee0edad4d67856c3371db803b9b77841d2046e53
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-20 18:33:26 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -2,6 +2,23 @@ const express = require("express")
 
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json())
+
+server.use('*', (req,res,next) =>{
+    next({ status: 404, message: "not found"})
+
+    //res.json( {status: 404, message: "not found"})
+
+    // res.status(404).json({
+    //     message: "not found"
+    // })
+})
+
+server.use( (err, req, res, next) => { //eslint-disable-line
+    res.status( err.status || 500).json({ // ^ need next in this line, even if we don't use it or it won't work
+        message: err.message
+    })
+})
+
 
 module.exports = server
