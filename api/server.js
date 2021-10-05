598e07ad78991155d631642e6772a32eefa4b052
Rick <ricksmycodeguy@gmail.com>
2021-10-05 17:05:37 -0500

    assured server error handler working properly

--- api/server.js
+++ api/server.js
@@ -8,19 +8,21 @@ server.use('./api/cars', carsRouter);
 server.get('/', (req, res) => {
     res.json({ message: 'Cars Api is working add a route to see more' });
     console.log('api is working');
-})
+});
 
-server.use("*", (req, res) => {
-    res.status(404).json({
-        message: 'not found'
-    })
-})
+server.use('*', (req, res, next) => {
+    next({status: 404, message: 'Bad Route try another'});
+    //alternative
+    // res.status(404).json({
+    //     message: 'Bad route try another'
+    // });
+});
 
 server.use((err, req, res, next) => { // eslint-disable-line
     res.status(err.statsu || 500).json({
         message: err.message,
         stack: err.stack,
-    })
-})
+    });
+});
 
-module.exports = server
+module.exports = server;
