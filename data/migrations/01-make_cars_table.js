5e8cafa27a3366f3a256d0d3b22acee4d5a0a408
Rick <ricksmycodeguy@gmail.com>
2021-10-05 13:47:30 -0500

    build out 01-make_cars_table.js

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.table('cars', table => {
+    table.increments();
+    table.text('vin', 128).unique().notNullable();
+    table.text('make', 128).notNullable();
+    table.text('modle', 128).notNullable();
+    table.integer('mileage').notNullable();
+    table.text('title');
+    table.text('transmission');
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
