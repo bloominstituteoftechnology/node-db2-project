845631c2c7742ad74e1d8a9387413dcd2d4380fe
Rick <ricksmycodeguy@gmail.com>
2021-10-05 14:54:13 -0500

    adjust seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -2,8 +2,8 @@
 exports.seed = async function (knex) {
     await knex('cars').truncate()
     await knex('cars').insert([
-        {vin: '11111111111111110', make: "Ford", model: "F150", mileage: '145000'},
-        {vin: '11111111111111112', make: "Ford", model: "F150", mileage: '20000'},
-        {vin: '11111111111111113', make: "Ford", model: "F150", mileage: '10000'}
+        {vin: '11111111111111110', make: "Ford", model: "F150", mileage: 145000},
+        {vin: '11111111111111112', make: "Ford", model: "F150", mileage: 20000},
+        {vin: '11111111111111113', make: "Ford", model: "F150", mileage: 10000}
     ]);
 };
