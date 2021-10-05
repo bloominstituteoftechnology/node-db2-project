a588eda63fbcaf34a6a7901cd8604a50a1e07cf8
Rick <ricksmycodeguy@gmail.com>
2021-10-05 15:56:30 -0500

    cars table successfully migrated checked n sqlite3

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,15 +1,15 @@
 exports.up = async function (knex) {
   await knex.schema.table('cars', table => {
     table.increments();
-    table.text('vin', 128).unique().notNullable();
+    table.text('vin', 17).unique().notNullable();
     table.text('make', 128).notNullable();
     table.text('modle', 128).notNullable();
     table.integer('mileage').notNullable();
-    table.text('title');
-    table.text('transmission');
+    table.text('title', 128);
+    table.text('transmission', 128);
   })
 };
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('cars');
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars');
 };
