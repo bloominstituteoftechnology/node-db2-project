11558564f67c2699f78c2900e263c6c7d3e1f928
Sarah D Herren <herren.sarah@icloud.com>
2021-10-05 13:51:03 -0500

    created migration file, added imports and endpoints in cars-router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,28 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+const {} = require('./cars-middleware');
+
+router.get('/', (req, res, next) => {
+    try {
+        
+    } catch (error) {
+       next(error) 
+    }
+})
+router.get('/:id', (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+})
+router.post('/', (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
