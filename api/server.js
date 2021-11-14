ed63ec954045cbc394157cb0ae823f57f92bc0be
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-14 12:34:36 -0800

    migrations and server set up

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router')
 
 const server = express()
-
 // DO YOUR MAGIC
+server.use(express.json())
+server.use('/api/cars', carsRouter)
 
 module.exports = server
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', (table) => {
+    table.increments()
+    table.text('vin').notNullable().unique()
+    table.text('make').notNullable()
+    table.text('model').notNullable()
+    table.integer('mileage').notNullable()
+    table.text('title')
+    table.text('transmision')
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
