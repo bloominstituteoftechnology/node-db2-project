90bcb41e2f9cfb342c0a19f7b6d8a331dca6093c
quinn5moli <quinn.moli@gmail.com>
2021-11-13 17:03:00 -0800

    begin server build; build migrations schema

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
-const express = require("express")
+const express = require('express')
+
+const carsRoutes = require('./cars/cars-router')
 
 const server = express()
 
 // DO YOUR MAGIC
+server.use(express.json());
+server.use('/api/cars', carsRoutes);
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
 
 module.exports = server
