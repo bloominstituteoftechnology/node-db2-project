de09dd3fa89580b9c06c2b6bcbeb5b3810de01d4
quinn5moli <quinn.moli@gmail.com>
2021-11-13 17:03:29 -0800

    migrations build

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,19 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+
+    table.increments();
+    table.string('vin').unique().notNullable();
+    table.string('make').notNullable();
+    table.string('model').notNullable();
+    table.string('mileage').notNullable();
+    table.string('title');
+    table.string('transmission');
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+
+  return knex.schema.dropTableIfExists('cars');
 };
