5d7a4c8555e3a9a14189d50baf28e00a7bef5c2d
quinn5moli <quinn.moli@gmail.com>
2021-11-13 17:08:42 -0800

    build cars model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,18 @@
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars')
 }
 
 const getById = () => {
   // DO YOUR MAGIC
+  return db('cars').where('id', id).first()
 }
 
-const create = () => {
+const create = async (car) => {
   // DO YOUR MAGIC
+  const [id] = await db('cars').insert(car)
+
+  return getById(id)
 }
+
+module.exports = { getAll, getById, create }
