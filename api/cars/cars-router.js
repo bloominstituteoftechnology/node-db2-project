63e375643ca1395f9a92c37fc65aa0a945783a6e
quinn5moli <quinn.moli@gmail.com>
2021-11-13 17:21:13 -0800

    routers scaffolding

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,25 @@
 // DO YOUR MAGIC
+const router = require('express').Router();
+
+
+const Cars = require('./cars-model');
+
+router.get('/', (req, res) => {
+
+})
+
+router.get(':/id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.use((err, req, res) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = router
