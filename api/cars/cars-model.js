d9a0e5d53ca0d8a66dc6e1a7c3268127a1c5029b
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-14 12:56:09 -0800

    cars-model complete

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,25 @@
-const getAll = () => {
+const db = require('../../data/db-config')
+
+const getAll = async () => {
   // DO YOUR MAGIC
+  const result = await db('cars')
+  return result
 }
 
-const getById = () => {
+const getById = async () => {
   // DO YOUR MAGIC
+  const result = await db('cars').where('id', id).first()
+  return result
 }
 
-const create = () => {
+const create = async () => {
   // DO YOUR MAGIC
+  const [ id ] = await db('cars').insert(car)
+  const post = await getById(id)
+  return post
+}
+module.exports = {
+  getAll, 
+  getById,
+  create
 }
