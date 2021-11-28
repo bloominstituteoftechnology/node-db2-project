f2493fe47e8166f1670846090501df2b4c7da1a5
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-27 16:25:17 -0800

    added migrate commands and updated database

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -10,14 +10,14 @@ const cars = [
     transmision: 'manual'
   },
   {
-    vin: '1111111111111111',
+    vin: '2222222222222222',
     make: 'jeep',
     model: 'grand cherokee',
     mileage: 115000,
     title: 'clean',
   },
   {
-    vin: '1111111111111111',
+    vin: '3333333333333333',
     make: 'ford',
     model: 'bronco',
     mileage: 250000,
