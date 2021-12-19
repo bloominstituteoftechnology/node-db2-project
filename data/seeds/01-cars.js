3b7cca8ae207f014459213d67c2150f403919dd6
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-19 15:39:05 -0800

    set up cars object array

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
-// STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla', 
+        milage: 432000
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius', 
+        milage: 32000
+        title: 'salvage',
+   
+    },
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'mustang', 
+        milage: 2000
+  
+    },
+]
+
+exports.seed = function(knex){
+    return knex('cars')
+    .truncate()
+    .then(()=> {
+        return knex('cars').insert(cars)
+    })
+}
