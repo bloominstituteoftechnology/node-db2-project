9a48fe2119e5e257e5b471642e5072aafc85ffc3
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-23 20:21:47 -0600

    started working on middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,19 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const carsModel = require("./cars-model")
+
+
+const checkCarId =async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const car = await carsModel.getById(id)
+    if (!car) {
+      res.status(404).json(`car with id ${id} could not be found`)
+    } else {
+      res.car = car
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +26,8 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId
 }
