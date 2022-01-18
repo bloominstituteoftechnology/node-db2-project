707807939fa792c9563d4d4522441dbf3d397359
Leo Oladimu <leooladimu9@gmail.com>
2022-01-18 17:13:50 -0500

    2

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express")
-
 const server = express()
+const carsRouter = require('./cars/cars-router');
 
-// DO YOUR MAGIC
+server.use(express.json());
+
+server.use('/api/cars', carsRouter);
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'wrong'
+  })
+})
 
 module.exports = server
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,22 @@
-// STRETCH
+const cars = [
+  {
+    vin: '111111111111111111',
+    make: 'chevy',
+    model: 'camaro'
+  },
+  {
+    vin: '111111111111111111',
+    make: 'ford',
+    model: 'mustang'
+  },
+  {
+    vin: '111111111111111111',
+    make: 'dodge',
+    model: 'charger'
+  }
+]
+
+exports.seed = async function(knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}
