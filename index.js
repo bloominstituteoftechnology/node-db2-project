3e5c853a088950d6bfe7d4d4808805261859367b
Leo Oladimu <leooladimu9@gmail.com>
2022-01-18 15:59:50 -0500

    1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server')
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9900;
 
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
