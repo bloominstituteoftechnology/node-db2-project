e4ada41973b283efbb92c5dcf42f2f61d9a9f497
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-29 13:03:05 -0800

    server updated

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require("express")
-
+const carsRouter = require('./cars/cars-router')
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json())
+server.use('/api/cars', carsRouter)
 
+server.use('*', (req, res, next) => {
+    next({
+        status: 404,
+        message: 'not found'
+    })
+})
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 module.exports = server
