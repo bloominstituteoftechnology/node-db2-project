6511c88f433deaf5c6bef88962d3cc26ef63df81
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-29 13:24:53 -0800

    router.get working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+const Car = require('./cars-model')
+const {
+    checkCarId,
+    checkVinNumberUnique,
+    checkCarPayload,
+    checkVinNumberValid,
+} = require('./cars-middleware')
+
+router.get('/', async (req, res, next) => {
+   try{
+    const cars = await Car.getAll()
+    res.json(cars)
+   }catch(err){
+    next(err)
+   }
+})
+module.exports = router
