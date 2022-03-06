98eb18ed72ca48f7202dcf52b1726125629576a4
Amanda Burtness <amandaburtness@gmail.com>
2022-03-06 14:17:50 -0600

    mvp

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,17 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars')
 }
 
 const getById = () => {
   // DO YOUR MAGIC
+  return db('cars').where('id', id).first()
 }
 
-const create = () => {
+const create = ({ vin, make, model, mileage, title, transmission }) => {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert({vin, make, model, mileage, title, transmission})
+  return getById(id)
 }
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments() // primary key
+    tbl.text('vin', 128).unique().notNullable()
+    tbl.string('make', 128).notNullable()
+    tbl.string('model', 128).notNullable()
+    tbl.string('mileage', 128).notNullable()
+    tbl.string('title', 128)
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("fruits")
 };
